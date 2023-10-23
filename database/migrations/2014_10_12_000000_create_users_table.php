f5d24b3081b04e918a973cf5d0e3893b383ca101
Peterson A. Sena <petersonsena24@gmail.com>
2023-10-23 17:26:42 +0200

    Creating user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',75);
+            $table->string('surname',75);
+            $table->string('password', 45);
+            $table->string('dni/nie',9)->unique();
+            $table->string('email', 125)->unique();
+            $table->boolean('active')->default(true);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
