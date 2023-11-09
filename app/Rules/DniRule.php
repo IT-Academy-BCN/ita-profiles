8f0adef91702f2438caaf1e946d639e5b35e0768
Peterson A Sena <petersonsena24@gmail.com>
2023-11-09 20:35:30 +0100

    add catala translate to invalid dni

--- app/Rules/DniRule.php
+++ app/Rules/DniRule.php
@@ -15,7 +15,7 @@ class DniRule implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         if (! ($this->isValidNif(strtoupper($value)) || $this->isValidNie(strtoupper($value)))) {
-            $fail('The :attribute is not valid.');
+            $fail('No és un :Attribute vàlid');
         }
     }
 
