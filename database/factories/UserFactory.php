65810eb53d9a88716100b5102f6b46d3cf0b4ee9
Peterson A Sena <petersonsena24@gmail.com>
2023-11-13 01:21:57 +0100

    Create and apply LoginControllerTest

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'surname' => fake()->name(),
+            'dni' => Str::random(9),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
