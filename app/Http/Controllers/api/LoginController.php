bbef044f2ba43748814bb73254c6eca4d0de5100
Peterson A Sena <petersonsena24@gmail.com>
2023-11-13 01:18:32 +0100

    Create login and logout methods

---
+++ app/Http/Controllers/api/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($data)) {
+            $user = Auth::user();
+            /** @var \App\Models\User $user * */
+            $token = $user->createToken('auth_token')->accessToken;
+
+            return response()->json(['message' => 'Autenticació amb èxit. Benvingut ', 'name' => $user->name, 'token' => $token], 200);
+        }
+
+        throw new HttpResponseException(response()->json(['message' => __('Email o contrasenya incorrecte')], 401));
+    }
+
+    public function logout(Request $request)
+    {
+        $user = Auth::user();
+
+        $user->tokens()->delete();
+
+        return response()->json(['message' => __('Desconnexió realitzada amb èxit')], 200);
+
+    }
+}
--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\LoginController;
 use App\Http\Controllers\api\StudentController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::post('login', [LoginController::class, 'login'])->name('login');
 Route::post('/students', [StudentController::class, 'store'])->name('student.create');
 Route::get('/students', [StudentController::class, 'index'])->name('students.list');
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout']);
+});
 
 //Route::get('/skins/available', [SkinController::class, 'index'])->name('skins.available');
