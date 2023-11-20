4bbc3e5e133e1df00a1b04574f963baa3c583460
eli-mi98 <elisiavmi@gmail.com>
2023-11-20 12:29:01 +0100

    - Recruiter List resource creation

---
+++ app/Http/Resources/RecruiterListResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RecruiterListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->user->name,
+            'surname' => $this->user->surname,
+            'company' => $this->company,
+            'sector' => $this->sector, 
+        ];
+    }
+}
