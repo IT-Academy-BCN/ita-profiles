dd5fbd5a350cc775ee2f2867df6616fc10c1867d
elpupas <elpupas@users.noreply.github.com>
2023-11-27 11:40:59 +0000

    PHP Linting (Pint)

--- tests/Feature/LoginControllerTest.php
+++ tests/Feature/LoginControllerTest.php
@@ -3,13 +3,10 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class LoginControllerTest extends TestCase
 {
-  
-
     public function setUp(): void
     {
         parent::setUp();
