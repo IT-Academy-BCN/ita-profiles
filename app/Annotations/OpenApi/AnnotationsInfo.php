616ce4de4f3f0cb2a50880e2521619c5b5f0b1e3
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2024-01-31 18:06:39 +0100

    + swagger hotfix

--- app/Annotations/OpenApi/AnnotationsInfo.php
+++ app/Annotations/OpenApi/AnnotationsInfo.php
@@ -8,12 +8,16 @@ namespace App\Annotations\OpenApi;
  *   title="ITA Profiles page API documentation",
  *   description="Documentation needed for the API Rest for the ITA Profiles page to IT Academy. Some useful links below:
  *   [ITA Profles Backend](https://github.com/IT-Academy-BCN/ita-profiles-backend)
- *   [ITA Profiles Frontend](https://github.com/IT-Academy-BCN/ita-profiles-frontend)
- *   To use the endpoints, prefix them with '**api/v1**'. For example: '**POST: https://itaperfils.eurecatacademy.org/api/v1/students'**'."
+ *   [ITA Profiles Frontend](https://github.com/IT-Academy-BCN/ita-profiles-frontend)"
  * )
  *
  *   @OA\Server(
  *     url="http://127.0.0.1:8000"
+ *   )
+ *   @OA\Server(
  *     url="http://127.0.0.1:8000/api/v1"
+ *   )
  */
-class AnnotationsInfo {}
+class AnnotationsInfo
+{
+}
