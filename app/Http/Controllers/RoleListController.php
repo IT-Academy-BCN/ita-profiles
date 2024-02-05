c8fd2bdf6a66b0faf0f44653b03ed318f8e21834
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2024-02-05 18:49:15 +0100

    + Controller fix

--- app/Http/Controllers/RoleListController.php
+++ app/Http/Controllers/RoleListController.php
@@ -8,7 +8,7 @@ class RoleListController extends Controller
 {
     public function getRoleList()
     {
-        $filePath = base_path('database/data/students.json');
+        $filePath = base_path('database/data/role_list.json');
 
         if (File::exists($filePath)) {
             $jsonContent = File::get($filePath);
