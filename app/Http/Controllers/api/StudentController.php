fc547ec84fc9fa5ba811434094e7814160c2a524
Rosa <xenco34@gmail.com>
2024-02-06 21:06:52 +0100

    deleted index method

--- app/Http/Controllers/api/StudentController.php
+++ app/Http/Controllers/api/StudentController.php
@@ -16,26 +16,7 @@ use Illuminate\Support\Str;
 
 class StudentController extends Controller
 {
-    /* public function index()
-    {
-        $studentsList = Student::all();
-
-        if (! $studentsList) {
-            throw new HttpResponseException(response()->json([
-                'message' => __('Alguna cosa ha anat malament.Intenta-ho de nou més tard.')
-            ], 404));
-        } elseif ($studentsList->isEmpty()) {
-            throw new HttpResponseException(response()->json([
-                'message' => __('No hi ha estudiants a la base de dades.')
-            ], 404));
-        }
-        // Transformo la lista de estudiantes usando StudentListResource
-        $studentsList = $studentsList->map(function ($student) {
-            return new StudentListResource($student);
-        });
-
-        return response()->json(['data' => $studentsList], 200);
-    } */
+    
 
     public function store(StoreStudentRequest $request)
     {
