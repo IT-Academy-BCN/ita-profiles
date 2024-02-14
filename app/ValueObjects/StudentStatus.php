ffe98484c51368f74d2ad0b82f318b40023e4421
Jordi <jmdesarrollo82@gmail.com>
2024-02-14 13:16:19 +0100

    WIP

---
+++ app/ValueObjects/StudentStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ValueObjects;
+
+enum StudentStatus: string {
+    case ACTIVE = 'Active';
+    case INACTIVE = 'Inactive';
+    case IN_A_BOOTCAMP = 'In a Bootcamp';
+    case IN_A_JOB = 'In a Job';
+}
