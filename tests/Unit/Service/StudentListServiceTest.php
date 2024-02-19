3f6cde468be3278f594017b0dc2d814f5c2bb9a9
Peterson A Sena <petersonsena@gmail.com>
2024-02-20 00:59:10 +0100

    StudentListService test

---
+++ tests/Unit/Service/StudentListServiceTest.php
@@ -0,0 +1,37 @@
+<?php 
+declare(strict_types = 1);
+
+namespace Service;
+
+use App\Models\Resume;
+use App\Service\StudentListService;
+use Tests\TestCase;
+
+class StudentListServiceTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->resumeUpdateService = new StudentListService();
+    }
+
+    public function tearDown(): void
+    {
+        $this->artisan('migrate:fresh');
+        parent::tearDown();
+    }
+
+    public function testCanInstantiate(): void
+    {
+        self::assertInstanceOf(StudentListService::class, $this->resumeUpdateService);
+    }
+
+    public function testExecuteStudentListService(): void
+    {
+        Resume::factory()->count(10)->create();
+        $data = $this->resumeUpdateService->execute();
+        self::assertCount(10, $data);
+
+    }
+
+}
