09e6192bd264c6432ea4a94bb888a08ed540982f
Peterson A Sena <petersonsena@gmail.com>
2024-02-20 00:54:39 +0100

    StudentListController and service implements

--- app/Http/Controllers/api/StudentListController.php
+++ app/Http/Controllers/api/StudentListController.php
@@ -3,13 +3,30 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Service\StudentListService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StudentListController extends Controller
 {
+
+    private StudentListService $studentListService;
+
+    public function __construct(StudentListService $studentListService)
+    {
+        $this->studentListService = $studentListService;
+
+    }
     public function __invoke()
     {
-        $data = file_get_contents(base_path('database/data/students.json'));
+            try{
+                $data = $this->studentListService->execute();
 
-        return response()->json(json_decode($data, true));
+                return response()->json($data,200);
+            }catch(ModelNotFoundException $resumesNotFoundException){
+                throw new HttpResponseException(response()->json([
+                    'message' => $resumesNotFoundException->getMessage()], $resumesNotFoundException->getCode()));
+            }
+     
     }
 }
