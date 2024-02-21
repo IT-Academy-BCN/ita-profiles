af2e54a10e9ffbfb9001ca990b72274c229269c3
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2024-02-21 12:43:10 +0100

    + swagger doc update

--- app/Http/Controllers/api/SpecializationListController.php
+++ app/Http/Controllers/api/SpecializationListController.php
@@ -9,7 +9,6 @@ class SpecializationListController extends Controller
 {
     public function __invoke()
     {
-        // Retrieve unique specialization codes from the Resume model excluding 'Not Set'
         $specialization_list = Resume::distinct()
             ->where('specialization', '!=', 'Not Set')
             ->pluck('specialization')
