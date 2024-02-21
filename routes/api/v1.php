b4dfd1aeeb3a1e4ac04df1fceb3326cd9f5e242a
Francisco <ciscosolana@gmail.com>
2024-02-21 09:06:43 +0100

    #60/Feature/Move-students-routes-out-off-protected-ones

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -22,16 +22,20 @@ use Illuminate\Support\Facades\Route;
 
 //No Auth
 Route::post('login', [AuthController::class, 'login'])->name('login');
-Route::post('/students', [StudentController::class, 'store'])->name('student.create');
-Route::get('/students', [StudentController::class, 'index'])->name('students.list');
-Route::get('/students/{id}', [StudentController::class, 'show'])->name('student.show');
 
 Route::post('/recruiters', [RecruiterController::class, 'store'])->name('recruiter.create');
 Route::get('/recruiters', [RecruiterController::class, 'index'])->name('recruiter.list');
 Route::get('/recruiters/{id}', [RecruiterController::class, 'show'])->name('recruiter.show');
 
-// Students
+// Students for home
 Route::get('/student/list/for-home', StudentListController::class)->name('profiles.home');
+// Students
+Route::get('/students', [StudentController::class, 'index'])->name('students.list');
+Route::post('/students', [StudentController::class, 'store'])->name('student.create');
+Route::get('/students/{student}', [StudentController::class, 'show'])->name('student.show');
+// TODO: Move the following two routes to Auth routes later
+Route::put('/students/{student}', [StudentController::class, 'update'])->name('student.update');
+Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('student.delete');
 
 // Specialization List Endpoint
 Route::get('/specialization/list', SpecializationListController::class)->name('roles.list');
@@ -41,8 +45,8 @@ Route::post('/admins', [AdminController::class, 'store'])->name('admins.create')
 //Passport Auth with token
 Route::middleware('auth:api')->group(function () {
     //Student
-    Route::put('/students/{id}', [StudentController::class, 'update'])->middleware('can:update.student')->name('student.update');
-    Route::delete('/students/{id}', [StudentController::class, 'destroy'])->middleware('can:delete.student')->name('student.delete');
+    // Route::put('/students/{student}', [StudentController::class, 'update'])->middleware('can:update.student')->name('student.update');
+    // Route::delete('/students/{student}', [StudentController::class, 'destroy'])->middleware('can:delete.student')->name('student.delete');
     //Recruiter
     Route::put('/recruiters/{id}', [RecruiterController::class, 'update'])->name('recruiter.update');
     Route::delete('/recruiters/{id}', [RecruiterController::class, 'destroy'])->name('recruiter.delete');
