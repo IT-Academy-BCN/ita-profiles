44c3569cb801726198ed841a9858dfa94e00eeba
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2024-02-25 13:13:54 +0100

    + 'id' on controller

--- app/Http/Controllers/api/StudentProjectsDetailController.php
+++ app/Http/Controllers/api/StudentProjectsDetailController.php
@@ -15,18 +15,21 @@ class StudentProjectsDetailController extends Controller
 
             'projects' => [
                 [
+                    'project_id' => '1',
                     'project_name' => 'ITA Project 1',
                     'project_site' => 'Acme Corporation',
                     'project_skills' => ['PHP', 'JavaScript', 'HTML', 'CSS'],
                     'project_repository' => 'https://github.com/user/project1'
                 ],
                 [
+                    'project_id' => '2',
                     'project_name' => 'ITA Project 2',
                     'project_site' => 'Globex Industries',
                     'project_skills' => ['Python', 'Django', 'React'],
                     'project_repository' => 'https://github.com/user/project2'
                 ],
                 [
+                    'project_id' => '3',
                     'project_name' => 'ITA Project 3',
                     'project_site' => 'Wayne Enterprises',
                     'project_skills' => ['Java', 'Spring Boot', 'Angular'],
