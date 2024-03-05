ecb17f19de0c149518c1516454a877f97b2937e5
Rosa <xenco34@gmail.com>
2024-03-05 17:50:51 +0100

    Add query to filter results by specialization

--- app/Service/StudentListService.php
+++ app/Service/StudentListService.php
@@ -10,32 +10,38 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class StudentListService
 {
-    public function execute(): array
-    {
 
+
+    public function execute(?string $specialization = null): array
+{
+    if ($specialization) {
+        $resumes = Resume::where('specialization', $specialization )->get();
+    } else {
         $resumes = Resume::all();
-        if($resumes->isEmpty()) {
-            throw new ModelNotFoundException(__('No hi ha resums'), 404);
-        }
-
-        $data = $resumes->map(function ($resume): array {
-            $tagsIds = json_decode($resume->tags_ids, true);
-
-            $randomNumber = rand(1, 3);
-
-            $tags = Tag::whereIn('id', $tagsIds)->get(['id', 'tag_name']);
-            $mappedTags = $tags->map(function ($tag) {
-                return ['id' => $tag->id, 'name' => $tag->tag_name];
-            });
-
-            return [
-                'fullname' => $resume->student->name . " " . $resume->student->surname,
-                'subtitle' => $resume->subtitle,
-                'photo' => asset('/img/stud_' . $randomNumber . '.png'),
-                'tags' => $mappedTags,
-
-            ];
-        });
-        return $data->toArray();
     }
+
+    if($resumes->isEmpty()) {
+        throw new ModelNotFoundException(__('No hi ha resums'), 404);
+    }
+
+    $data = $resumes->map(function ($resume): array {
+        $tagsIds = json_decode($resume->tags_ids, true);
+
+        $randomNumber = rand(1, 3);
+
+        $tags = Tag::whereIn('id', $tagsIds)->get(['id', 'tag_name']);
+        $mappedTags = $tags->map(function ($tag) {
+            return ['id' => $tag->id, 'name' => $tag->tag_name];
+        });
+
+        return [
+            'fullname' => $resume->student->name . " " . $resume->student->surname,
+            'subtitle' => $resume->subtitle,
+            'photo' => asset('/img/stud_' . $randomNumber . '.png'),
+            'tags' => $mappedTags,
+        ];
+    });
+
+    return $data->toArray();
+}
 }
