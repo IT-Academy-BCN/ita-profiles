c08ee67b743190524264022f547e4c13d3038ab6
Rosa <xenco34@gmail.com>
2024-03-06 19:19:37 +0100

    add test

--- tests/Feature/Student/StudentListTest.php
+++ tests/Feature/Student/StudentListTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Student;
 
 use Tests\TestCase;
+use App\Service\StudentListService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class StudentListTest extends TestCase
 {
@@ -38,4 +40,18 @@ class StudentListTest extends TestCase
             ],
         ]);
     }
+
+    public function test_student_list_controller_throws_exception()
+{
+    $this->mock(StudentListService::class, function ($mock) {
+        $mock->shouldReceive('execute')
+            ->andThrow(new ModelNotFoundException('No hi ha resums', 404));
+    });
+
+    $response = $this->getJson(route('profiles.home'));
+
+    $response->assertStatus(404);
+    $response->assertJson(['message' => 'No hi ha resums']);
+}
+
 }
