5de3ff85a0c78301eb1231c56d33b073f55ecc35
Rosa <xenco34@gmail.com>
2024-03-09 20:12:51 +0100

    Refactor: Separated the logic into parts for better understanding of the code

--- app/Service/StudentListService.php
+++ app/Service/StudentListService.php
@@ -7,6 +7,7 @@ namespace App\Service;
 use App\Models\Resume;
 use App\Models\Tag;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Collection;
 
 class StudentListService
 {
@@ -14,34 +15,50 @@ class StudentListService
 
     public function execute(?string $specialization = null): array
 {
+    $resumes = $this->getResumes($specialization);
+
+    return $this->mapResumesToData($resumes);
+}
+
+private function getResumes(?string $specialization): Collection
+{
     if ($specialization) {
-        $resumes = Resume::where('specialization', $specialization )->get();
+        $resumes = Resume::where('specialization', $specialization)->get();
     } else {
         $resumes = Resume::all();
     }
 
-    if($resumes->isEmpty()) {
+    if ($resumes->isEmpty()) {
         throw new ModelNotFoundException(__('No hi ha resums'), 404);
     }
 
-    $data = $resumes->map(function ($resume): array {
-        $tagsIds = json_decode($resume->tags_ids, true);
+    return $resumes;
+}
 
-        $randomNumber = rand(1, 3);
+private function mapResumesToData(Collection $resumes): array
+{
+    return $resumes->map(function ($resume): array {
+        return $this->mapResumeToData($resume);
+    })->toArray();
+}
 
-        $tags = Tag::whereIn('id', $tagsIds)->get(['id', 'tag_name']);
-        $mappedTags = $tags->map(function ($tag) {
-            return ['id' => $tag->id, 'name' => $tag->tag_name];
-        });
+private function mapResumeToData($resume): array
+{
+    return [
+        'fullname' => $resume->student->name . " " . $resume->student->surname,
+        'subtitle' => $resume->subtitle,
+        'photo' => asset('/img/stud_' . rand(1, 3) . '.png'),
+        'tags' => $this->getMappedTags($resume),
+    ];
+}
 
-        return [
-            'fullname' => $resume->student->name . " " . $resume->student->surname,
-            'subtitle' => $resume->subtitle,
-            'photo' => asset('/img/stud_' . $randomNumber . '.png'),
-            'tags' => $mappedTags,
-        ];
-    });
-
-    return $data->toArray();
+private function getMappedTags($resume): array
+{
+    return Tag::whereIn('id', json_decode($resume->tags_ids, true))
+               ->get(['id', 'tag_name'])
+               ->map(function ($tag) {
+                   return ['id' => $tag->id, 'name' => $tag->tag_name];
+               })
+               ->toArray();
 }
 }
