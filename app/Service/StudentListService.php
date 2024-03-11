ed3e14cdf6e9ac5c6af8512466cd0cfcf9301399
Rosa <xenco34@gmail.com>
2024-03-11 14:44:34 +0100

    Updated execute and getResumes methods to receive array to be able to filter on several fields at once

--- app/Service/StudentListService.php
+++ app/Service/StudentListService.php
@@ -12,53 +12,52 @@ use Illuminate\Support\Collection;
 class StudentListService
 {
 
+    public function execute(?array $specializations = null): array
+    {
+        $resumes = $this->getResumes($specializations);
 
-    public function execute(?string $specialization = null): array
-{
-    $resumes = $this->getResumes($specialization);
-
-    return $this->mapResumesToData($resumes);
-}
-
-private function getResumes(?string $specialization): Collection
-{
-    if ($specialization) {
-        $resumes = Resume::where('specialization', $specialization)->get();
-    } else {
-        $resumes = Resume::all();
+        return $this->mapResumesToData($resumes);
     }
 
-    if ($resumes->isEmpty()) {
-        throw new ModelNotFoundException(__('No hi ha resums'), 404);
+    private function getResumes(?array $specializations): Collection
+    {
+        if ($specializations) {
+            $resumes = Resume::whereIn('specialization', $specializations)->get();
+        } else {
+            $resumes = Resume::all();
+        }
+
+        if ($resumes->isEmpty()) {
+            throw new ModelNotFoundException(__('No hi ha resums'), 404);
+        }
+
+        return $resumes;
     }
 
-    return $resumes;
-}
+    private function mapResumesToData(Collection $resumes): array
+    {
+        return $resumes->map(function ($resume): array {
+            return $this->mapResumeToData($resume);
+        })->toArray();
+    }
 
-private function mapResumesToData(Collection $resumes): array
-{
-    return $resumes->map(function ($resume): array {
-        return $this->mapResumeToData($resume);
-    })->toArray();
-}
+    private function mapResumeToData($resume): array
+    {
+        return [
+            'fullname' => $resume->student->name . " " . $resume->student->surname,
+            'subtitle' => $resume->subtitle,
+            'photo' => asset('/img/stud_' . rand(1, 3) . '.png'),
+            'tags' => $this->getMappedTags($resume),
+        ];
+    }
 
-private function mapResumeToData($resume): array
-{
-    return [
-        'fullname' => $resume->student->name . " " . $resume->student->surname,
-        'subtitle' => $resume->subtitle,
-        'photo' => asset('/img/stud_' . rand(1, 3) . '.png'),
-        'tags' => $this->getMappedTags($resume),
-    ];
-}
-
-private function getMappedTags($resume): array
-{
-    return Tag::whereIn('id', json_decode($resume->tags_ids, true))
-               ->get(['id', 'tag_name'])
-               ->map(function ($tag) {
-                   return ['id' => $tag->id, 'name' => $tag->tag_name];
-               })
-               ->toArray();
-}
+    private function getMappedTags($resume): array
+    {
+        return Tag::whereIn('id', json_decode($resume->tags_ids, true))
+            ->get(['id', 'tag_name'])
+            ->map(function ($tag) {
+                return ['id' => $tag->id, 'name' => $tag->tag_name];
+            })
+            ->toArray();
+    }
 }
