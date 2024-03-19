6b2eb5ae2bd234d8935e7ea3a9f62e9ff91150c8
Francisco <ciscosolana@gmail.com>
2024-03-19 17:32:40 +0100

    Feature/New-Project-Model-with-its-factory,migration-and-seeder.
    - Modification of resumes table to add projects field
    - Add Projects to ResumeFactory

---
+++ database/migrations/2024_03_19_135429_add_projects_to_resumes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('resumes', function (Blueprint $table) {
+            $table->json('project_ids')->default(new Expression('(JSON_ARRAY())'))->after('specialization');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('resumes', function (Blueprint $table) {
+            $table->dropColumn('project_ids');
+        });
+    }
+};
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Project::factory(2)->create();
+    }
+}
