003eb4b1c87a3908cd7ce4e59cc8fdb7fa3bb212
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2024-03-21 14:37:03 +0100

    + set up into test file

--- tests/Feature/Student/StudentProjectsDetailControllerTest.php
+++ tests/Feature/Student/StudentProjectsDetailControllerTest.php
@@ -2,24 +2,32 @@
 
 namespace Tests\Feature\Student;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Student;
 use App\Models\Project;
 
 class StudentProjectsDetailControllerTest extends TestCase
 {
+    protected $student;
+    protected $projects;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Create a student
+        $this->student = Student::factory()->create();
+
+        // Create projects associated with the student
+        $this->projects = Project::factory()->count(3)->create();
+        $this->student->resume()->create([
+            'project_ids' => json_encode($this->projects->pluck('id')->toArray())
+        ]);
+    }
+
     public function test_controller_returns_correct_response_with_valid_uuid()
     {
-        $student = Student::factory()->create();
-        $projects = Project::factory()->count(3)->create();
-
-        $student->resume()->create([
-            'project_ids' => json_encode($projects->pluck('id')->toArray())
-        ]);
-
-        $response = $this->get(route('projects.list', ['student' => $student->id]));
+        $response = $this->get(route('projects.list', ['student' => $this->student->id]));
 
         $response->assertStatus(200)
             ->assertJsonStructure([
