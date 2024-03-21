c3e2cd942c29fe3db0f694efb11853b6e4a72873
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2024-03-21 14:29:13 +0100

    + 200 response with ok uuid / test

--- tests/Feature/Student/StudentProjectsDetailControllerTest.php
+++ tests/Feature/Student/StudentProjectsDetailControllerTest.php
@@ -5,16 +5,34 @@ namespace Tests\Feature\Student;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Student;
+use App\Models\Project;
 
 class StudentProjectsDetailControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_controller_returns_correct_response_with_valid_uuid()
     {
-        $response = $this->get('/');
+        $student = Student::factory()->create();
+        $projects = Project::factory()->count(3)->create();
 
-        $response->assertStatus(200);
+        $student->resume()->create([
+            'project_ids' => json_encode($projects->pluck('id')->toArray())
+        ]);
+
+        $response = $this->get(route('projects.list', ['student' => $student->id]));
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'projects' => [
+                    '*' => [
+                        'uuid',
+                        'project_name',
+                        'company_name',
+                        'project_url',
+                        'tags',
+                        'github_url'
+                    ]
+                ]
+            ]);
     }
 }
