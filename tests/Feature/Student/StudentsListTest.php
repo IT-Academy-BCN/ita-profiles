60854a2401f34b33a3240d08971375b5bddd2373
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-03-26 15:29:03 +0100

    Fixed namespaces of test files

--- tests/Feature/Student/StudentsListTest.php
+++ tests/Feature/Student/StudentsListTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Student;
 
 use Tests\TestCase;
 
--- tests/Feature/TagControllerTest.php
+++ tests/Feature/TagControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Api;
+namespace Tests\Feature;
 
 use App\Models\Tag;
 use App\Models\User;
