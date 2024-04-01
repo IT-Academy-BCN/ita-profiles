7c90dda2883c6de176a019d5361b671253260a97
JuanH941214 <juanhernandez941214@gmail.com>
2024-04-01 14:14:40 +0200

    created test for get specialization list

---
+++ tests/Feature/SpecializationListTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class SpecializationListTest extends TestCase
+{
+    public function testGetSpecializationList(){
+
+        $response = $this->getJson(route('roles.list'));
+        $response->assertStatus(200);
+        $specialization_list= $response->json();
+        $this->assertIsArray($specialization_list);
+        foreach($specialization_list as $specialization){
+            $this->assertIsString($specialization);
+        }
+       
+    }
+
+
+}
+
+
+
+?>
