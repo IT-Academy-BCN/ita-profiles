bd0e782f345a8abdcf11bccc269c806e0d150cf6
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2024-04-09 14:15:09 +0200

    + additional trainging / model / migration / factory / seeder

---
+++ app/Models/AdditionalTraining.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class AdditionalTraining extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    protected $guarded = ['id'];
+
+}
---
+++ database/factories/AdditionalTrainingFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AdditionalTraining;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AdditionalTrainingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AdditionalTraining::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $currentYear = date('Y');
+        
+        $beginningYear = $this->faker->numberBetween(2000, $currentYear - 1);
+
+        $endingYear = $this->faker->numberBetween($beginningYear, $currentYear);
+
+        return [
+            'course_name' => $this->faker->sentence(3),
+            'study_center' => $this->faker->company,
+            'course_beggining_year' => $beginningYear,
+            'course_ending_year' => $endingYear,
+            'duration_hrs' => $this->faker->numberBetween(60, 500),
+        ];
+    }
+}
