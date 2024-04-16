e9356e52436d894c6913ec19e7aac7c70518f293
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-04-16 19:55:26 +0200

    ModalityService updated
    To handle the 404 error, the throwing of the generic exception has been replaced with the ModelNotFoundException exception

--- app/Service/Student/ModalityService.php
+++ app/Service/Student/ModalityService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Service\Student;;
 
 use App\Models\Resume;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ModalityService
 {
@@ -14,7 +15,8 @@ class ModalityService
         $modality = $this->getModalityByStudentId($studentId);
 
         if (!$modality) {
-            throw new \Exception('No se encontró el currículum del usuario', 404);
+            throw new ModelNotFoundException('No se encontró el currículum del usuario', 404);
+            //throw new \Exception('No se encontró el currículum del usuario', 404);
         }
 
         return $modality;
