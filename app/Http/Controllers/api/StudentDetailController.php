da7d98e15f6beacde8b514a12e509c814da9142f
JuanH941214 <juanhernandez941214@gmail.com>
2024-04-17 18:17:54 +0200

    tests created

--- app/Http/Controllers/api/StudentDetailController.php
+++ app/Http/Controllers/api/StudentDetailController.php
@@ -6,18 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Models\Resume;
 use App\Service\StudentAboutService;
 use Illuminate\Http\Request;
-use Symfony\Polyfill\Intl\Idn\Idn;
+use Illuminate\Http\JsonResponse;
 
 class StudentDetailController extends Controller
 {
-    function __invoke(Request $request,$student_id){
+    function __invoke(Request $request,$student_id):JsonResponse{
 
         $studentDetails= Resume::where('student_id',$student_id)->get();
 
         if ($studentDetails->isEmpty()) {
             return response()->json(['error' => 'No se encontró ningún estudiante con el ID especificado'], 404);
         }
-        else{   
+        else{  
             return response()->json($studentDetails, 200);
         }
     }     
--- tests/Feature/Student/StudentDetailControllerTest.php
+++ tests/Feature/Student/StudentDetailControllerTest.php
@@ -15,38 +15,33 @@ class StudentDetailControllerTest extends TestCase
     {
         $student = Resume::factory()->create();
 
-        $response = $this->get('/api/student/' . $student->id);
+        $response = $this->get(route('student.detail', ['id' => $student->student_id]));
 
         $response->assertStatus(200);
-        $response->assertJson($student->toArray());
+        $response->assertJsonStructure([
+            '*' => [ 
+                'id',
+                'student_id',
+                'subtitle',
+                'linkedin_url',
+                'github_url',
+                'tags_ids',
+                'specialization',
+                'project_ids',
+                'created_at',
+                'updated_at',
+                'about'
+            ]    
+       
+        ]);
     }
 
     public function test_student_details_not_found()
     {
-        $response = $this->get('/api/student/1');
+        $response = $this->get(route('student.detail', ['id' => '12345']));
 
         $response->assertStatus(404);
         $response->assertJson(['error' => 'No se encontró ningún estudiante con el ID especificado']);
     }
 
-    public function test_student_details_with_multiple_resumes()
-    {
-        $student = Resume::factory()->count(3)->create(['student_id' => 1]);
-
-        $response = $this->get('/api/student/1');
-
-        $response->assertStatus(200);
-        $response->assertJson($student->toArray());
-    }
-
-    public function test_student_details_with_no_resumes()
-    {
-        $student = Resume::factory()->create(['student_id' => 1]);
-        Resume::where('student_id', 1)->delete();
-
-        $response = $this->get('/api/student/1');
-
-        $response->assertStatus(404);
-        $response->assertJson(['error' => 'No se encontró ningún estudiante con el ID especificado']);
-    }
 }
