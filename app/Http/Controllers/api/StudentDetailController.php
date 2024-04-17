ffe9736400dbd5897e8fa19288a6762f113d5290
JuanH941214 <juanhernandez941214@gmail.com>
2024-04-17 09:41:18 +0200

    get about service

--- app/Http/Controllers/api/StudentDetailController.php
+++ app/Http/Controllers/api/StudentDetailController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Service\StudentAboutService;
+use Illuminate\Http\Request;
+
 
 class StudentDetailController extends Controller
 {
@@ -12,4 +15,23 @@ class StudentDetailController extends Controller
 
         return response()->json(json_decode($data, true));
     }
+
+
+        protected $studentAboutService;
+    
+        public function __construct(StudentAboutService $studentAboutService)
+        {
+            $this->studentAboutService = $studentAboutService;
+        }
+    
+        public function getAbout(Request $request, $student_id)
+        {
+            try {
+                $about = $this->studentAboutService->getAboutByStudentId($student_id);
+                return response()->json(['about' => $about], 200);
+            } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+                return response()->json(['message' => 'Estudiante no encontrado'], 404);
+            }
+        }
+    
 }
