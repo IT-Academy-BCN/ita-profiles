819b9e6c1922857c442a7a7e856c4f21f2be95c1
JuanH941214 <juanhernandez941214@gmail.com>
2024-04-17 12:49:54 +0200

    student detail controller real data

--- app/Http/Controllers/api/StudentDetailController.php
+++ app/Http/Controllers/api/StudentDetailController.php
@@ -3,35 +3,24 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Resume;
 use App\Service\StudentAboutService;
 use Illuminate\Http\Request;
-
+use Symfony\Polyfill\Intl\Idn\Idn;
 
 class StudentDetailController extends Controller
 {
-    public function __invoke()
-    {
-        $data = file_get_contents(base_path('database/data/studentDetail.json'));
-
-        return response()->json(json_decode($data, true));
+    function getDetailsByStudentId(Request $request,$id){
+        if(!$id){
+            return response()->json(['error' => 'ID de estudiante no válido'], 400);
+        }
+        else{
+            $studentDetails= Resume::where('student_id',$id)->get();
+            return response()->json($studentDetails, 200);
+        }
+    
     }
 
-
-        protected $studentAboutService;
     
-        public function __construct(StudentAboutService $studentAboutService)
-        {
-            $this->studentAboutService = $studentAboutService;
-        }
-    
-        public function getAbout(Request $request, $student_id)
-        {
-            try {
-                $about = $this->studentAboutService->getAboutByStudentId($student_id);
-                return response()->json(['about' => $about], 200);
-            } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
-                return response()->json(['message' => 'Estudiante no encontrado'], 404);
-            }
-        }
     
 }
