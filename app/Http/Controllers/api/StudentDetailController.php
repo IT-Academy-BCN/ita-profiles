b50dcc7d46b8a043029d9b20650059bea7363823
JuanH941214 <juanhernandez941214@gmail.com>
2024-04-17 13:32:05 +0200

    added response for errors

--- app/Http/Controllers/api/StudentDetailController.php
+++ app/Http/Controllers/api/StudentDetailController.php
@@ -10,17 +10,20 @@ use Symfony\Polyfill\Intl\Idn\Idn;
 
 class StudentDetailController extends Controller
 {
-    function __invoke(Request $request,$id){
-        if(!$id){
+    function __invoke(Request $request,$student_id){
+        if(!$student_id){
             return response()->json(['error' => 'ID de estudiante no válido'], 400);
         }
         else{
-            $studentDetails= Resume::where('student_id',$id)->get();
+            $studentDetails= Resume::where('student_id',$student_id)->get();
             return response()->json($studentDetails, 200);
+
+            if ($studentDetails->isEmpty()) {
+                return response()->json(['error' => 'No se encontró ningún estudiante con el ID especificado'], 404);
+            }
+
         }
-    
-    }
 
     
-    
+    }     
 }
