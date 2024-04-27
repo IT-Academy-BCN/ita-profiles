bb5c6b9441eb0ca9dd960d4fa7155ad3f8802bd8
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-04-27 12:18:44 +0200

    Documentation

--- app/Annotations/OpenApi/controllersAnnotations/studentLanguagesDetailAnnotations/AnnotationsStudentLanguagesDetail.php
+++ app/Annotations/OpenApi/controllersAnnotations/studentLanguagesDetailAnnotations/AnnotationsStudentLanguagesDetail.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Annotations\OpenApi\controllersAnnotations\studentLanguagesDetailAnnotations;
 
 use OpenApi\Annotations as OA;
@@ -11,8 +13,8 @@ class AnnotationsStudentLanguagesDetail
      *      path="/students/{uuid}/languages",
      *      operationId="getStudentLanguages",
      *      tags={"Languages"},
-     *      summary="Get a detailed list of languages and level for a student",
-     *      description="Returns a list of Languages and level for a specific student. (Note: This endpoint returns hardcoded project details)",
+     *      summary="Get a detailed list of languages and their respective level from a specific student's resume",
+     *      description="Returns a detailed list of languages and their respective level from a specific student's resume",
      *      @OA\Parameter(
      *          name="uuid",
      *          description="Student UUID",
@@ -27,31 +29,52 @@ class AnnotationsStudentLanguagesDetail
      *          response=200,
      *          description="Successful operation",
      *          @OA\JsonContent(
-     *              type="array",
-     *              @OA\Items(
-     *                  type="object",
-     *                  @OA\Property(
-     *                      property="language_id",
-     *                      type="int",
-     *                      description="id for languange"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="language_name",
-     *                      type="string",
-     *                      description="Language"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="language_level",
-     *                      type="string",
-     *                      description="Language level"
+     *              type="object",
+     *              @OA\Property(
+     *                  property="languages",
+     *                  type="array",
+     *                  @OA\Items(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="language_id",
+     *                          type="string",
+     *                          description="Language UUID",
+     *                          example="e6b4432b-d2f8-4e06-b727-6ecaf40e6e0e"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="language_name",
+     *                          type="string",
+     *                          description="Language name",
+     *                          example="Anglès"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="language_level",
+     *                          type="string",
+     *                          description="Language level",
+     *                          example="Bàsic"
+     *                      ),
      *                  )
      *              )
      *          )
      *      ),
-     *      @OA\Response(
-     *          response=404,
-     *          description="Student not found"
-     *      )
+     *     @OA\Response(
+        *     response=404,
+        *     description="Language not found or Student not found or Resume not found",
+        *     @OA\JsonContent(
+        *         type="object",
+        *         @OA\Property(property="message", type="string", example="L'estudiant amb ID: {studentId} no té informat cap idioma al seu currículum"),
+        *         @OA\Property(property="message2", type="string", example="No s'ha trobat cap estudiant amb aquest ID {studentId}"),
+        *         @OA\Property(property="message3", type="string", example="No s'ha trobat cap currículum per a l'estudiant amb id: {studentId}")
+        *     )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Server error",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="string", example="Hi ha hagut un error")
+     *         )
+     *     )
      * )
      */
 
