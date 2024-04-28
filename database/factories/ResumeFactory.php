981ebdb261e1838b9587e8eea03776b3ef357fe9
Rosa <xenco34@gmail.com>
2024-04-28 16:54:26 +0200

    add declare(strict_types=1); & delete comments

--- database/factories/ResumeFactory.php
+++ database/factories/ResumeFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use App\Models\Student;
@@ -7,16 +9,8 @@ use App\Models\AdditionalTraining;
 use App\Models\Project;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Resume>
- */
 class ResumeFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
 
     public const  SUBTITLES = [
         "Ingeniero de Software",
