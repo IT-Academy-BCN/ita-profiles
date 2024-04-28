44cbe40f0bb68954e31dd97957109b94d5545fc3
Rosa <xenco34@gmail.com>
2024-04-28 16:47:04 +0200

    Add declare(strict_types=1);
    Delete: $table->timestamps(); from companies & projects table

--- database/migrations/2024_03_19_135429_add_projects_to_resumes_table.php
+++ database/migrations/2024_03_19_135429_add_projects_to_resumes_table.php
@@ -1,14 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::table('resumes', function (Blueprint $table) {
@@ -16,9 +16,6 @@ return new class () extends Migration {
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('resumes', function (Blueprint $table) {
