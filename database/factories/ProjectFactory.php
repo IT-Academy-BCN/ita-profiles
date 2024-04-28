1f5f1272ae3fd9c41b535236adfe7aa287984bba
Rosa <xenco34@gmail.com>
2024-04-28 17:01:38 +0200

    Refactor Added 'declare(strict_types=1)'. Added 'use App\Models\Tag;' to import the Tag model and use it in the code. Removed the tag data array in the Project factory, as tags are now retrieved from the tags table. Removed unnecessary comments.

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -1,37 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Company;
+use App\Models\Tag;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
- */
 class ProjectFactory extends Factory
 {
-    protected $tagNames = [
-        "PHP", "Laravel", "Javascript",
-        "React","Vue","HTML&CSS",
-        "MongoDB","SQL","Tailwind",
-        "Bootstrap", "JQuery", "Angular",
-        "Nodejs","Express", "Java",
-        "Python", "C","C++",
-        "C#","Ruby","Spring Boot",
-        "Ruby on Rails","Django","Redis",
-        "Git","GitHub",
-    ];
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    
+    private function getTagObjects(): array
+    {
+        $tagIds = Tag::inRandomOrder()->limit($this->faker->numberBetween(1, 10))->pluck('id')->toArray();
+        
+        return $tagIds;
+    }
+
     public function definition(): array
     {
         return [
             'name' => $this->faker->company,
             'company_id' => Company::inRandomOrder()->first()->id ?? Company::factory()->create()->id,
-            'tags' => json_encode($this->faker->randomElements($this->tagNames, 10)),
+            'tags' => json_encode($this->getTagObjects()),
             'github_url' => $this->faker->parse('https://github.com/') . $this->faker->userName,
             'project_url' => $this->faker->url,
         ];
