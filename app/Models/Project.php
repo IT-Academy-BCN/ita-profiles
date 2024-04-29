415795bae5c151dfd0c747e8246e59863dab8a67
Rosa <xenco34@gmail.com>
2024-04-29 19:11:09 +0200

    Files returned to App\Models

---
+++ app/Models/Project.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    protected $guarded = ['id'];
+    public $timestamps = false;
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
