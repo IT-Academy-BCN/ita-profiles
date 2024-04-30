ca646e6bc73645bc097f65d4d5953b4253ac4a49
CarlesRuizDeveloper <crmfeina@gmail.com>
2024-04-30 12:08:53 +0200

    Swager its working

--- app/Annotations/OpenApi/controllersAnnotations/studentAnnotations/AnnotationsStudentDetail.php
+++ app/Annotations/OpenApi/controllersAnnotations/studentAnnotations/AnnotationsStudentDetail.php
@@ -9,7 +9,7 @@ class AnnotationsStudentDetail
      *
      * @OA\Get (
      *     path="/student/{id}/detail/for-home",
-     *     operationId="getStudentDetails",
+     *     operationId="getStudentDetailsabout",
      *     tags={"Student"},
      *     summary="Get details of a Student.",
      *     description="Get the details of a specific student. Authentication is not required.",
@@ -21,8 +21,8 @@ class AnnotationsStudentDetail
      *          required=true,
      *
      *          @OA\Schema(
-     *              type="integer",
-     *              format="int64"
+     *              type="string"
+     *              
      *          ),
      *      ),
      *
@@ -41,14 +41,13 @@ class AnnotationsStudentDetail
      *
      *                     @OA\Property(property="name", type="string", example="John"),
      *                     @OA\Property(property="surname",type="string", example="Doe"),
-     *                     @OA\Property(property="subtitle", type="string", example="Engineer and Developer."),
-     *                     @OA\Property(property="about", type="string", example="Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
+     *                     @OA\Property(property="subtitle", type="string", example="Engineer and Developer."),     *                     
      *                     @OA\Property(property="cv", type="string", example="My currículum."),
      *                     @OA\Property(property="bootcamp", type="string", example="PHP Developer"),
      *                     @OA\Property(property="end_date", type="date", example="..." ),
      *                     @OA\Property(property="linkedin", type="string", example="http://www.linkedin.com"),
      *                     @OA\Property(property="github", type="string", example="http://www.github.com"),
-     *                     @OA\Property(property="about", type="text", example="Todo sobre mi y muchas cosas más."),
+     *                     @OA\Property(property="about", type="text", example="Tot sobre mi i alguna cosa més"),
      *                 )
      *             )
      *         )
