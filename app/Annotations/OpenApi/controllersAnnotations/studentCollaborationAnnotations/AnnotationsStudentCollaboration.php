1cd4e2ee50aa4ac659c961bfdd1244687f10f24b
AndreaLN25 <andrealn14@gmail.com>
2024-05-07 12:51:19 +0200

    Improvements based on the feedback received during PR reviews.

--- app/Annotations/OpenApi/controllersAnnotations/studentCollaborationAnnotations/AnnotationsStudentCollaboration.php
+++ app/Annotations/OpenApi/controllersAnnotations/studentCollaborationAnnotations/AnnotationsStudentCollaboration.php
@@ -27,17 +27,59 @@ class AnnotationsStudentCollaboration
  *         response=200,
  *         description="Successful. Collaboration list retrieved",
  *         @OA\JsonContent(
+ *         type="object",
+ *         @OA\Property(
+ *             property="collaborations",
  *             type="array",
- *                 @OA\Items(
- *                     type="object",
- *                     @OA\Property(property="uuid", type="string", format="uuid"),
- *                     @OA\Property(property="collaboration_name", type="string"),
- *                     @OA\Property(property="collaboration_description", type="text"),
- *                     @OA\Property(property="collaboration_quantity", type="integer"),
- *                 )
+ *             @OA\Items(
+ *                 type="object",
+ *                 @OA\Property(
+ *                     property="uuid",
+ *                     type="string",
+ *                     description="Collaboration UUID",
+ *                     example="e6b4432b-d2f8-4e06-b727-6ecaf40e6e0e"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="collaboration_name",
+ *                     type="string",
+ *                     description="Name of the collaboration",
+ *                     example="Project X"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="collaboration_description",
+ *                     type="string",
+ *                     description="Description of the collaboration",
+ *                     example="A collaborative project on topic Y"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="collaboration_quantity",
+ *                     type="integer",
+ *                     description="Quantity of the collaboration",
+ *                     example=3
+ *                 ),
  *             )
  *         )
+ *     ),
+ *
+ *      @OA\Response(
+ *         response=404,
+ *         description="Collaborations not found or Student not found",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(property="message", type="string", example="Collaborations not found for the student with ID: {studentId}"),
+ *             @OA\Property(property="message2", type="string", example="No student found with ID: {studentId}")
+ *         )
+ *     ),
+ *
+ *     @OA\Response(
+ *         response=500,
+ *         description="Server error",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(property="error", type="string", example="There was a server error")
+ *         )
  *     )
+ *
  * )
  */
 
