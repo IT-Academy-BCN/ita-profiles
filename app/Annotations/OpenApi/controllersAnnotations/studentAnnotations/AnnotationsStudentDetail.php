44acbb575a61336161513ddec15971b273a2e114
AndreaLN25 <andrealn14@gmail.com>
2024-05-08 13:12:57 +0200

    Same docs. Pushed again due to develop changes.

--- app/Annotations/OpenApi/controllersAnnotations/studentAnnotations/AnnotationsStudentDetail.php
+++ app/Annotations/OpenApi/controllersAnnotations/studentAnnotations/AnnotationsStudentDetail.php
@@ -5,66 +5,67 @@ namespace App\Annotations\OpenApi\controllersAnnotations\studentAnnotations;
 class AnnotationsStudentDetail
 {
     /**
-     * @OA\Get(
-     *     path="/student/detail/for-home",
-     *     operationId="getStudentDetail",
+     * @OA\Get (
+     *     path="/student/{id}/detail/for-home",
+     *     operationId="getStudentDetailsabout",
      *     tags={"Student"},
      *     summary="Get Student Detail.",
-     *     description="Get student detail registered. No authentication required",
-     *
+     *     description="Retrieve details of a specific student. No authentication required.",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Student ID",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
      *     @OA\Response(
      *         response=200,
-     *         description="",
-     *
+     *         description="Success. Returns the student details.",
      *         @OA\JsonContent(
      *             @OA\Property(
-     *                 type="array",
      *                 property="data",
+     *                 type="array",
      *                 @OA\Items(
-     *                     type="object",
-     *                     @OA\Property(property="student_id", type="integer", example=1),
+     *                     @OA\Property(property="fullname", type="string", example="Katrine Wyman Jacobson"),
+     *                     @OA\Property(property="subtitle", type="string", example="Full Stack developer en PHP"),
      *                     @OA\Property(
-     *                         property="profile_detail",
+     *                         property="social_media",
      *                         type="object",
-     *                         @OA\Property(property="fullname", type="string", example="Juan Pérez"),
-     *                         @OA\Property(property="subtitle", type="string", example="Desarrollador Frontend"),
      *                         @OA\Property(
-     *                             property="social_media",
+     *                             property="github",
      *                             type="object",
-     *                             @OA\Property(
-     *                                 property="linkedin",
-     *                                 type="object",
-     *                                 @OA\Property(property="url", type="string", example="https://es.linkedin.com/"),
-     *                             ),
-     *                             @OA\Property(
-     *                                 property="github",
-     *                                 type="object",
-     *                                 @OA\Property(property="url", type="string", example="https://github.com/"),
-     *                             )
+     *                             @OA\Property(property="url", type="string", example="https://github.com/bettie52")
      *                         ),
      *                         @OA\Property(
-     *                             property="about",
+     *                             property="linkedin",
      *                             type="object",
-     *                             @OA\Property(property="description", type="string", example="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a euismod elit. Nunc elit ante, vulputate sed maximus eu, posuere eu nibh. In eget lacus in mi pharetra volutpat a a justo. Fusce aliquet nibh nec accumsan facilisis. Suspendisse in tempor nibh, eu fermentum velit. Suspendisse cursus ultricies ipsum, eget tincidunt arcu pretium laoreet. Pellentesque eget egestas erat. Donec dapibus pharetra ultrices. Vivamus mollis sapien sed laoreet interdum."
-     *                              )
-     *                          ),
-     *                             @OA\Property(property="tags",type="array",@OA\Items(ref="#/components/schemas/Tag"))
+     *                             @OA\Property(property="url", type="string", example="https://linkedin.com/abernathy.dayne")
      *                         )
-     *
-     *                  )
-     *              )
-     *          )
-     *      ),
-     *
-     *
-     *      @OA\Schema(
-     *           schema="Tag",
-     *           type="object",
-     *           @OA\Property(property="id", type="integer", example=1),
-     *           @OA\Property(property="name", type="string", example="JavaScript"),
-     *           example={"id": 1, "name": "JavaScript"}
-     *      )
+     *                     ),
+     *                     @OA\Property(property="about", type="string", example="Iusto aut debitis soluta facere tempore quisquam. Vel assumenda aliquid quod et eum quos ex. Ipsa ea tempora minima occaecati. Culpa occaecati quod laboriosam reiciendis quia consequuntur."),
+     *                     @OA\Property(
+     *                         property="tags",
+     *                         type="array",
+     *                         @OA\Items(
+     *                             @OA\Property(property="id", type="integer", example=4),
+     *                             @OA\Property(property="name", type="string", example="HTML&CSS")
+     *                         )
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="No hem trobat cap estudiant amb aquest ID"
+     *     ),
+     *     @OA\Response(
+     *         response=500,            
+     *         description="Error inesperat"
+     *     )
      * )
      */
-    public function __invoke() {}
+    public function show() {}
 }
--- database/factories/ResumeFactory.php
+++ database/factories/ResumeFactory.php
@@ -36,6 +36,7 @@ class ResumeFactory extends Factory
                 ['Frontend', 'Backend', 'Fullstack', 'Data Science', 'Not Set'],
             ),
             'project_ids' => json_encode($projectIds),
+            'about' => $this->faker->paragraph,
             'modality' => $this->faker->randomElements(['Presencial', 'Híbrid', 'Remot'], rand(1, 3)),
             'additional_trainings_ids' => json_encode($additionalTrainingsIds),
             'collaborations_ids' => json_encode($collaborationsIds),
