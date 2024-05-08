81199d35ffdedbb327c4e74c9b9f6f3e2d2ce775
Jordi Morillo <jmdesarrollo82@gmail.com>
2024-05-08 16:21:14 +0200

    fix: add missing parenthesis

--- app/Annotations/OpenApi/controllersAnnotations/studentCollaborationAnnotations/AnnotationsStudentCollaboration.php
+++ app/Annotations/OpenApi/controllersAnnotations/studentCollaborationAnnotations/AnnotationsStudentCollaboration.php
@@ -4,84 +4,85 @@ namespace App\Annotations\OpenApi\controllersAnnotations\studentCollaborationAnn
 
 class AnnotationsStudentCollaboration
 {
-/**
- * @OA\Get(
- *     path="/students/{uuid}/collaborations",
- *     operationId="getStudentCollaborations",
- *     summary="Retrieve a list of collaborations",
- *     tags={"Collaboration"},
- *
- *
- *          @OA\Parameter(
- *          name="uuid",
- *          description="Student UUID",
- *          required=true,
- *          in="path",
- *          @OA\Schema(
- *              type="string",
- *              format="uuid"
- *          )
- *      ),
- *
- *     @OA\Response(
- *         response=200,
- *         description="Successful. Collaboration list retrieved",
- *         @OA\JsonContent(
- *         type="object",
- *         @OA\Property(
- *             property="collaborations",
- *             type="array",
- *             @OA\Items(
- *                 type="object",
- *                 @OA\Property(
- *                     property="uuid",
- *                     type="string",
- *                     description="Collaboration UUID",
- *                     example="e6b4432b-d2f8-4e06-b727-6ecaf40e6e0e"
- *                 ),
- *                 @OA\Property(
- *                     property="collaboration_name",
- *                     type="string",
- *                     description="Name of the collaboration",
- *                     example="Project X"
- *                 ),
- *                 @OA\Property(
- *                     property="collaboration_description",
- *                     type="string",
- *                     description="Description of the collaboration",
- *                     example="A collaborative project on topic Y"
- *                 ),
- *                 @OA\Property(
- *                     property="collaboration_quantity",
- *                     type="integer",
- *                     description="Quantity of the collaboration",
- *                     example=3
- *                 ),
- *             )
- *         )
- *     ),
- *
- *      @OA\Response(
- *         response=404,
- *         description="Collaborations not found or Student not found",
- *         @OA\JsonContent(
- *             type="object",
- *             @OA\Property(property="message", type="string", example="Collaborations not found for the student with ID: {studentId}"),
- *             @OA\Property(property="message2", type="string", example="No student found with ID: {studentId}")
- *         )
- *     ),
- *
- *     @OA\Response(
- *         response=500,
- *         description="Server error",
- *         @OA\JsonContent(
- *             type="object",
- *             @OA\Property(property="error", type="string", example="There was a server error")
- *         )
- *     )
- *
- * )
- */
-
-    public function __invoke(){}
+    /**
+     * @OA\Get(
+     *     path="/students/{uuid}/collaborations",
+     *     operationId="getStudentCollaborations",
+     *     summary="Retrieve a list of collaborations",
+     *     tags={"Collaboration"},
+     *
+     *
+     *     @OA\Parameter(
+     *         name="uuid",
+     *         description="Student UUID",
+     *         required=true,
+     *         in="path",
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *
+     *      @OA\Response(
+     *         response=200,
+     *         description="Successful. Collaboration list retrieved",
+     *         @OA\JsonContent(
+     *         type="object",
+     *             @OA\Property(
+     *                 property="collaborations",
+     *                 type="array",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(
+     *                         property="uuid",
+     *                         type="string",
+     *                         description="Collaboration UUID",
+     *                         example="e6b4432b-d2f8-4e06-b727-6ecaf40e6e0e"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="collaboration_name",
+     *                         type="string",
+     *                         description="Name of the collaboration",
+     *                         example="Project X"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="collaboration_description",
+     *                         type="string",
+     *                         description="Description of the collaboration",
+     *                         example="A collaborative project on topic Y"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="collaboration_quantity",
+     *                         type="integer",
+     *                         description="Quantity of the collaboration",
+     *                         example=3
+     *                     ),
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=404,
+     *         description="Collaborations not found or Student not found",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string", example="Collaborations not found for the student with ID: {studentId}"),
+     *             @OA\Property(property="message2", type="string", example="No student found with ID: {studentId}")
+     *         )
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=500,
+     *         description="Server error",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="string", example="There was a server error")
+     *         )
+     *     )
+     * )
+     */
+    public function __invoke()
+    {
+    }
 }
