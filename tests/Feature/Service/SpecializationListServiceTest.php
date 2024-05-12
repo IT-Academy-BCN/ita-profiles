ef61cbc4de0b4f45b3f51f58fd9e5414a2960173
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-05-12 22:20:53 +0200

    Correction of the first method in the SpecializationListServiceTest
    This commit corrects the first method of the SpecializationListServiceTest, which mistakenly had the content of the second method.

--- tests/Feature/Service/SpecializationListServiceTest.php
+++ tests/Feature/Service/SpecializationListServiceTest.php
@@ -27,9 +27,9 @@ class SpecializationListServiceTest extends TestCase
     {
         Resume::query()->delete();
 
-        $specialization = 'Not Set';
-    
-        for ($i = 0; $i < 3; $i++) {
+        $specializations = ['Frontend', 'Backend', 'Fullstack', 'Data Science', 'Not Set'];
+        
+        foreach ($specializations as $specialization) {
             ResumeFactory::new()->specificSpecialization($specialization)->create();
         }
 
@@ -38,10 +38,12 @@ class SpecializationListServiceTest extends TestCase
         $response->assertStatus(200);
 
         $specialization_list= $response->json();
-
-        $this->assertIsArray($specialization_list);
         
-        $this->assertEquals([], $specialization_list);
+        $this->assertCount(4, $specialization_list);
+        $this->assertContains('Frontend', $specialization_list);
+        $this->assertContains('Backend', $specialization_list);
+        $this->assertContains('Fullstack', $specialization_list);
+        $this->assertContains('Data Science', $specialization_list);
     }
 
     public function testSpecializationListServiceReturnsAVoidArrayForExistingResumesWithSpecializationFieldWithNotSetValue(): void
