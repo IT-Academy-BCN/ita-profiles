e94e9cf210ad515fa76e0bdaa4695ecef9f098e3
AndreaLN25 <andrealn14@gmail.com>
2024-05-14 11:20:54 +0200

    Swagger Doc, not working still

---
+++ app/Annotations/OpenApi/controllersAnnotations/registerUserAnnotations/AnnotationsRegisterUser.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Annotations\OpenApi\ControllersAnnotations\RegisterUserAnnotations;
+
+class AnnotationsRegisterUser
+{
+    /**
+     * @OA\Post(
+     *      path="/register",
+     *      operationId="registerUser",
+     *      summary="Register a new user",
+     *      tags={"User"},
+     *
+     *      @OA\RequestBody(
+     *
+     *           @OA\JsonContent(
+     *
+     *              type="object",
+     *              @OA\Property(property="username", type="string", example="user1"),
+     *              @OA\Property(property="dni", type="string", example="12345678A"),
+     *              @OA\Property(property="email", type="string", format="email", example="user1@user.com"),
+     *              @OA\Property(property="password", type="string", example="123456789"),
+     *              @OA\Property(property="password_confirmation", type="string", example="123456789"),
+     *          )
+     *      ),
+     *
+     *       @OA\Response(
+     *          response=201,
+     *          description="User registered successfully",
+     *
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="token", type="string", example="enGJ56Bvbhb56fCVJftbGciOiJSUzI1NiJ9"),
+     *              @OA\Property(property="email", type="string", example="user1@user.com"),
+     *          )
+     *      ),
+     *
+     *
+     *      @OA\Response(
+     *          response=422,
+     *          description="Validation error",
+     *
+     *          @OA\JsonContent(
+     *
+     *              @OA\Property(property="message", type="string", example="The given data was invalid."),
+     *              @OA\Property(property="errors", type="object",
+     *              )
+     *          )
+     *       ),
+     *
+     *
+     *      @OA\Response(
+     *         response=404,
+     *         description="Register was not succesful.Please try it again later."
+     *      ),
+     *
+     *
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="string", example="Internal server error"),
+     *         )
+     *     )
+     */
+
+
+}
