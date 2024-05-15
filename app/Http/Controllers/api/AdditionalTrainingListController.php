d608ac070c782f3e046ae05fe546ea9f436b0530
Ric-De <ricdellazari@gmail.com>
2024-05-15 03:38:58 +0200

    Refactor AdditionalTrainingList-Erase commented code in 2 files

--- app/Http/Controllers/api/AdditionalTrainingListController.php
+++ app/Http/Controllers/api/AdditionalTrainingListController.php
@@ -1,36 +1,5 @@
 <?php
 
-/* declare(strict_types=1);
-
-namespace App\Http\Controllers\api;
-use App\Http\Controllers\Controller;
-use App\Service\AdditionalTrainingService;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Exception;
-class AdditionalTrainingListController extends Controller
-{
-    protected $additionalTrainingService;
-
-    public function __construct(AdditionalTrainingService $additionalTrainingService)
-    {
-        $this->additionalTrainingService = $additionalTrainingService;
-    }
-
-    public function __invoke($uuid)
-    {
-        try {
-            $additionalTrainingDetail = [
-                'additional_trainings' => $this->additionalTrainingService->getAdditionalTrainingDetails($uuid),
-            ];
-            return response()->json($additionalTrainingDetail);
-        } catch (ModelNotFoundException $e) {
-            return response()->json(['error' => 'Student not found.'], 404);
-        } catch (Exception $e) {
-            return response()->json(['error' => 'Internal server error.'], 500);
-        }
-    }
-} */
-
 declare(strict_types=1);
 
 namespace App\Http\Controllers\api;
--- app/Service/AdditionalTrainingService.php
+++ app/Service/AdditionalTrainingService.php
@@ -1,34 +1,5 @@
 <?php 
 
-/* declare(strict_types=1);
-
-namespace App\Service;
-
-use App\Models\Student;
-use App\Models\AdditionalTraining;
-
-class AdditionalTrainingService
-{
-    public function getAdditionalTrainingDetails($uuid)
-    {
-        $student = Student::where('id', $uuid)->with('resume')->firstOrFail();
-        $resume = $student->resume;
-        $additionalTrainingIds = json_decode($resume->additional_trainings_ids);
-        $additionalTrainings = AdditionalTraining::findMany($additionalTrainingIds);
-
-        return $additionalTrainings->map(function ($additionalTraining) {
-            return [
-                'uuid' => $additionalTraining->id,
-                'course_name' => $additionalTraining->course_name,
-                'study_center' => $additionalTraining->study_center,
-                'course_beginning_year' => $additionalTraining->course_beginning_year,
-                'course_ending_year' => $additionalTraining->course_ending_year,
-                'duration_hrs' => $additionalTraining->duration_hrs,
-            ];
-        });
-    }
-} */
-
 declare(strict_types=1);
 
 namespace App\Service;
