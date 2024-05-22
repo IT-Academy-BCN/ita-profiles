2db22d60394ec133f1cbd1c5a8400fb25b637f53
Jordi Morillo <jmdesarrollo82@gmail.com>
2024-05-22 13:19:59 +0200

    fix: replace docker build routes

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
-docker build -t my-laravel-api:$BRANCH_NAME -f backend/Dockerfile backend/
+docker build -t my-laravel-api:$BRANCH_NAME -f Dockerfile ./
 docker run -d -p 80:8000 --name laravel_api_$BRANCH_NAME my-laravel-api:$BRANCH_NAME
 
 # Esperar a que el contenedor esté listo
