495787b05c99ab18c60186ece8300ac25d29cba1
Jordi Morillo <jmdesarrollo82@gmail.com>
2024-05-22 13:16:41 +0200

    feature: add a script that will raise up the containers during the pull request

---
+++ scripts/start_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+docker build -t my-laravel-api:$BRANCH_NAME -f backend/Dockerfile backend/
+docker run -d -p 80:8000 --name laravel_api_$BRANCH_NAME my-laravel-api:$BRANCH_NAME
+
+# Esperar a que el contenedor esté listo
+sleep 10
