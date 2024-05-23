7e571b94a31f7822a0cfa8f557aeab3551b4334c
Diegdar <diegdarch@hotmail.com>
2024-05-23 17:34:36 +0200

    Tests have been added to RegisterUserServiceTest to instantiate and create id's for User, Student and Resume

--- tests/Feature/Service/User/RegisterUserServiceTest.php
+++ tests/Feature/Service/User/RegisterUserServiceTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use App\Http\Requests\RegisterRequest;
+use App\Models\Resume;
+use App\Models\Student;
 use Service\User\UserRegisterService;
 
 class RegisterUserServiceTest extends TestCase
@@ -30,6 +33,55 @@ class RegisterUserServiceTest extends TestCase
             'specialization' => 'Backend',
             'password_confirmation' => 'Password%123',
         ];
+    }
+
+    public function test_user_can_be_instantiated(): void
+    {
+        $user = new User();
+        $this->assertInstanceOf(User::class, $user);
+    }
+
+    public function test_student_can_be_instantiated(): void
+    {
+        $student = new Student();
+        $this->assertInstanceOf(Student::class, $student);
+    }
+
+    public function test_resume_can_be_instantiated(): void
+    {
+        $resume = new Resume();
+
+        $this->assertInstanceOf(Resume::class, $resume);
+    }
+
+    public function test_user_uuid_is_generated_when_user_is_created(): void
+    {
+        $userData = $this->createUserData();
+        $user = User::create($userData);
+
+        $this->assertNotEmpty($user->id);
+    }
+
+    public function test_student_uuid_is_generated_when_user_is_created(): void
+    {
+        $userData = $this->createUserData();
+        $user = User::create($userData);
+        $student = Student::create(['user_id' => $user->id]);
+
+        $this->assertNotEmpty($student->id);
+    }
+
+    public function test_resume_uuid_is_generated_when_user_is_created(): void
+    {
+        $userData = $this->createUserData();
+        $user = User::create($userData);
+        $student = Student::create(['user_id' => $user->id]);
+        $resume = new Resume;
+        $resume->student_id = $student->id;
+        $resume->specialization = $userData['specialization'] ?? null;
+        $resume->save();
+
+        $this->assertNotEmpty($resume->id);
     }
 
     public function test_user_creation_with_valid_data(): void
@@ -82,7 +134,7 @@ class RegisterUserServiceTest extends TestCase
      *
      * @param array $array Data array with user information to test.
      * @param bool $resultCorrect Expected result of the test (true if user creation should succeed, false otherwise).
-     */    
+     */
     public function test_required_fields_for_user_creation(array $array, bool $resultCorrect): void
     {
         $success = $this->userService->createUser($array);
