7c71977ab13a7b35ebbf2cfbd11f18e1e3947717
Agonpas <agonpas@gmail.com>
2024-05-23 20:29:42 +0200

    Swagger update with AnnotationsTagDetail.php added

--- app/Annotations/OpenApi/controllersAnnotations/tagAnnotations/AnnotationsTag.php
+++ app/Annotations/OpenApi/controllersAnnotations/tagAnnotations/AnnotationsTag.php
@@ -46,56 +46,6 @@ class AnnotationsTag
     public function store() {}
 
     /**
-     * @OA\Get(
-     *      path="/tags/{id}",
-     *      operationId="getTagById",
-     *      tags={"Tags"},
-     *      summary="Get details of a specific tag",
-     *      description="Retrieve details of a specific tag based on the provided ID.",
-     *
-     *
-     *      @OA\Parameter(
-     *          name="id",
-     *          in="path",
-     *          required=true,
-     *          description="Tag ID",
-     *
-     *          @OA\Schema(type="integer", format="int64"),
-     *      ),
-     *
-     *     @OA\Response(
-     *         response=200,
-     *         description="Tag details retrieved successfully.",
-     *
-     *         @OA\JsonContent(
-     *
-     *             type="object",
-     *
-     *           @OA\Property(
-     *           property="data",
-     *           type="object",
-     *                 @OA\Property(property="id", type="integer", example=1),
-     *                 @OA\Property(property="tag_name", type="string", example="Laravel"),
-     *                 @OA\Property(property="created_at", type="string", format="date-time", example="2024-01-25T12:34:56Z"),
-     *                 @OA\Property(property="updated_at", type="string", format="date-time", example="2024-01-25T12:34:56Z"),
-     *             )
-     *         )
-     *     ),
-     *
-     *     @OA\Response(
-     *         response=404,
-     *         description="Tag not found.",
-     *
-     *         @OA\JsonContent(
-     *
-     *             @OA\Property(property="message", type="string", example="Tag not found."),
-     *         ),
-     *     ),
-     *   )
-     */
-    public function show() {}
-
-    /**
      * @OA\Put(
      *     path="/tags/{id}",
      *     operationId="updateTag",
---
+++ app/Annotations/OpenApi/controllersAnnotations/tagAnnotations/AnnotationsTagDetail.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Annotations\OpenApi\controllersAnnotations\tagAnnotations;
+
+use OpenApi\Annotations as OA;
+
+class AnnotationsTagDetail
+{ 
+    /**
+    * @OA\Get(
+    *      path="/tags/{tagId}",
+    *      operationId="getTagDetailById",
+    *      tags={"Tags"},
+    *      summary="Get details of a specific tag",
+    *      description="Retrieve details of a specific tag based on the provided ID.",
+    *
+    *
+    *      @OA\Parameter(
+    *          name="tagId",
+    *          in="path",
+    *          required=true,
+    *          description="Tag ID",
+    *
+    *          @OA\Schema(type="integer", format="int64"),
+    *      ),
+    *
+    *     @OA\Response(
+    *         response=200,
+    *         description="Tag details retrieved successfully.",
+    *
+    *         @OA\JsonContent(
+    *
+    *             type="object",
+    *
+    *           @OA\Property(
+    *           property="data",
+    *           type="object",
+    *                 @OA\Property(property="id", type="integer", example=1),
+    *                 @OA\Property(property="tag_name", type="string", example="Laravel"),
+    *                 @OA\Property(property="created_at", type="string", format="date-time", example="2024-01-25T12:34:56Z"),
+    *                 @OA\Property(property="updated_at", type="string", format="date-time", example="2024-01-25T12:34:56Z"),
+    *             )
+    *         )
+    *     ),
+    *
+    *     @OA\Response(
+    *         response=404,
+    *         description="Tag not found.",
+    *
+    *         @OA\JsonContent(
+    *
+    *             @OA\Property(property="message", type="string", example="Tag not found with id {tagId}."),
+    *         ),
+    *     ),
+    *   )
+    */
+   public function __invoke() {}
+}
