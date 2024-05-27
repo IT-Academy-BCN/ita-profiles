c780f9b32a5cc57018804a03728432de3710903a
Jordi Morillo <jmdesarrollo82@gmail.com>
2024-05-27 19:01:59 +0200

    feature: raise PR in local

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+npm install
+npm run dev
+
 /usr/local/bin/wait-for-it.sh mysql:3306 --timeout=120 --strict -- php artisan migrate:fresh
 /usr/local/bin/wait-for-it.sh mysql:3306 --timeout=120 --strict -- php artisan db:seed
 php artisan cache:clear
 php artisan config:clear
 php artisan config:cache
 php artisan serve --host=0.0.0.0 --port=8000
-npm install
-npm run dev
