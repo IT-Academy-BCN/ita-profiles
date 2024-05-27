9bd4db5b3c994a28efe334f62f98f1a0065c73f2
Dar-ya218 <dasharoura13465@gmail.com>
2024-05-28 00:35:10 +0200

    add endpoint ditail

--- src/components/studentProfile/StudentProfile.tsx
+++ src/components/studentProfile/StudentProfile.tsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import StudentProfileDetail from './StudentProfileDetail';
-import CompletedSteps from './CompletedSteps';
 import MenuNavbar from '../menuNavbar/MenuNavbar';
 import UserNavbar from '../userNavBar/UserNavbar';
+import StudentProfileDetail from './StudentProfileDetail';
+import CompletedSteps from './CompletedSteps';
+import { StudentContext } from '../../context/StudentContext';
 
+const StudentProfile: React.FC = () => {
+  const { studentData } = useContext(StudentContext);
 
+  if (!studentData) {
+    return <div>Loading...</div>;
+  }
 
-const StudentProfile: React.FC = () => (
-  
-
-  <div className="flex">
-    <MenuNavbar />
-    <div className="flex w-full md:w-[calc(100%-176px)] flex-col p-2.5 md:p-2 md:pb-8 md:pr-14">
-      <UserNavbar />
-      <div className="flex h-[90vh] w-full justify-between items-center rounded-xl bg-white px-28 pt-20 mt-3">
-        <CompletedSteps />
-        <StudentProfileDetail />
+  return (
+    <div className="flex">
+      <MenuNavbar />
+      <div className="flex w-full md:w-[calc(100%-176px)] flex-col p-2.5 md:p-2 md:pb-8 md:pr-14">
+        <UserNavbar />
+        <div className="flex h-[90vh] w-full justify-between items-center rounded-xl bg-white px-28 pt-20 mt-3">
+          <CompletedSteps/> 
+          <StudentProfileDetail />
+        </div>
+        <Link to="/">
+          <p className='absolute bottom-10 left-10'>Back to Home</p>
+        </Link>
       </div>
-      <Link to="/">
-        <p className='absolute bottom-10 left-10'>Back to Home</p>
-      </Link>
-
-      
     </div>
-  </div>
-);
+  );
+};
 
 export default StudentProfile;
