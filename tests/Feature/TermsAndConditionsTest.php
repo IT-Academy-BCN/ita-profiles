83a34e10d1bf522626298718dc3736fccb3803b6
CarlesRuizDeveloper <crmfeina@gmail.com>
2024-05-28 12:05:20 +0200

    Test returns LoremIpsum

---
+++ tests/Feature/TermsAndConditionsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TermsAndConditionsTest extends TestCase
+{
+    /**
+     * Test return LoremIpsum.
+     */
+
+    public function testTermsEndpointReturnsLoremIpsum()
+{
+    $response = $this->get('/login/terms-and-conditions');
+
+    $response->assertStatus(200);
+    $response->assertJson(['content' => 'Lorem ipsum dolor sit amet...']); 
+}
+
+
+}
