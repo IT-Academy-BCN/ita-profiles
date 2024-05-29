1101473220ccbee48fa5bdd61eef483d6bfdd884
Fabrizio Contu <fab.contu@gmail>
2024-05-29 09:27:13 +0200

    Edit Profile Modal creation

---
+++ src/components/studentDetailCards/studentDataSection/EditProfileModal.tsx
@@ -0,0 +1,12 @@
+const EditProfileModal = () => {
+    return
+    ;<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="w-120 relative flex flex-col items-center rounded-lg bg-white py-8 ps-10 px-5">
+            <h2 className="text-lg self-start font-bold md:text-2xl">
+                Editar Datos
+            </h2>
+        </div>{' '}
+    </div>
+}
+
+export default EditProfileModal
