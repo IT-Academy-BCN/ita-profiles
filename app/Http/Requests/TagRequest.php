4ea1666e5427f5e40f4f3907db65a5e2de4b64da
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-06-02 12:55:00 +0200

    Added failedValidation method to TagRequest class

--- app/Http/Requests/TagRequest.php
+++ app/Http/Requests/TagRequest.php
@@ -3,26 +3,31 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class TagRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'tag_name' => 'required|string|max:75|unique:tags',
         ];
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        $errors = $validator->errors();
+        $response = response()->json([
+            'message' => __('La validació ha fallat.'),
+            'errors' => $errors,
+        ], 422);
+
+        throw new HttpResponseException($response);
     }
 }
