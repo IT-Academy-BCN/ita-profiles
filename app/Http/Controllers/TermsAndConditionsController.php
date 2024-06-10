c9ecd37b82e03368e74cca5e9dfed7854aad8d11
CarlesRuizDeveloper <crmfeina@gmail.com>
2024-06-10 08:06:40 +0200

    create service and change controller

--- app/Http/Controllers/TermsAndConditionsController.php
+++ app/Http/Controllers/TermsAndConditionsController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\TermsAndConditionsService;
+use Exception;
 
 class TermsAndConditionsController extends Controller
 {
+    protected $termsAndConditionsService;
+
+    public function __construct(TermsAndConditionsService $termsAndConditionsService)
+    {
+        $this->termsAndConditionsService = $termsAndConditionsService;
+    }
+
     public function getTermsAndConditions()
     {
-        $termsAndConditions = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi veniam voluptates aperiam laborum est necessitatibus repellendus inventore quis nemo beatae odio, reiciendis quaerat laboriosam harum rerum ab veritatis tempore optio.';
-
-        return response()->json(['content' => $termsAndConditions]);
+        try {
+            $termsAndConditions = $this->termsAndConditionsService->getTermsAndConditions();
+            return response()->json(['content' => $termsAndConditions], 200);
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Hem tingut un error amb els termes i condicions', 'message' => $e->getMessage()], 500);
+        }
     }
-    
 }
+
---
+++ app/Service/TermsAndConditionsSercide.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class TermsAndConditionsService
+{
+    public function getTermsAndConditions()
+    {
+        return 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi veniam voluptates aperiam laborum est necessitatibus repellendus inventore quis nemo beatae odio, reiciendis quaerat laboriosam harum rerum ab veritatis tempore optio.';
+    }
+}
