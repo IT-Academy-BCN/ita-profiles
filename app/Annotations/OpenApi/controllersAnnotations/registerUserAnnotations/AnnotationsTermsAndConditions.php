e230740eea4ccaecddad8315fbeeb2a45097d686
CarlesRuizDeveloper <crmfeina@gmail.com>
2024-06-12 12:22:57 +0200

    creating swagger

---
+++ app/Annotations/OpenApi/controllersAnnotations/registerUserAnnotations/AnnotationsTermsAndConditions.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Annotations\OpenApi\controllersAnnotations\studentResumeAnnotations;
+
+class AnnotationsTermsAndConditions
+{
+    /**
+     * @OA\Get(
+     *     path="/register",
+     *     operationId="getTermsAndConditions",
+     *     tags={"UserRegisterTerms"},
+     *     summary="Get terms and conditions.",
+     *      
+     *
+     *    
+
+       * )
+     */
+    public function __invoke() {}
+}
