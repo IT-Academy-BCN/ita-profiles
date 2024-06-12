4e48d3665a3b15f6e08f6af6c32fa498d0e0abf8
CarlesRuizDeveloper <crmfeina@gmail.com>
2024-06-12 12:39:31 +0200

    Swagger documentation

--- app/Annotations/OpenApi/controllersAnnotations/registerUserAnnotations/AnnotationsTermsAndConditions.php
+++ app/Annotations/OpenApi/controllersAnnotations/registerUserAnnotations/AnnotationsTermsAndConditions.php
@@ -3,18 +3,47 @@
 namespace App\Annotations\OpenApi\controllersAnnotations\studentResumeAnnotations;
 
 class AnnotationsTermsAndConditions
-{
+{   
     /**
      * @OA\Get(
-     *     path="/register",
-     *     operationId="getTermsAndConditions",
-     *     tags={"UserRegisterTerms"},
-     *     summary="Get terms and conditions.",
-     *      
-     *
-     *    
-
-       * )
+     *     path="/register/terms-and-conditions",
+     *     tags={"Terms and Conditions"},
+     *     summary="Get terms and conditions",
+     *     description="Get register terms and conditions .",
+     * 
+     * 
+     *     @OA\Response(
+     *         response=200,
+     *         description="Términos y condiciones obtenidos correctamente",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="content",
+     *                 type="string",
+     *                 description="El contenido de los términos y condiciones"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Error en el servidor",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="error",
+     *                 type="string",
+     *                 description="Mensaje de error"
+     *             ),
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 description="Detalles del error"
+     *             )
+     *         )
+     *     )
+     * )
      */
+
+
     public function __invoke() {}
 }
