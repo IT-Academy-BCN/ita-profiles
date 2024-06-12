6d985b7a99535cb00a26a3135b270498f4c608fa
Jordi Morillo <jmdesarrollo82@gmail.com>
2024-06-12 13:17:00 +0200

    fix: app key not found during testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,24 +7,18 @@ fi
 
 # Ejecuta las instrucciones de Composer y Artisan
 composer install
-composer clear-cache
-composer dump-autoload
-php artisan cache:clear
-php artisan config:clear
+chmod 777 -R storage
+cp .env.docker .env
+
 php artisan optimize
 php artisan clear-compiled
-php artisan key:generate
-php artisan config:cache
-chmod 777 -R storage
-
 php artisan migrate:fresh --seed
 php artisan l5-swagger:generate
-cp .env.docker .env
+php artisan key:generate
+php artisan passport:install --force --no-interaction
 php artisan config:clear
 php artisan config:cache
 php artisan cache:clear
-php artisan key:generate
-php artisan passport:install --force --no-interaction
 
 # Ejecuta el comando recibido como argumento del entrypoint
 exec "$@"
