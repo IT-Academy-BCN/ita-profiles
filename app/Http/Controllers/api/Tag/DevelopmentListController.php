b4ffbce04236de17f28a8440e7b816c44116abf8
Agonpas <agonpas@gmail.com>
2024-06-16 12:46:11 +0200

    Strict types added 	modified:   app/Http/Controllers/api/Tag/DevelopmentListController.php 	modified:   app/Http/Requests/Tag/TagStoreRequest.php 	modified:   app/Http/Requests/Tag/TagUpdateRequest.php

--- app/Http/Controllers/api/Tag/DevelopmentListController.php
+++ app/Http/Controllers/api/Tag/DevelopmentListController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\api\Tag;
 
 use App\Http\Controllers\Controller;
--- app/Http/Requests/Tag/TagUpdateRequest.php
+++ app/Http/Requests/Tag/TagUpdateRequest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Requests\Tag;
 
 use App\Models\Tag;
