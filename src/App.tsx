e958cc8114228c3e3f7734abd1c057c94b749dc2
aida-rodi <aida.rodi.1997@gmail.com>
2024-06-28 16:43:32 +0200

    feature/ Added react router to 'App.tsx'

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import SmallScreenProvider from './context/SmallScreenContext'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import './styles/App.css'
 
 const App = () => (
   <SmallScreenProvider>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        
+        <Route
+          path='/'
+          element={<Home />}
+          >
+        </Route>
+
+        {/* <Route
+          path='/profile'
+          element={<Profile />}
+          >
+        </Route> */}
+
+      </Routes>
+    </BrowserRouter>    
   </SmallScreenProvider>
 )
 
