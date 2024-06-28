976d4b85ddaca9cecd884d51104aa74a38b9780d
aida-rodi <aida.rodi.1997@gmail.com>
2024-06-28 17:16:12 +0200

    Feature/ Added content to StudentProfilePage and made it accessible with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import SmallScreenProvider from './context/SmallScreenContext'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import StudentProfilePage from './pages/StudentProfilePage'
 import './styles/App.css'
 
 const App = () => (
   <SmallScreenProvider>
     <BrowserRouter>
       <Routes>
-        
+
         <Route
           path='/'
           element={<Home />}
           >
         </Route>
 
-        {/* <Route
+        <Route
           path='/profile'
-          element={<Profile />}
+          element={<StudentProfilePage />}
           >
-        </Route> */}
+        </Route>
 
       </Routes>
     </BrowserRouter>    
