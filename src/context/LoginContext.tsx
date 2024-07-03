b852d85d358d22941615626765b8afa04dce0870
aida-rodi <aida.rodi.1997@gmail.com>
2024-07-03 11:51:13 +0200

    feature/ applied User interface to login context

--- src/context/LoginContext.tsx
+++ src/context/LoginContext.tsx
@@ -1,8 +1,8 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 
 interface LoginContextProps {
-  token2: string | null;
-  login: (token: string) => void;
+  /* token2: string | null; */
+  login: (user: {id: string, token: string}) => void;
   logout: () => void;
   isLoggedIn: boolean;
 }
@@ -10,34 +10,32 @@ interface LoginContextProps {
 const LoginContext = createContext<LoginContextProps | undefined>(undefined);
 
 const LoginProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [token2, setToken] = useState<string | null>(null);
+  /* const [token2, setToken] = useState<string | null>(null); */
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
-  const login = (token: string) => {
-    console.log(token2);
-    setToken(token);
-    localStorage.setItem('token', token);
+  const login = (user: {id: string, token: string}) => {
+    /* setToken(user.token); */
+    localStorage.setItem('token', user.token);
     setIsLoggedIn(true);
     console.log("isLoggedIn after login:", isLoggedIn);
-    console.log(token2);
   };
 
   const logout = () => {
     localStorage.removeItem('token');
-    setToken(null);
+    /* setToken(null); */
     setIsLoggedIn(false);
     console.log("isLoggedIn after logout:", isLoggedIn);
-    console.log("token after logout:",token2);
+    /* console.log("token after logout:",token2); */
   };
 
-  useEffect(() => {
+  /* useEffect(() => {
     const storedToken = localStorage.getItem('token');
     setIsLoggedIn(Boolean(storedToken)); 
-  }, []);
+  }, []); */
 
 
   return (
-    <LoginContext.Provider value={{ token2, login, logout, isLoggedIn }}>
+    <LoginContext.Provider value={{ /* token2, */ login, logout, isLoggedIn }}>
       {children}
     </LoginContext.Provider>
   );
