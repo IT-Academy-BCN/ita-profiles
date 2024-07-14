62c1fc94a8efabdae068dad9bb3d0ff2b4f2c2a3
begoIT24 <blopezprof@gmail.com>
2024-07-14 20:03:06 +0200

    adding service and request

---
+++ app/Http/Requests/AddStudentLanguageRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AddStudentLanguageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'language_id' => 'required|uuid|exists:languages,id'
+        ];
+    }
+
+    /**
+     * If validator fails returns the exception in json form
+     *
+     * @return array
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors()], 422));
+    }
+}
