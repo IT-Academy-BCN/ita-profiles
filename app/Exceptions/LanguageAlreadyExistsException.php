a1e1a55fc35e899282705f82c94f0ced798dd1f2
begoIT24 <blopezprof@gmail.com>
2024-07-14 20:27:24 +0200

    new personalized exceptions

---
+++ app/Exceptions/LanguageAlreadyExistsException.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use Exception;
+use Throwable;
+
+class LanguageAlreadyExistsException extends Exception
+{
+    public const MESSAGE = 'L\'idioma %s ja existeix al perfil de l\'estudiant %s';
+
+    public function __construct($languageId, $studentId, $code = 404, Throwable $previous = null)
+    {
+        $message = sprintf(self::MESSAGE, $languageId, $studentId);
+        parent::__construct($message, $code, $previous);
+    }
+}
