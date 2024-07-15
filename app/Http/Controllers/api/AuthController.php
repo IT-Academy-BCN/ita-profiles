8852fd73663922b303be9d09de7386d39e738b5a
spider <spider@webdev.webdev>
2024-07-15 12:21:21 +0200

    Controller Auth Refactored

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -12,6 +12,10 @@ use App\Service\User\UserService;
 use App\DTO\UserDTO;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use App\Exceptions\UserNotFoundException;
+use App\Exceptions\UserNotFoundInRedisException;
+use App\Exceptions\UserNotStoredInRedisException;
+use App\Exceptions\CouldNotCreateJWTokenPassportException;
+
 
 use App\Models\User;
 
@@ -23,7 +27,6 @@ class AuthController extends Controller
 	{
 		$this->userService = $userService;
 	}
-
 	
 	public function signin(SigninRequest $request): JsonResponse
 	{
@@ -33,112 +36,38 @@ class AuthController extends Controller
 			$user = $this->userService->getUserByDNI($request->dni);
 
 			// Pass the user object to checkUserCredentials
-			if ($this->userService->checkUserCredentials($user, $request['password'])) {
+			if ($this->userService->checkUserCredentials($user, $request['password']))
+			{
 				// Generate token using Laravel Passport.
 				$token = $this->userService->generateJWToken($user);
 				// Store user ID and token in Redis
-				$stored = $this->userService->storeUserIDAndTokenRedis($user->id, $token);
-
-				if ($stored) {
-					$userDTO = new UserDTO(
-						(string)$user->id,
-						$token
-					);
-					// Return success response
-					return response()->json($userDTO, 200);
-				} else {
-					// Handle error in storing token
-					throw new HttpResponseException(response()->json([
-						'message' => __('Failed to store session information. Please try again.')
-					], 500));
-				}
-			} else {
-				// Return error response for invalid credentials
-				throw new HttpResponseException(response()->json([
-					'message' => __('Wrong User Identication or Password.')
-				], 401));
-			}
-		
-		} catch (UserNotFoundException $e) {
-            return response()->json(['message' => $e->getMessage()],$e->getCode() ?:  500);
-        } catch (Exception $e) {
-            return response()->json(['message' => $e->getMessage()], $e->getCode() ?: 500);
-        }
-		
-		
-	}
-	
-	
-	/*
-	public function signin(SigninRequest $request): JsonResponse
-	{
-		// Get user object by DNI
-		$user = $this->userService->getUserByDNI($request->dni);
-
-		// Pass the user object to checkUserCredentials
-		if ($user && $this->userService->checkUserCredentials($user, $request['password'])) {
-			// Generate token using Laravel Passport.
-			$token = $this->userService->generateJWToken($user);
-			// Store user ID and token in Redis
-			$stored = $this->userService->storeUserIDAndTokenRedis($user->id, $token);
-
-			if ($stored) {
+				$this->userService->storeUserIDAndTokenRedis($user->id, $token);
+				//Create UserDTO to return it as a JSON Response
 				$userDTO = new UserDTO(
 					(string)$user->id,
 					$token
 				);
 				// Return success response
 				return response()->json($userDTO, 200);
-			} else {
-				// Handle error in storing token
+
+			}else{
+				// Return error response for invalid credentials
 				throw new HttpResponseException(response()->json([
-					'message' => __('Failed to store session information. Please try again.')
-				], 500));
+					'message' => ('Wrong User Identication or Password.')
+				], 401));
 			}
-		} else {
-			// Return error response for invalid credentials
-			throw new HttpResponseException(response()->json([
-				'message' => __('Wrong User Identication or Password.')
-			], 401));
+		
 		}
-
-
-		// 	if($this->userService->checkUserCredentials($request->dni, $request->password) == True)
-		// 	{
-
-		// 		$userID = $this->userService->getUserIDByDNI($request->dni);
-
-		// 		if(empty($userID) == False){
-		// 			$jwt = $this->userService->generateJWToken($userID);
-		// 			$storedBool = $this->userService->storeUserIDAndTokenRedis($userID, $jwt);
-		// 			if($storedBool == True){
-		// 				$userDTO = new UserDTO(
-		// 					(string)$userID,
-		// 					$jwt
-		// 				);
-
-		// 				return response()->json($userDTO, 200);
-
-		// 			}else{
-		// 				throw new HttpResponseException(response()->json([
-		// 				'message' => __(
-		// 					'Something went wrong...'
-		// 				)], 401));	
-		// 			}
-		// 		}else{
-		// 			throw new HttpResponseException(response()->json([
-		// 				'message' => __(
-		// 					'Wrong User Identication or Password'
-		// 				)], 401));
-		// 		}
-
-		// 	}else{
-
-		// 		throw new HttpResponseException(response()->json([
-		// 			'message' => __(
-		// 				'Wrong User Identication or Password'
-		// 			)], 401));
-		// 	}
-
-	}*/
+		catch(UserNotFoundException | UserNotFoundInRedisException | UserNotStoredInRedisException | CouldNotCreateJWTokenPassportException $e)
+		{
+            return response()->json(['message' => $e->getMessage()], $e->getCode() ?:  500);
+        
+        }
+        catch(Exception $e)
+        {
+            return response()->json(['message' => $e->getMessage()], $e->getCode() ?: 500);
+        }
+			
+	}
+	
 }
