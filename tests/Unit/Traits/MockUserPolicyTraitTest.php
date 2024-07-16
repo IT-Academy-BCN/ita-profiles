56fbebfcbcf8a5bcf7235bfd870ab97cde4759e6
spider <spider@webdev.webdev>
2024-07-16 10:24:03 +0200

    Test Trait Mock Policy Almost Done

--- tests/Unit/Traits/MockUserPolicyTraitTest.php
+++ tests/Unit/Traits/MockUserPolicyTraitTest.php
@@ -9,6 +9,8 @@ use Illuminate\Testing\TestResponse;
 use Mockery;
 use Laravel\Passport\Client;
 use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 use App\Http\Middleware\EnsureStudentOwner;
 use App\Models\Resume;
@@ -39,9 +41,10 @@ class MockUserPolicyTraitTest extends TestCase
      *
      * @return void
      */
-	public function testMockEnsureStudentOwnerMiddlewareSuccess(): void
-    {
+	//public function testMockEnsureStudentOwnerMiddlewareSuccess(): void
+    //{
         
+       /*
         Route::get('/test-policy', function () {
 			$user = User::factory()->create();
 			$user_2 = User::factory()->create();
@@ -51,6 +54,8 @@ class MockUserPolicyTraitTest extends TestCase
 			//$this->beginMockUserPolicy();
 			$result = $policy->canAccessResource($user, $user_2);
 			//$user->canAccessResource($user, $user_2);
+			 // Use the 'canAccessResource' policy method to authorize the request
+            $this->authorize('canAccessResource', $userProfile);
 			echo "called\n";
 			echo $result."\n";
 			var_dump($result);
@@ -62,12 +67,43 @@ class MockUserPolicyTraitTest extends TestCase
 			}	
 				
            
-        });
+        });*/
         
+        
+    public function testMockEnsureStudentOwnerMiddlewareSuccess(): void
+    {
+        // Define a custom route for testing
+        $user = User::factory()->create();
+		$user_2 = User::factory()->create();
+		$user->save();
+		$user_2->save();
+        
+        $this->app['router']->get('/test-policy', function () {
+            // Fetch the authenticated user
+			$user = Auth::user();
+			$user_2 = User::factory()->create();
+			$user_2->save();
+            //$this->authorize('canAccessResource', $user);
+            //return 'Allowed';
+            
+            // Perform authorization check
+			if (Gate::allows('canAccessResource', $user_2)) {
+				return response('Allowed', 200);
+			}
+			
+			return response('Forbidden', 403);
+            
+            
+            //return response()->json(['message' => 'Authorized']);
+        });
 
-        $response = $this->get('/test-policy');
+        $response = $this->actingAs($user)->get('/test-policy');
         $response->assertStatus(200);
-        $response->assertSee('Allowed');
+        //$response->assertSee('Allowed');
+        //var_dump($response);
+        //echo var_dump($response) . "\n";
+        //$response->assertOk();
+        //$this->assertEquals('Allowed', $response);
   
     }
 	
