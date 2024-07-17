1bc1b3f55691b83b40a495cbf48476ff6136ff56
Nacho Albiol <nacho.albiol88@gmail.com>
2024-07-17 10:25:36 +0200

    Added Swagger annotations for Token Authentication

--- app/Annotations/OpenApi/InfoAnnotation.php
+++ app/Annotations/OpenApi/InfoAnnotation.php
@@ -6,15 +6,24 @@ namespace App\Annotations\OpenApi;
  * @OA\Info(
  *   version="1.0.0",
  *   title="ITA Profiles page API documentation",
- *   description="
-
-    API Rest documentation used on ITA Profiles WEB. Some useful links below:
- *   [ITA Profles Backend Repository](https://github.com/IT-Academy-BCN/ita-profiles-backend)
- *   [ITA Profiles Frontend Repository](https://github.com/IT-Academy-BCN/ita-profiles-frontend)
- *   [ITA Profiles WEB](https://ornate-dieffenbachia-e0ff84.netlify.app)"
+ *   description="API Rest documentation used on ITA Profiles WEB. Some useful links below:
+ *     [ITA Profiles Backend Repository](https://github.com/IT-Academy-BCN/ita-profiles-backend)
+ *     [ITA Profiles Frontend Repository](https://github.com/IT-Academy-BCN/ita-profiles-frontend)
+ *     [ITA Profiles WEB](https://ornate-dieffenbachia-e0ff84.netlify.app)"
  * )
- *   @OA\Server(
- *     url= L5_SWAGGER_CONST_HOST
- *       )
+ * @OA\Server(
+ *   url=L5_SWAGGER_CONST_HOST
+ * )
+ * @OA\SecurityScheme(
+ *   type="http",
+ *   description="Bearer token authentication",
+ *   name="Bearer",
+ *   in="header",
+ *   scheme="bearer",
+ *   bearerFormat="JWT",
+ *   securityScheme="bearerAuth"
+ * )
  */
-class InfoAnnotation {}
+class InfoAnnotation
+{
+}
