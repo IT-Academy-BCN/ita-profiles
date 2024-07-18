3b7893d690d3ad8d7843d7bc0bdfca31bf97d50e
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-07-18 11:24:09 +0200

    Gracias Nacho!

--- app/Http/Controllers/api/Student/UpdateStudentLanguagesController.php
+++ app/Http/Controllers/api/Student/UpdateStudentLanguagesController.php
@@ -29,47 +29,11 @@ class UpdateStudentLanguagesController extends Controller
             $data = $request->validated();
 
             // Encontrar el estudiante por ID
-            // $student = Student::findOrFail($studentId);
             $student = $this->updateStudentLanguagesService->findStudentById($studentId);
 
-            // Obtener el CV del estudiante y sus lenguajes
-            // $resume = $student->resume;
+            // Encontrar el idioma por nombre y nivel
             $resume = $this->updateStudentLanguagesService->findStudentResume($student);
 
-            // $languagesToUpdate = $resume->languages;
-
-            // Encontrar el lenguaje solicitado por nombre y nivel
-            // $newLanguage = Language::where('language_name', $data['language_name'])
-            //     ->where('language_level', $data['language_level'])
-            //     ->firstOrFail();
-
-            // Actualizar el language_id en la tabla pivote
-            //     foreach ($languagesToUpdate as $languageToUpdate) {
-            //         if ($languageToUpdate->language_name === $data['language_name']) {
-            //             $resume->languages()->updateExistingPivot($languageToUpdate->id, ['language_id' => $newLanguage->id]);
-
-            //             return response()->json([
-            //                 'message' => 'Language updated successfully',
-            //             ]);
-            //         }
-            //     }
-
-            //     // Si no se encontró el lenguaje
-            //     return response()->json([
-            //         'message' => 'Language not found for this student'
-            //     ], 404);
-
-            // } catch (ModelNotFoundException $e) {
-            //     return response()->json([
-            //         'message' => 'Student or Language not found',
-            //         'error' => $e->getMessage()
-            //     ], 404);
-            // } catch (Exception $e) {
-            //     return response()->json([
-            //         'message' => 'An error occurred while updating the language',
-            //         'error' => $e->getMessage()
-            //     ], 500);
-            // }
             if ($this->updateStudentLanguagesService->updateStudentLanguage($resume, $data['language_name'], $data['language_level'])) {
                 return response()->json(['message' => 'Language updated successfully']);
             } else {
