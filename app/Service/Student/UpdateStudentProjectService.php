0b98a50b784fe48b26e34d96779a268a023a6ded
begoIT24 <blopezprof@gmail.com>
2024-07-24 11:46:33 +0200

    update project name fixed

--- app/Service/Student/UpdateStudentProjectService.php
+++ app/Service/Student/UpdateStudentProjectService.php
@@ -7,6 +7,7 @@ namespace App\Service\Student;
 use App\Models\Project;
 use App\Models\Student;
 use App\Models\Company;
+use App\Models\Tag;
 use Illuminate\Support\Facades\DB;
 use App\Exceptions\StudentNotFoundException;
 use App\Exceptions\ProjectNotFoundException;
@@ -20,7 +21,6 @@ class UpdateStudentProjectService
         DB::transaction(function () use ($studentId, $projectId, $data) {
             $this->getStudent($studentId);
             $project = $this->getProject($projectId);
-
             $this->updateProject($project, $data);
         });
     }
@@ -46,10 +46,16 @@ class UpdateStudentProjectService
     private function updateProject(Project $project, array $data): void
     {
         $project->name = $data['project_name'] ?? $project->name;
-        $project->tags = json_encode($data['tags'] ?? json_decode($project->tags));
+        //$project->tags = json_encode($data['tags'] ?? json_decode($project->tags));
         $project->github_url = $data['github_url'] ?? $project->github_url;
         $project->project_url = $data['project_url'] ?? $project->project_url;
-    
+        
+        if (isset($data['tags'])) {           
+            $tagsArray = $data['tags'];
+            $tags = Tag::whereIn('tag_name', $tagsArray)->get();
+            $tagIds = $tags->pluck('id')->toArray();
+            $project->tags = json_encode($tagIds);
+        }
         if (isset($data['company_name'])) {
             $company = Company::find($project->company_id);
             $company->name = $data['company_name'];
