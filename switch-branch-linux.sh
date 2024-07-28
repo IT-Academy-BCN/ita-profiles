7eea64d396d2d1fb0a32bcf4865808ed22a55ec5
JustTheDude001 <152200322+JustTheDude001@users.noreply.github.com>
2024-07-28 20:24:15 +0200

    Update switch-branch-linux.sh

--- switch-branch-linux.sh
+++ switch-branch-linux.sh
@@ -3,5 +3,5 @@ docker volume prune -f
 docker network prune -f
 if [ -d "./node_modules" ]; then sudo rm -Rf ./node_modules; fi
 if [ -d "./vendor" ]; then sudo rm -Rf ./vendor; fi
-docker-compose up
+docker-compose up -d
 
