294578d239187429ddbec6b48b86dc829afbda40
JustTheDude001 <152200322+JustTheDude001@users.noreply.github.com>
2024-07-29 12:20:43 +0200

    Update entrypoint_node.sh

--- entrypoint_node.sh
+++ entrypoint_node.sh
@@ -4,7 +4,5 @@ npm install
 npm run build
 chmod -R 777 /var/www/html/node_modules
 
-# Ejecuta el comando recibido como argumento del entrypoint
-#exec "$@"
 #Build continuously the react application (when changes are made):
 npm run watch
