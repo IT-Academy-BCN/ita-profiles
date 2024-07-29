9ca884617a5940c4d9c5dfee80b3bd559cef0415
begoIT24 <blopezprof@gmail.com>
2024-07-29 10:38:59 +0200

    modyfing controller to include the authorization before the updateproject function

--- app/Http/Controllers/api/Student/UpdateStudentProjectController.php
+++ app/Http/Controllers/api/Student/UpdateStudentProjectController.php
@@ -4,31 +4,40 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\api\Student;
 
+use Exception;
 use App\Http\Controllers\Controller;
 use App\Service\Student\UpdateStudentProjectService;
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\UpdateStudentProjectRequest;
 use App\Exceptions\StudentNotFoundException;
 use App\Exceptions\ProjectNotFoundException;
+use App\Exceptions\UserNotFoundException;
+use App\Service\Student\StudentService;
 
 class UpdateStudentProjectController extends Controller
 {
-    private $studentupdateProjectService;
+    private UpdateStudentProjectService $updateStudentProjectService;
 
-    public function __construct(UpdateStudentProjectService $studentupdateProjectService)
+    private StudentService $studentService;
+
+    public function __construct(UpdateStudentProjectService $updateStudentProjectService, StudentService $studentService)
     {
-        $this->studentupdateProjectService = $studentupdateProjectService;
+        $this->updateStudentProjectService = $updateStudentProjectService;
+        $this->studentService = $studentService;
     }
 
     public function __invoke(UpdateStudentProjectRequest $request, $studentId, $projectId): JsonResponse
     {       
         try {
+            $userProfile = $this->studentService->findUserByStudentID($studentId);
+            $this->authorize('canAccessResource', $userProfile);
+
             $data = $request->all();            
-            $this->studentupdateProjectService->execute($studentId, $projectId, $data);
+            $this->updateStudentProjectService->execute($studentId, $projectId, $data);
             return response()->json(['message' => 'El projecte s\'ha actualitzat'], 200);
-        } catch (StudentNotFoundException | ProjectNotFoundException $e) {
+        } catch (UserNotFoundException |StudentNotFoundException | ProjectNotFoundException $e) {
             return response()->json(['message' => $e->getMessage()], $e->getCode());
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return response()->json(['message' => $e->getMessage()], $e->getCode() ?: 500);
         }
     }
