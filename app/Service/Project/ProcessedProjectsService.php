e729117ac0b80f42b8e4e6f12d8d13aad4645edc
Nacho Albiol <nacho.albiol88@gmail.com>
2024-07-30 12:57:41 +0200

    Added logic to store Project in Resume project_ids array and cleaned some code.

--- app/Service/Project/ProcessedProjectsService.php
+++ app/Service/Project/ProcessedProjectsService.php
@@ -2,17 +2,22 @@
 
 namespace App\Service\Project;
 
+use Illuminate\Support\Facades\Log;
+
 class ProcessedProjectsService
 {
     private $processedProjects = [];
 
     public function addProcessedProject(string $gitHubUsername): void
     {
+        Log::info("Adding processed project for GitHub username: " . $gitHubUsername);
         $this->processedProjects[] = $gitHubUsername;
     }
 
     public function hasProcessedProject(string $gitHubUsername): bool
     {
-        return in_array($gitHubUsername, $this->processedProjects);
+        $hasProcessed = in_array($gitHubUsername, $this->processedProjects);
+        Log::info("Checking if project has been processed for GitHub username: " . $gitHubUsername . " - " . ($hasProcessed ? "Yes" : "No"));
+        return $hasProcessed;
     }
 }
