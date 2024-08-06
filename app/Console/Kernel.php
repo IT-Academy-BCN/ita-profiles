d1e2974036c4b49567ead27e72708fca56966991
Nacho Albiol <nacho.albiol88@gmail.com>
2024-08-06 12:03:08 +0200

    Removed Artisan Command & Restored original Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,18 +7,12 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-        Commands\FetchGithubRepos::class,
-    ];
-
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('app:fetch-github-repos')
-        //     ->everyMinute()
-        //     ->appendOutputTo('./storage/logs/cron.log');
+        // $schedule->command('inspire')->hourly();
     }
 
     /**
--- app/Providers/HandleProjectRetrieved.php
+++ app/Providers/HandleProjectRetrieved.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Service\Project\FetchGitHubReposService;
 use App\Service\Project\ProcessedProjectsService;
 use App\Service\Resume\GetGitHubUsernamesService;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
 
 class HandleProjectRetrieved
@@ -17,11 +16,8 @@ class HandleProjectRetrieved
     /**
      * Create the event listener.
      */
-    public function __construct(
-        GetGitHubUsernamesService $getGitHubUsernamesService,
-        ProcessedProjectsService $processedProjectsService,
-        FetchGitHubReposService  $fetchGitHubReposService
-    ) {
+    public function __construct(GetGitHubUsernamesService $getGitHubUsernamesService, ProcessedProjectsService $processedProjectsService, FetchGitHubReposService  $fetchGitHubReposService)
+    {
         $this->getGitHubUsernamesService = $getGitHubUsernamesService;
         $this->processedProjectsService = $processedProjectsService;
         $this->fetchGitHubReposService = $fetchGitHubReposService;
@@ -32,36 +28,27 @@ class HandleProjectRetrieved
      */
     public function handle(ProjectRetrieved $event): void
     {
-         $project = $event->project;
+        $project = $event->project;
 
-         try {
-             $gitHubUsername = $this->getGitHubUsernamesService->getSingleGitHubUsername($project);
-             //Log::info("GitHub username retrieved: " . $gitHubUsername);
-             //Log::info("GitHub username retrieved: " . $project->id);
-         } catch (\Exception $e) {
-             Log::error("Error retrieving GitHub username: " . $e->getMessage());
-             return;
-         }
+        try {
+            $gitHubUsername = $this->getGitHubUsernamesService->getSingleGitHubUsername($project);
+        } catch (\Exception $e) {
+            Log::error("Error retrieving GitHub username: " . $e->getMessage());
+            return;
+        }
 
-         if (!$this->processedProjectsService->hasProcessedProject($gitHubUsername)) {
-             // Marca el GitHub username como procesado
-             $this->processedProjectsService->addProcessedProject($gitHubUsername);
+        if (!$this->processedProjectsService->hasProcessedProject($gitHubUsername)) {
+            // Marca el GitHub username como procesado
+            $this->processedProjectsService->addProcessedProject($gitHubUsername);
 
-             Log::info("Sending project model for GitHub username: " . $gitHubUsername);
+            Log::info("Sending project model for GitHub username: " . $gitHubUsername);
 
-             try {
-                 // Log the type and value of gitHubUsername
-                 //Log::info("GitHub username before Artisan call: " . $gitHubUsername);
-
-                 // Ejecuta el comando de consola si es necesario
-//                 Artisan::call('app:fetch-github-repos', [
-//                     'gitHubUsername' => $gitHubUsername,
-//                 ]);
-                 $this->fetchGitHubReposService->fetchGitHubRepos($gitHubUsername);
-                 //Log::info("Artisan command executed successfully.");
-             } catch (\Exception $e) {
-                 //Log::error("Error executing Artisan command: " . $e->getMessage());
-             }
-         }
+            try {
+                $this->fetchGitHubReposService->fetchGitHubRepos($gitHubUsername);
+                //Log::info("Artisan command executed successfully.");
+            } catch (\Exception $e) {
+                //Log::error("Error executing Artisan command: " . $e->getMessage());
+            }
+        }
     }
 }
