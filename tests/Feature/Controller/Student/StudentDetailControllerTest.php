892063f54b7a7b1deab31ca1f68f9b700f83b2bb
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-09-17 13:12:40 +0200

    typo

--- tests/Feature/Controller/Student/StudentDetailControllerTest.php
+++ tests/Feature/Controller/Student/StudentDetailControllerTest.php
@@ -42,15 +42,4 @@ class StudentDetailControllerTest extends TestCase
 
         $response->assertJson(['message' => 'No query results for model [App\\Models\\Student] ' . $nonExistentStudentId]);
     }
-
-    public function testStudentDetailControllerReturns_404StatusAndResumeNotFoundExceptionMessageForValidStudentWithoutResume(): void
-    {
-        $this->student->resume->delete();
-
-        $response = $this->get(route('student.details', ['student' => $this->student]));
-
-        $response->assertStatus(404);
-
-        $response->assertJson(['message' => 'No s\'ha trobat cap currículum per a l\'estudiant amb id: ' . $this->student->id]);
-    }
 }
