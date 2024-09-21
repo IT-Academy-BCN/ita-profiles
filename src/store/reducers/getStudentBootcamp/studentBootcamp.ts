796f8c4a81eba0633c005209cc9f56fff7612e33
JuanDNJ <juandevnjv@gmail.com>
2024-09-21 11:41:54 +0200

    Creating state in redux studentBootcamp and getStudenBootcampThunk On branch feature/#241-fe-link-the-profile-to-the-active-user-once-logged-in Changes to be committed: 	new file:   src/store/reducers/getStudentBootcamp/getStudenBootcampThunk.ts 	new file:   src/store/reducers/getStudentBootcamp/studentBootcamp.ts

---
+++ src/store/reducers/getStudentBootcamp/studentBootcamp.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-param-reassign */
+
+import { createSlice } from "@reduxjs/toolkit";
+import { TBootcamp } from "../../../interfaces/interfaces";
+import getStudenBootcamp from "./getStudenBootcampThunk";
+
+
+const bootcampData: TBootcamp[] = [];
+
+
+
+
+const studenBootcamp = createSlice({
+    name: "studenDetailSlice",
+    initialState: {
+        isLoadindBootcamp: false,
+        isErrorBootcamp: false,
+        bootcampData
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(getStudenBootcamp.pending, (state) => {
+            state.isLoadindBootcamp = true
+            state.isErrorBootcamp = false
+        })
+        builder.addCase(getStudenBootcamp.fulfilled, (state, action) => {
+            state.bootcampData = action.payload.bootcamps
+            state.isLoadindBootcamp = false
+            state.isErrorBootcamp = false
+        })
+        builder.addCase(getStudenBootcamp.rejected, (state) => {
+            state.isLoadindBootcamp = false
+            state.isErrorBootcamp = true
+        })
+    }
+});
+
+export default studenBootcamp.reducer;
