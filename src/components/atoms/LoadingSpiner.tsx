cdcbddc9ebe168bb1c4c7a389e51aa7c86d1126d
JuanDNJ <juandevnjv@gmail.com>
2024-09-21 12:26:49 +0200

    Editing the name displayed by the LoadingSpiner On branch feature/#241-fe-link-the-profile-to-the-active-user-once-logged-in Changes to be committed: 	modified:   src/components/atoms/LoadingSpiner.tsx

--- src/components/atoms/LoadingSpiner.tsx
+++ src/components/atoms/LoadingSpiner.tsx
@@ -3,7 +3,7 @@ const LoadingSpiner: React.FC<{
     type?: string,
     textColor?: string,
     spinerColor?: string
-}> = ({ textContent = 'Cargando', type = "loading-spinner", textColor = "currentColor", spinerColor = "#C20087" }) => (
+}> = ({ textContent = 'Loading', type = "loading-spinner", textColor = "currentColor", spinerColor = "#C20087" }) => (
     <small className='text-md flex items-center gap-4'>
         <i style={{ color: `${textColor}` }}>{textContent}</i> <span className={`loading ${type} text-[${spinerColor}]`} />
     </small>
