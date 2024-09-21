8e77f9a1dfdb1a9dac623a13000e5b33871b4542
JuanDNJ <juandevnjv@gmail.com>
2024-09-21 11:54:43 +0200

    Modifying the useStudentDetailHook hook, adding the getStudenBootcamp thunk function to be executed. On branch feature/#241-fe-link-the-profile-to-the-active-user-once-logged-in Changes to be committed: 	modified:   src/hooks/useStudentDetailHook.ts

--- src/hooks/useStudentDetailHook.ts
+++ src/hooks/useStudentDetailHook.ts
@@ -6,6 +6,7 @@ import { SmallScreenContext } from "../context/SmallScreenContext";
 import { useStudentIdContext } from "../context/StudentIdContext";
 import getStudenProjects from "../store/reducers/getStudenProjects/studenProjectsThunk";
 import getStudentCollaborations from "../store/reducers/getStudentCollaborations/studentCollaborationsThunk";
+import getStudenBootcamp from "../store/reducers/getStudentBootcamp/getStudenBootcampThunk";
 
 const useStudentDetailHook = (rol?: string | null) => {
     const { isMobile }: TSmallScreenContext = useContext(SmallScreenContext)
@@ -18,10 +19,12 @@ const useStudentDetailHook = (rol?: string | null) => {
             getStudent(getStudenDetail(studentSUID))
             getStudent(getStudenProjects(studentSUID))
             getStudent(getStudentCollaborations(studentSUID))
+            getStudent(getStudenBootcamp(studentSUID))
         } else if (!rol && studentUUID) {
             getStudent(getStudenDetail(studentUUID))
             getStudent(getStudenProjects(studentUUID))
             getStudent(getStudentCollaborations(studentUUID))
+            getStudent(getStudenBootcamp(studentUUID))
         }
 
     }, [getStudent, rol, studentSUID, studentUUID])
