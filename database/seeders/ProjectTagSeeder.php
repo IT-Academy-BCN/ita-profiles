21a17a6cb574f854befefd285d045a6437c475b2
diegdar <diegdarch@hotmail.com>
2024-09-23 16:34:22 +0200

    Refactor: StudentTagSeeder and ProjectTagSeeder were refactored

--- database/seeders/ProjectTagSeeder.php
+++ database/seeders/ProjectTagSeeder.php
@@ -4,9 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Project;
 use App\Models\Tag;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class ProjectTagSeeder extends Seeder
 {
@@ -15,40 +14,26 @@ class ProjectTagSeeder extends Seeder
      */
     public function run(): void
     {
-        // Insertar 20 registros únicos en la tabla pivote
-        for ($i = 0; $i < 20; $i++) {
-            $pair = $this->getUniquePair();
-            $this->insertTagProject($pair['tag_id'], $pair['project_id']);
+        $tags = Tag::all();
+        $students = Project::all();
+
+        foreach ($students as $student) {
+            $assignedTags = [];
+            $assignedTags = $this->assignTagsToStudent($tags, $assignedTags);
+
+            $student->tags()->attach($assignedTags);
         }
     }
-
-    /**
-     * Obtiene una combinación única de tag_id y project_id.
-     */
-    private function getUniquePair(): array
+    public function assignTagsToStudent(Collection $tags, array $assignedTags): array
     {
-        $existingPairs = DB::table('project_tag')->pluck('project_id', 'tag_id')->toArray();
+        while (count($assignedTags) < rand(2, 5)) {
+            $tag = $tags->random();
 
-        do {
-            $tagId = Tag::inRandomOrder()->value('id');
-            $projectId = Project::inRandomOrder()->value('id');
-        } while (isset($existingPairs[$tagId])
-                && $existingPairs[$tagId] == $projectId);
+            if (!in_array($tag->id, $assignedTags)) {
+                $assignedTags[] = $tag->id;
+            }
+        }
 
-        return [
-            'tag_id' => $tagId,
-            'project_id' => $projectId,
-        ];
-    }
-
-    /**
-     * Inserta una combinación de tag_id y project_id en la tabla pivote.
-     */
-    private function insertTagProject(int $tagId, string $projectId): void
-    {
-        DB::table('project_tag')->insert([
-            'tag_id' => $tagId,
-            'project_id' => $projectId,
-        ]);
+        return $assignedTags;
     }
 }
--- database/seeders/StudentTagSeeder.php
+++ database/seeders/StudentTagSeeder.php
@@ -2,52 +2,35 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Models\Student;
 use App\Models\Tag;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Seeder;
 
 class StudentTagSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        // Insertar 20 registros únicos en la tabla pivote
-        for ($i = 0; $i < 20; $i++) {
-            $pair = $this->getUniquePair();
-            $this->insertTagStudent($pair['tag_id'], $pair['student_id']);
+        $tags = Tag::all();
+        $students = Student::all();
+
+        foreach ($students as $student) {
+            $assignedTags = [];
+            $assignedTags = $this->assignTagsToStudent($tags, $assignedTags);
+
+            $student->tags()->attach($assignedTags);
         }
     }
-
-    /**
-     * Obtiene una combinación única de tag_id y student_id.
-     */
-    private function getUniquePair(): array
+    public function assignTagsToStudent(Collection $tags, array $assignedTags): array
     {
-        $existingPairs = DB::table('student_tag')->pluck('student_id', 'tag_id')->toArray();
+        while (count($assignedTags) < rand(2, 5)) {
+            $tag = $tags->random();
 
-        do {
-            $tagId = Tag::inRandomOrder()->value('id');
-            $studentId = Student::inRandomOrder()->value('id');
-        } while (isset($existingPairs[$tagId])
-                && $existingPairs[$tagId] == $studentId);
+            if (!in_array($tag->id, $assignedTags)) {
+                $assignedTags[] = $tag->id;
+            }
+        }
 
-        return [
-            'tag_id' => $tagId,
-            'student_id' => $studentId,
-        ];
-    }
-
-    /**
-     * Inserta una combinación de tag_id y student_id en la tabla pivote.
-     */
-    private function insertTagStudent(int $tagId, string $studentId): void
-    {
-        DB::table('student_tag')->insert([
-            'tag_id' => $tagId,
-            'student_id' => $studentId,
-        ]);
+        return $assignedTags;
     }
 }
