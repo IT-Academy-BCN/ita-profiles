c9c855895138836eaa72ca589b28af7b14c32a4e
JuanDNJ <juandevnjv@gmail.com>
2024-09-23 21:48:25 +0200

    [FE] Create: new file src/__tests__/store/reducers/getStudentLanguages/studentLanguages.test.ts On branch feature/#241-fe-link-the-profile-to-the-active-user-once-logged-in

---
+++ src/__tests__/store/reducers/getStudentLanguages/studentLanguages.test.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-param-reassign */
+// import { createSlice } from "@reduxjs/toolkit";
+// import getStudentLanguagesThunk from "./studentLanguagesThunk";
+// import { TLanguage } from "../../../interfaces/interfaces";
+
+// const languagesData: TLanguage[] = []
+
+// const studentLanguages = createSlice({
+//     name: "studentLanguagesSlice",
+//     initialState: {
+//         isLoadingLanguages: false,
+//         isErrorLanguages: false,
+//         languagesData
+//     },
+//     reducers: {},
+//     extraReducers: (builder) => {
+//         builder.addCase(getStudentLanguagesThunk.pending, (state) => {
+//             state.isLoadingLanguages = true
+//             state.isErrorLanguages = false
+//         })
+//         builder.addCase(getStudentLanguagesThunk.fulfilled, (state, action) => {
+//             state.languagesData = action.payload
+//             state.isLoadingLanguages = false
+//             state.isErrorLanguages = false
+//         })
+//         builder.addCase(getStudentLanguagesThunk.rejected, (state) => {
+//             state.isLoadingLanguages = false
+//             state.isErrorLanguages = true
+//         })
+//     }
+// });
+
+// export default studentLanguages.reducer;
+describe("StudentLanguagesTest reducer", () => {
+    it("defined", () => {
+        expect(undefined).toBeDefined();
+    })
+})
