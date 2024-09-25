f5c352b79136dc691fd9d4cef4900c7af8c63fed
JuanDNJ <juandevnjv@gmail.com>
2024-09-25 16:44:49 +0200

    Reapply "[FE] Fix pr_check"
    This reverts commit 739fe30a98241d42e0b7cc0812d4ac2d564a0a7e.

--- src/components/studentDetailCards/languagesSection/LanguagesCard.tsx
+++ src/components/studentDetailCards/languagesSection/LanguagesCard.tsx
@@ -1,29 +1,24 @@
-import { useState } from 'react'
-import type { TLanguage } from '../../../interfaces/interfaces'
 import { useAppSelector } from '../../../hooks/ReduxHooks'
 import LoadingSpiner from '../../atoms/LoadingSpiner'
 
 const LanguagesCard: React.FC = () => {
   const { studentLanguages } = useAppSelector(state => state.ShowStudentReducer)
   const { languagesData, isLoadingLanguages, isErrorLanguages } = studentLanguages
-
-  console.log('languagesData:', languagesData);
-
-
   return (
     <div data-testid="LanguagesCard">
       <h3 className="text-lg font-bold">Idiomas</h3>
       {isLoadingLanguages && <LoadingSpiner />}
-      {!isLoadingLanguages && 
-        <div className="flex flex-col gap-2">          
+      {isErrorLanguages && <LoadingSpiner textContent='Upss!!' type="loading-bars" textColor="red" />}
+      {!isLoadingLanguages &&
+        <div className="flex flex-col gap-2">
           <div className="flex flex-col gap-1">
             {languagesData.map((language) => (
-                <ul key={language.language_id} className="flex flex-col">
-                  <li className="text-sm font-semibold text-black-2">
-                    {language.language_name}
-                  </li>
-                </ul>
-              ))}
+              <ul key={language.language_id} className="flex flex-col">
+                <li className="text-sm font-semibold text-black-2">
+                  {language.language_name}
+                </li>
+              </ul>
+            ))}
           </div>
         </div>}
     </div>
