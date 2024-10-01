bbf32789a1f84c2f14eee09d0e6ca18bf4ce84c0
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-10-01 12:30:18 +0200

    fix: correct wrong references

--- bin/reboot-linux.bat
+++ bin/reboot-linux.bat
@@ -2,10 +2,10 @@ docker-compose down
 docker system prune --all -f
 docker volume prune -f
 docker network prune -f
-if [ $(docker network ls | grep app-network) ]; then sh ./bin/disconnect_and_remove_network.sh; fi
+if [ $(docker network ls | grep app-network) ]; then sh ./disconnect_and_remove_network.sh; fi
 docker network create app-network
-if [ -d "./node_modules" ]; then sudo rm -Rf ./node_modules; fi
-if [ -d "./vendor" ]; then sudo rm -Rf ./vendor; fi
+if [ -d "../node_modules" ]; then sudo rm -Rf ../node_modules; fi
+if [ -d "../vendor" ]; then sudo rm -Rf ../vendor; fi
 docker-compose up --build -d
 docker network connect app-network mysql
 docker network connect app-network php
--- bin/reboot.bat
+++ bin/reboot.bat
@@ -2,10 +2,10 @@ docker compose down
 docker system prune --all -f
 docker volume prune -f
 docker network prune -f
-@REM if [ $$(docker network ls | grep app-network) ]; then sh ./bin/disconnect_and_remove_network.sh; fi
+@REM if [ $$(docker network ls | grep app-network) ]; then sh ./disconnect_and_remove_network.sh; fi
 docker network create app-network
-@REM if [ -d "./node_modules" ]; then sudo rm -Rf ./node_modules; fi
-@REM if [ -d "./vendor" ]; then sudo rm -Rf ./vendor; fi
+@REM if [ -d "../node_modules" ]; then sudo rm -Rf ../node_modules; fi
+@REM if [ -d "../vendor" ]; then sudo rm -Rf ../vendor; fi
 docker compose up --build -d
 docker network connect app-network mysql
 docker network connect app-network php
