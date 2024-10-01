e56866c0b753cc7b9c7350edf1a4f66d8f91dab7
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-10-01 11:37:01 +0200

    setup modelbinding

--- app/Http/Controllers/api/Student/StudentBootcampDetailController.php
+++ app/Http/Controllers/api/Student/StudentBootcampDetailController.php
@@ -4,30 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\api\Student;
 
-use Exception;
 use App\Http\Controllers\Controller;
-use App\Service\Student\StudentBootcampDetailService;
-use App\Exceptions\StudentNotFoundException;
-use App\Exceptions\ResumeNotFoundException;
+use App\Models\Student;
 use Illuminate\Http\JsonResponse;
 
 class StudentBootcampDetailController extends Controller
 {
-    private StudentBootcampDetailService $studentBootcampDetailService;
-
-    public function __construct(StudentBootcampDetailService $studentBootcampDetailService)
-    {
-        $this->studentBootcampDetailService = $studentBootcampDetailService;
-    }
-    public function __invoke($studentId): JsonResponse
-    {
-        try {
-            $bootcampDetails = $this->studentBootcampDetailService->execute($studentId);
-            return response()->json(['bootcamps' => $bootcampDetails]);
-        } catch (StudentNotFoundException | ResumeNotFoundException $e) {
-            return response()->json(['message' => $e->getMessage()], $e->getCode());
-        } catch (Exception $e) {
-            return response()->json(['message' => $e->getMessage()], $e->getCode() ?: 500);
-        }
-    }
+    public function __invoke(Student $student): JsonResponse {}
 }
