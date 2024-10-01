04f4d5d5a6c59d2f5c7ecce004a1622e7523f953
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-10-01 15:15:04 +0200

    final refactor

--- tests/Feature/Controller/Student/StudentBootcampDetailControllerTest.php
+++ tests/Feature/Controller/Student/StudentBootcampDetailControllerTest.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Controller\Student;
 
-use App\Models\Bootcamp;
-use App\Models\Resume;
+use App\Models\Student;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use App\Http\Controllers\api\Student\StudentBootcampDetailController;
-use App\Service\Student\StudentBootcampDetailService;
 use Tests\TestCase;
 
 class StudentBootcampDetailControllerTest extends TestCase
@@ -16,36 +13,29 @@ class StudentBootcampDetailControllerTest extends TestCase
     use DatabaseTransactions;
 
     protected $studentWithoutBootcamps;
-
     protected $studentWithBootcamps;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->studentWithoutBootcamps = Resume::factory()->create()->student;
+        $this->studentWithoutBootcamps = Student::has('resume.bootcamps', '=', 0)->first();
 
-        $resume = Resume::factory()->create();
-
-        $bootcamp = Bootcamp::factory()->create();
-
-        $resume->bootcamps()->attach($bootcamp->id, ['end_date' => now()->subYear()->addDays(rand(1, 365))]);
-
-        $this->studentWithBootcamps = $resume->student;
+        $this->studentWithBootcamps = Student::has('resume.bootcamps', '>', 0)->first();
     }
 
     public function testStudentBootcampDetailControllerGetStudentBootcampDetails(): void
     {
-        $response = $this->getJson(route('student.bootcamp', ['studentId' =>  $this->studentWithBootcamps->id]));
+        $response = $this->getJson(route('student.bootcamp', ['student' => $this->studentWithBootcamps->id]));
 
         $response->assertStatus(200);
 
         $response->assertJsonStructure([
             'bootcamps' => [
                 '*' => [
-                    'bootcamp_id',
-                    'bootcamp_name',
-                    'bootcamp_end_date',
+                    'id',
+                    'name',
+                    'end_date',
                 ],
             ],
         ]);
@@ -55,29 +45,19 @@ class StudentBootcampDetailControllerTest extends TestCase
     {
         $nonexistentUuid = "00000000-0000-0000-0000-000000000000";
 
-        $response = $this->getJson(route('student.bootcamp', ['studentId' => $nonexistentUuid]));
+        $response = $this->getJson(route('student.bootcamp', ['student' => $nonexistentUuid]));
 
         $response->assertStatus(404);
 
-        $response->assertJson(['message' => "No s'ha trobat cap estudiant amb aquest ID: $nonexistentUuid"]);
+        $response->assertJson(['message' => 'No query results for model [App\\Models\\Student] ' . $nonexistentUuid]);
     }
 
     public function testStudentBootcampDetailControllerReturnsEmptyArrayForStudentWithoutBootcamp(): void
     {
-        $response = $this->getJson(route('student.bootcamp', ['studentId' => $this->studentWithoutBootcamps->id]));
+        $response = $this->getJson(route('student.bootcamp', ['student' => $this->studentWithoutBootcamps->id]));
 
         $response->assertStatus(200);
 
         $response->assertJson(['bootcamps' => []]);
     }
-
-    public function testStudentBootcampDetailControllerCanBeInstantiated(): void
-    {
-        $studentBootcampDetailService = $this->createMock(StudentBootcampDetailService::class);
-
-        $controller = new StudentBootcampDetailController($studentBootcampDetailService);
-
-        $this->assertInstanceOf(StudentBootcampDetailController::class, $controller);
-    }
-
 }
