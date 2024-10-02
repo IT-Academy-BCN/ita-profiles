2067aabc4646ac8a7705ec027b77fbf9d2a22f02
diegdar <diegdarch@hotmail.com>
2024-10-02 16:08:12 +0200

    refactor(Annotations): :truck: Rename parameter from studentId to student and change output messages

--- app/Annotations/OpenApi/Controllers/StudentResume/StudentLanguagesDetailAnnotation.php
+++ app/Annotations/OpenApi/Controllers/StudentResume/StudentLanguagesDetailAnnotation.php
@@ -10,7 +10,7 @@ class StudentLanguagesDetailAnnotation
 {
     /**
      * @OA\Get(
-     *      path="/student/{studentId}/resume/languages",
+     *      path="/student/{student}/resume/languages",
      *      operationId="getStudentResumeLanguages",
      *      tags={"Student -> Resume"},
      *      summary="Gets the languages spoken by a student",
@@ -19,7 +19,7 @@ class StudentLanguagesDetailAnnotation
 It returns a list of languages along with any other relevant information, such as the proficiency level in each language.",
      *
      *      @OA\Parameter(
-     *          name="studentId",
+     *          name="student",
      *          description="Student ID",
      *          required=true,
      *          in="path",
@@ -39,7 +39,7 @@ It returns a list of languages along with any other relevant information, such a
      *                  @OA\Items(
      *                      type="object",
      *                      @OA\Property(
-     *                          property="language_id",
+     *                          property="id",
      *                          type="string",
      *                          description="Language UUID",
      *                          example="e6b4432b-d2f8-4e06-b727-6ecaf40e6e0e"
@@ -65,8 +65,7 @@ It returns a list of languages along with any other relevant information, such a
         *     description="Student or Resume not found",
         *     @OA\JsonContent(
         *         type="object",
-        *         @OA\Property(property="message", type="string", example="No s'ha trobat cap estudiant amb aquest ID {studentId}"),
-        *         @OA\Property(property="message2", type="string", example="No s'ha trobat cap currículum per a l'estudiant amb id: {studentId}")
+        *         @OA\Property(property="message", type="string", example="No query results for model [App\Models\Student] {studentId}"),
         *     )
      *     ),
      *     @OA\Response(
