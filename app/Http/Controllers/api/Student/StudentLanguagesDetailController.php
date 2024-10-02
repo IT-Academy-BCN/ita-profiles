e7e09276f1a8c00871151c31071724a459123e44
diegdar <diegdarch@hotmail.com>
2024-10-02 13:04:57 +0200

    refactor: :recycle: Controller was refactored and service and its tests were deleted
    StudentLanguagesDetailController was refactored and  StudentLanguageDetailService and itst tests: StudentLanguageDetailServiceTest were deleted according to Model binding structure

--- app/Http/Controllers/api/Student/StudentLanguagesDetailController.php
+++ app/Http/Controllers/api/Student/StudentLanguagesDetailController.php
@@ -1,34 +1,18 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App\Http\Controllers\api\Student;
 
-use Exception;
 use App\Http\Controllers\Controller;
-use App\Service\Student\StudentLanguageDetailService;
-use App\Exceptions\StudentNotFoundException;
-use App\Exceptions\ResumeNotFoundException;
+use App\Http\Resources\LanguageCollection;
+use App\Models\Student;
 use Illuminate\Http\JsonResponse;
 
 class StudentLanguagesDetailController extends Controller
 {
-    private StudentLanguageDetailService $studentLanguageDetailService;
-
-    public function __construct(StudentLanguageDetailService $studentLanguageDetailService)
+    public function __invoke(Student $student): JsonResponse
     {
-        $this->studentLanguageDetailService = $studentLanguageDetailService;
-    }
-
-    public function __invoke(string $studentId): JsonResponse
-    {
-        try {
-            $service = $this->studentLanguageDetailService->execute($studentId);
-            return response()->json(['languages' => $service]);
-        } catch (StudentNotFoundException | ResumeNotFoundException $e) {
-            return response()->json(['message' => $e->getMessage()], $e->getCode());
-        } catch (Exception $e) {
-            return response()->json(['message' => $e->getMessage()], $e->getCode() ?: 500);
-        }
+        $languages = $student->resume?->languages ?? collect();
+        return response()->json(new LanguageCollection(resource: $languages));
     }
 }
