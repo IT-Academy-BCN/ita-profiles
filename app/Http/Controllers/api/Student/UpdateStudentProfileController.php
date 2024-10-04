124cbf15e5ae02c449c25d56f0603aa7647a63c4
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-10-04 11:40:14 +0200

    refactor: remove service

--- app/Http/Controllers/api/Student/UpdateStudentProfileController.php
+++ app/Http/Controllers/api/Student/UpdateStudentProfileController.php
@@ -10,30 +10,24 @@ use Illuminate\Http\{
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateStudentRequest;
-use App\Service\Student\UpdateStudentProfileService;
 
 class UpdateStudentProfileController extends Controller
 {
-    private UpdateStudentProfileService $updateStudentProfileService;
 
-    public function __construct(UpdateStudentProfileService $updateStudentProfileService)
-    {
-        $this->updateStudentProfileService = $updateStudentProfileService;
-    }
-
-    /**
-     * @throws ResumeNotFoundException
-     */
     public function __invoke(UpdateStudentRequest $request, Student $student): JsonResponse
     {
-        $dataStudentProfileUpdate = $request->validated();
+        $data = $request->validated();
 
-        $this->updateStudentProfileService->execute($student, $dataStudentProfileUpdate);
+        $student->update($data);
+
+        $resume = $student->resume?->update($data) ?? collect();
+
+        $student->tags()->sync($data['tags_ids'] ?? []);
 
         return response()->json([
-            'profile' => 'El perfil de l\'estudiant s\'actualitza correctament'
+            'profile' => 'El perfil de l\'estudiant s\'actualitza correctament',
+            'resume' => $resume,
         ]);
-
 
     }
 }
