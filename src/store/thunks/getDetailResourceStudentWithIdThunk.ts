a7b9d84b712e9e2e780984f9521490b51cb02314
JuanDNJ <juandevnjv@gmail.com>
2024-10-07 08:51:45 +0200

    [FE] getDetailResourceStudentWithIdThunk.ts "Using const resumes" Changes to be committed: 	modified:   src/store/thunks/getDetailResourceStudentWithIdThunk.ts

--- src/store/thunks/getDetailResourceStudentWithIdThunk.ts
+++ src/store/thunks/getDetailResourceStudentWithIdThunk.ts
@@ -1,23 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getStudentDetailWithId } from "../../api/student/getStudentDetailWithId";
+import { getStudentDetailWithId, resumes } from "../../api/student/getStudentDetailWithId";
 
 const getStudentBootcampThunk = createAsyncThunk(
     "getStudentBootcampThunk",
     async (studentId: string | null) => {
-        const response = await getStudentDetailWithId(studentId, "bootcamp")
+        const response = await getStudentDetailWithId(studentId, resumes.bootcamp)
         return response;
     })
 const getStudentCollaborationThunk = createAsyncThunk(
     "getStudentCollaborationThunk",
     async (studentId: string | null) => {
-        const response = await getStudentDetailWithId(studentId, "collaborations")
+        const response = await getStudentDetailWithId(studentId, resumes.collaborations)
         return response;
     })
 const getStudentDetailThunk = createAsyncThunk(
     "getStudentDetailThunk",
     async (studentID: string | null) => {
         try {
-            const response = await getStudentDetailWithId(studentID, "detail")
+            const response = await getStudentDetailWithId(studentID, resumes.detail)
             return response;
         } catch (error) {
             const MyError = error as ErrorEvent
@@ -27,26 +27,26 @@ const getStudentDetailThunk = createAsyncThunk(
 const getStudentLanguagesThunk = createAsyncThunk(
     "getStudentLanguagesThunk",
     async (studentID: string | null) => {
-        const response = await getStudentDetailWithId(studentID, "languages")
+        const response = await getStudentDetailWithId(studentID, resumes.languages)
         return response;
     })
 const getStudentModalityThunk = createAsyncThunk(
     "getStudentModalityThunk",
     async (studenSUID: string | null) => {
-        const response = await getStudentDetailWithId(studenSUID, "modality")
+        const response = await getStudentDetailWithId(studenSUID, resumes.modality)
         return response;
     })
 const getStudentProjectsThunk = createAsyncThunk(
     "getStudentProjectsThunk",
     async (studenSUID: string | null) => {
-        const response = await getStudentDetailWithId(studenSUID, "projects")
+        const response = await getStudentDetailWithId(studenSUID, resumes.projects)
         return response;
     })
 const getStudentAdditionalTrainingThunk = createAsyncThunk(
     "getStudentAdditionalTrainingThunk",
     async (studentID: string | null) => {
         try {
-            const response = await getStudentDetailWithId(studentID, "additionaltraining")
+            const response = await getStudentDetailWithId(studentID, resumes.additionaltraining)
             return response;
         } catch (error) {
             const MyError = error as ErrorEvent
