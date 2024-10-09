d6b4b045c54f1462d925c551c62eef3ea0ecb8f6
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-10-09 14:26:04 +0200

    fix: update reboot.bat

--- bin/reboot.bat
+++ bin/reboot.bat
@@ -1,16 +1,29 @@
-docker-compose down
+@echo off
+
+docker compose down
 docker system prune --all -f
 docker volume prune -f
 docker network prune -f
-if [ $(docker network ls | grep app-network) ]; then sh ./disconnect_and_remove_network.sh; fi
+
+docker network ls | findstr app-network > nul
+if %ERRORLEVEL% == 0 (
+    call ./disconnect_and_remove_network.bat
+)
+
 docker network create app-network
-if [ -d "../node_modules" ]; then sudo rm -Rf ../node_modules; fi
-if [ -d "../vendor" ]; then sudo rm -Rf ../vendor; fi
+
+if exist "..\node_modules" (
+    rmdir /S /Q "..\node_modules"
+)
+
+if exist "..\vendor" (
+    rmdir /S /Q "..\vendor"
+)
+
 docker compose up --build -d
+
 docker network connect app-network mariadb
 docker network connect app-network php
 docker network connect app-network node
 docker network connect app-network webserver
 docker network connect app-network redis
-
-
