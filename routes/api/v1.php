cce6d2f51b6ac6c608b29e236cbba4711d85cf90
diegdar <diegdarch@hotmail.com>
2024-10-16 11:26:15 +0200

    refactor(routes): :recycle: rename studentId parameter to student in endpoint

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -57,11 +57,11 @@ Route::post('student/{student}/resume/languages', AddStudentLanguageController::
 Route::get('student/{student}/resume/additionaltraining', StudentAdditionalTrainingListController::class)->name('student.additionaltraining');
 Route::get('student/{student}/resume/collaborations', StudentCollaborationDetailController::class)->name('student.collaborations');
 Route::put('student/{student}/resume/collaborations', UpdateStudentCollaborationsController::class)->name('student.updateCollaborations');
+Route::get('student/{student}/resume/photo', GetStudentImageController::class)->middleware('auth:api', EnsureStudentOwner::class)->name('student.photo.get');
 
 Route::prefix('student/{studentId}/resume')->group(function () {
     Route::put('languages', UpdateStudentLanguagesController::class)->name('student.languages.update');
     Route::get('modality', StudentModalityController::class)->name('student.modality');
-    Route::get('photo', GetStudentImageController::class)->middleware('auth:api', EnsureStudentOwner::class)->name('student.photo.get');
     Route::post('photo', UpdateStudentImageController::class)->name('student.updatePhoto');
     Route::delete('languages/{languageId}', DeleteStudentResumeLanguageController::class)->name('student.language.delete');
 });
