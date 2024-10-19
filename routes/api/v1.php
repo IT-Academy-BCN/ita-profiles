7f4c8b9b27df1da7c1f450fab6b7ccea17f8dd69
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-10-19 10:55:59 +0200

    refactor: implementing Laravel's model binding on student.modality

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -46,6 +46,19 @@ Route::post('/signin', [AuthController::class, 'signin'])->name('signin');
 
 Route::get('student/resume/list', StudentListController::class)->name('students.list');
 
+Route::get('student/{student}/resume/languages', StudentLanguagesDetailController::class)->name('student.languages');
+
+Route::get('student/{student}/resume/detail', StudentDetailController::class)->name('student.details');
+Route::put('student/{student}/resume/profile', UpdateStudentProfileController::class)->name('student.updateProfile');
+Route::get('student/{student}/resume/bootcamp', StudentBootcampDetailController::class)->name('student.bootcamp');
+Route::get('student/{student}/resume/projects', StudentProjectsDetailController::class)->name('student.projects');
+Route::put('student/{student}/resume/projects/{project}', UpdateStudentProjectController::class)->middleware('auth:api')->name('student.updateProject');
+Route::post('student/{student}/resume/languages', AddStudentLanguageController::class)->name('student.addLanguage');
+Route::get('student/{student}/resume/additionaltraining', StudentAdditionalTrainingListController::class)->name('student.additionaltraining');
+Route::get('student/{student}/resume/collaborations', StudentCollaborationDetailController::class)->name('student.collaborations');
+Route::put('student/{student}/resume/collaborations', UpdateStudentCollaborationsController::class)->name('student.updateCollaborations');
+Route::put('student/{student}/resume/photo', UpdateStudentImageController::class)->name('student.updatePhoto');
+Route::get('student/{student}/resume/modality', StudentModalityController::class)->name('student.modality');
 Route::prefix('student/{student}/resume')->group(function () {
     Route::get('languages', StudentLanguagesDetailController::class)->name('student.languages');
     Route::get('detail', StudentDetailController::class)->name('student.details');
@@ -63,7 +76,7 @@ Route::prefix('student/{student}/resume')->group(function () {
 });
 
 Route::prefix('student/{studentId}/resume')->group(function () {
-    Route::get('modality', StudentModalityController::class)->name('student.modality');
+    Route::put('languages', UpdateStudentLanguagesController::class)->name('student.languages.update');
     Route::get('photo', GetStudentImageController::class)->middleware('auth:api', EnsureStudentOwner::class)->name('student.photo.get');
     Route::delete('languages/{languageId}', DeleteStudentResumeLanguageController::class)->name('student.language.delete');
 });
