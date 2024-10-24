43b1ac5d3dd48090aaabb652b468a2d372d17407
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-10-24 13:18:42 +0200

    refactor: add github_timestamp to resumes table

---
+++ database/migrations/2024_10_23_060620_add_github_timestamp_to_resumes_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('resumes', function (Blueprint $table) {
+            $table->timestamp('github_updated_at')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('resume', function (Blueprint $table) {
+            $table->dropColumn('github_updated_at');
+        });
+    }
+};
