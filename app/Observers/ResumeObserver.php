598d2d2112b10770a15f65d90249c733c6dd1f4d
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-10-25 09:04:16 +0200

    refactor: implement observer on Resume model

---
+++ app/Observers/ResumeObserver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Resume;
+use App\Service\Project\ProjectProcessingService;
+use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
+
+class ResumeObserver implements ShouldHandleEventsAfterCommit
+{
+    protected ProjectProcessingService $projectProcessingService;
+
+    public function __construct(ProjectProcessingService $projectProcessingService)
+    {
+        $this->projectProcessingService = $projectProcessingService;
+    }
+    public function saved(Resume $resume): void
+    {
+        if($resume->wasChanged('github_url')) {
+            $this->projectProcessingService->processSingleResume($resume);
+        }
+    }
+}
