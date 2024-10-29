b44165af04bafedbfb2f412935371b4ba46f1560
diega <diegochiola@gmail.com>
2024-10-29 16:29:16 +0100

    Create AddStudentImageController

---
+++ app/Http/Controllers/api/Student/AddStudentImageController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\api\Student;
+
+use App\Models\Student;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\UpdateImageStudentRequest;
+
+class AddStudentImageController extends Controller
+{
+    private string $photo_infix = '.profile_photo.';
+    private string $photos_path = 'public/photos/';
+    
+ 
+    public function __invoke(UpdateImageStudentRequest $request, Student $student): JsonResponse{
+        
+        $file = $request->file('photo');
+        $filename = time() . '.' . $student->id . $this->photo_infix . $file->hashName();
+        if ($student->photo) {
+            Storage::delete($this->photos_path . $student->photo);
+        }
+        $file->storeAs($this->photos_path, $filename);
+        $student->photo = $filename;
+        $student->save();
+        return response()->json(['message' => 'La imatge s\'ha afegit correctament']);
+    }
+}
