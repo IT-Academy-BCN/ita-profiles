f50736b46668b4a0f8a0cf111825a7f8dbaeeb2d
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-10-30 23:41:37 +0100

    refactor: restore DatabaseSeeder to original state

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,28 +4,25 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\Project;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Project::withoutEvents(function () {
-            $this->call(SigninTestSeeder::class); // Needs to be first in order to add the ID to student.
-            $this->call(RoleSeeder::class);
-            $this->call(TagSeeder::class);
-            $this->call(StudentListSeeder::class);
-            $this->call(ProjectSeeder::class);
-            $this->call(AdditionalTrainingSeeder::class);
-            $this->call(ResumeAdditionalTrainingSeeder::class);
-            $this->call(CollaborationSeeder::class);
-            $this->call(LanguageSeeder::class);
-            $this->call(ResumeLanguageSeeder::class);
-            $this->call(BootcampSeeder::class);
-            $this->call(ResumeBootcampSeeder::class);
-            $this->call(StudentTagSeeder::class);
-            $this->call(ProjectTagSeeder::class);
-        });
+        $this->call(SigninTestSeeder::class); // Needs to be first in order to add the ID to student.
+        $this->call(RoleSeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(StudentListSeeder::class);
+        $this->call(ProjectSeeder::class);
+        $this->call(AdditionalTrainingSeeder::class);
+        $this->call(ResumeAdditionalTrainingSeeder::class);
+        $this->call(CollaborationSeeder::class);
+        $this->call(LanguageSeeder::class);
+        $this->call(ResumeLanguageSeeder::class);
+        $this->call(BootcampSeeder::class);
+        $this->call(ResumeBootcampSeeder::class);
+        $this->call(StudentTagSeeder::class);
+        $this->call(ProjectTagSeeder::class);
     }
 }
