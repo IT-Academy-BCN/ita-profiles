2bd780dc9bc100ebd7c651073331ba397d18c88f
Tomi <tomas.casabona@gmail.com>
2024-10-30 18:43:09 +0100

    feedback message errors are implemented

---
+++ src/components/feedbackMessages/Error.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+
+type ErrorProps = {
+    message: string
+}
+
+export const Error: React.FC<ErrorProps> = ({ message }) => {
+    const [isVisible, setIsVisible] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(false)
+        }, 3000) // 3 segundos
+
+        // Limpia el temporizador cuando el componente se desmonte
+        return () => clearTimeout(timer)
+    }, [])
+
+    if (!isVisible) return null
+    return (
+        <div role="alert" className="alert alert-error">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+            </svg>
+            <span>{message}</span>
+        </div>
+    )
+}
--- src/components/students/studentDetail/studentProfile/studentProfileCards/MyProfileStudentDetailCard.tsx
+++ src/components/students/studentDetail/studentProfile/studentProfileCards/MyProfileStudentDetailCard.tsx
@@ -7,13 +7,19 @@ import LoadingSpiner from '../../../../atoms/LoadingSpiner'
 import { EditStudentProfile } from './editStudentProfile/EditStudentProfile'
 import { ModalPortals } from '../../../../ModalPortals'
 import { detailThunk } from '../../../../../store/thunks/getDetailResourceStudentWithIdThunk'
+import { Error } from '../../../../feedbackMessages/Error'
+import { Success } from '../../../../feedbackMessages/Success'
 
 const MyProfileStudentDetailCard: React.FC = () => {
     const [showFullDescription, setShowFullDescription] = useState(false)
     const [openEditProfile, setOpenEditProfile] = useState(false)
-    const { aboutData, isLoadingAboutData, isErrorAboutData } = useAppSelector(
-        (state) => state.ShowStudentReducer.studentDetails,
-    )
+    const {
+        aboutData,
+        isLoadingAboutData,
+        isErrorAboutData,
+        updatedError,
+        updatedMessage,
+    } = useAppSelector((state) => state.ShowStudentReducer.studentDetails)
 
     const dispatch = useAppDispatch()
 
@@ -33,6 +39,8 @@ const MyProfileStudentDetailCard: React.FC = () => {
         <div data-testid="StudentDataCard">
             {isLoadingAboutData && <LoadingSpiner />}
             {isErrorAboutData && <LoadingSpiner />}
+            {updatedError && <Error message={updatedError} />}
+            {updatedMessage && <Success message={updatedMessage} />}
             {openEditProfile && (
                 <ModalPortals>
                     <EditStudentProfile
