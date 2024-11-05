b7611cb7b205d8e63ea5ebcdf5b0cc93751b7b2b
GabrielCLuque <165780394+GabrielCLuque@users.noreply.github.com>
2024-11-05 10:50:24 +0100

    Update TagListController.php

--- app/Http/Controllers/api/Tag/TagListController.php
+++ app/Http/Controllers/api/Tag/TagListController.php
@@ -6,22 +6,15 @@ namespace App\Http\Controllers\api\Tag;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Tag\TagCollection;
-use App\Service\Tag\TagListService;
+use App\Models\Tag;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Collection;
 
 class TagListController extends Controller
 {
-    private TagListService $tagListService;
-
-    public function __construct(TagListService $tagListService)
-    {
-        $this->tagListService = $tagListService;
-    }
-    
     public function __invoke(): JsonResponse
     {
-        $tags = collect($this->tagListService->execute())->map(fn($tag) => (object) $tag);
+
+        $tags = Tag::all();
         return (new TagCollection($tags))->response()->setStatusCode(200);
     }
 }
