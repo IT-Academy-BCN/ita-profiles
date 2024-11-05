27d3a2284271e3c3cd69e0f396ea02f9521d1c2c
GabrielCLuque <165780394+GabrielCLuque@users.noreply.github.com>
2024-11-05 09:38:05 +0100

    Refactor using tagcollection

--- app/Http/Controllers/api/Tag/TagListController.php
+++ app/Http/Controllers/api/Tag/TagListController.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Http\Controllers\api\Tag;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Tag\TagResource;
+use App\Http\Resources\Tag\TagCollection;
 use App\Service\Tag\TagListService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
 
 class TagListController extends Controller
 {
@@ -20,13 +21,7 @@ class TagListController extends Controller
     
     public function __invoke(): JsonResponse
     {
-
-        $tagsArray = $this->tagListService->execute();
-
-        $tagsResource = TagResource::collection(collect($tagsArray)->map(function ($tag) {
-            return (object) $tag; 
-        }));
-
-        return $tagsResource->response();
+        $tags = collect($this->tagListService->execute())->map(fn($tag) => (object) $tag);
+        return (new TagCollection($tags))->response()->setStatusCode(200);
     }
 }
