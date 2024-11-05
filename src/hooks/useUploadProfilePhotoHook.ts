fb2bbcc32e27fb39606939a57feb3684ccaa32f2
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-11-05 08:08:32 +0100

    modified:   src/hooks/useUploadProfilePhotoHook.ts

--- src/hooks/useUploadProfilePhotoHook.ts
+++ src/hooks/useUploadProfilePhotoHook.ts
@@ -1,7 +1,25 @@
 import { useRef, useState } from "react";
+import { z } from 'zod';
 import { useAppDispatch, useAppSelector } from "./ReduxHooks";
 import { updateProfilePhotoThunk } from "../store/thunks/updateProfilePhotoThunk";
 import { resetSendingPhoto, setToggleProfileImage } from "../store/slices/student/detailSlice";
+import errorFilter from "./error-filter.svg"
+import errorUpload from "./error-upload.svg"
+import sendPhoto from "./send-photo.svg"
+
+const MAX_MB = 500000
+const imageFileSchema = z
+  .instanceof(File)
+  .refine((file) => file.type === "image/png" || file.type === "image/jpeg", {
+
+    message: "type",
+
+  })
+  .refine((file) => file.size <= MAX_MB, {
+
+    message: "size",
+
+  });
 
 const useUploadProfilePhotoHook = () => {
   const fileRef = useRef<HTMLInputElement | null>(null);
@@ -11,12 +29,44 @@ const useUploadProfilePhotoHook = () => {
   const [newProfilePicture, setNewProfilePicture] = useState<string>(aboutData.photo)
   const dispatch = useAppDispatch()
 
+  const [validationErrors] = useState([
+    {
+      code: "upload",
+      message: "Error al subir la imagen",
+      img: errorUpload
+    },
+    {
+      code: "size",
+      message: "El archivo debe pesar menos de 5 MB",
+      img: errorFilter
+    },
+    {
+      code: "type",
+      message: "El archivo debe ser una imagen en formato PNG o JPG",
+      img: errorFilter
+    },
+  ])
+
   const handlerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const files: File[] = Array.from(event.target.files)
-      setNewProfilePicture(() => URL.createObjectURL(files[0]))
-    } else {
-      setNewProfilePicture(aboutData.photo)
+      const validacion = imageFileSchema.safeParse(files[0])
+
+
+
+      if (validacion.success) {
+        console.log(files[0])
+        setNewProfilePicture(() => URL.createObjectURL(files[0]))
+
+      } else {
+
+        setNewProfilePicture(errorFilter)
+        console.log(files[0].size)
+        const existError = validationErrors.find(err => err.code === validacion.error.errors[0].message)
+        console.log(validacion.error.errors[0].message, existError)
+        setNewProfilePicture(existError!.img)
+      }
+
     }
     dispatch(resetSendingPhoto())
   }
@@ -63,13 +113,15 @@ const useUploadProfilePhotoHook = () => {
     photoSentSuccessfully,
     newProfilePicture,
     setToggleProfileImage,
+    sendPhoto,
     setNewProfilePicture,
     dispatch,
     handlerChange,
     handlerPictureClick,
     handlerCancel,
     handleKeyDown,
-    sendNewProfilePicture
+    sendNewProfilePicture,
+    validationErrors
   }
 }
 export default useUploadProfilePhotoHook
