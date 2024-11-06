4d884182d2f103328ea159a47e024fcf3f8b0291
diega <diegochiola@gmail.com>
2024-11-06 11:23:37 +0100

    Refactor AddStudentImageController to delete old photo after saving new one

--- app/Http/Controllers/api/Student/AddStudentImageController.php
+++ app/Http/Controllers/api/Student/AddStudentImageController.php
@@ -21,12 +21,16 @@ class AddStudentImageController extends Controller
         
         $file = $request->file('photo');
         $filename = time() . '.' . $student->id . $this->photo_infix . $file->hashName();
-        if ($student->photo) {
-            Storage::delete($this->photos_path . $student->photo);
-        }
+        $oldPhoto = $student->photo;
+
         $file->storeAs($this->photos_path, $filename);
         $student->photo = $filename;
         $student->save();
+
+        if ($oldPhoto) {
+            Storage::delete($this->photos_path . $oldPhoto);
+        }
+        
         return response()->json(['message' => 'La imatge s\'ha afegit correctament']);
     }
 }
