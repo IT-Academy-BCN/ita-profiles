e9ec6a0a3a1656a12273f88e5ddb10e12f959f1a
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-11-11 22:52:16 +0100

    fixing CIF

---
+++ database/migrations/2024_11_11_215057_create_companies_table.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 255);
+            $table->string('email', 255)->unique();
+            $table->string('CIF', 255)->unique();
+            $table->string('location', 255);
+            $table->string('website', 255);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
