6f15eaa9cbe81030fd7cb49825b49c80fbffec38
diega <diegochiola@gmail.com>
2024-11-13 17:44:48 +0100

    Create first test to verify if JobOffer Model exist

---
+++ tests/Feature/Console/Commands/JobOfferCommandTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Console\Commands;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\JobOffer;
+
+class JobOfferCommandTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_job_offer_model_exists(): void
+    {
+        // Check if the JobOffer model exists
+        $this->assertTrue(class_exists(JobOffer::class));
+    }
+}
