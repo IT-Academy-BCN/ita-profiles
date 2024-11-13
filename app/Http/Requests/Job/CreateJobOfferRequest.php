e753bfd4bd72406a451246b4e8cf57be2bc8730b
diega <diegochiola@gmail.com>
2024-11-13 18:31:04 +0100

    Delete comments

--- app/Http/Requests/Job/CreateJobOfferRequest.php
+++ app/Http/Requests/Job/CreateJobOfferRequest.php
@@ -7,17 +7,17 @@ use Illuminate\Foundation\Http\FormRequest;
 class CreateJobOfferRequest extends FormRequest
 {
     /**
-     * Determina si el usuario está autorizado a realizar esta solicitud.
+     * 
      *
      * @return bool
      */
     public function authorize(): bool
     {
-        return true;  // Cambia esto si necesitas validar autorización
+        return true;  
     }
 
     /**
-     * Obtén las reglas de validación que se aplicarán a la solicitud.
+     * 
      *
      * @return array
      */
