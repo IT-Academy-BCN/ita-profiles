e6204259109ab51c47221f4707e537939b6a592a
omgchyah <rossana.liendo@gmail.com>
2024-11-13 13:23:48 +0100

    Created SendMessageController

---
+++ app/Http/Controllers/api/Mesagge/SendMessageController.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\api\Mesagge;
+
+use app\Models\Message;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\SendMessageRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class SendMessageController extends Controller
+{
+    //
+}
