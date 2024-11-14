09f1643f7ac63c971a589b5697ea521eeecf2c31
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-11-14 18:23:52 +0100

    all entities

--- database/factories/RecruiterFactory.php
+++ database/factories/RecruiterFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,9 @@ class RecruiterFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = \Faker\Factory::create();
         return [
-            //
+            'user_id' => User::inRandomOrder()->first()->id,
         ];
     }
 }
