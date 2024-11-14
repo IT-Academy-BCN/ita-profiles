5245f2e59b554eadd80cb1ab38d61234f011be2f
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-11-14 18:31:27 +0100

    seeders

--- database/seeders/RecruiterSeeder.php
+++ database/seeders/RecruiterSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Recruiter;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class RecruiterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Recruiter::factory()->count(5)->create();
     }
 }
