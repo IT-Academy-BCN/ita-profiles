9ef2db747ed7e0d61796439904d6b1a6d9c1762c
Amanda457 <mandy96lomi@gmail.com>
2024-11-15 12:04:38 +0100

    test: write test to create company by command

--- tests/Feature/Console/Commands/CreateCompanyByCommandTest.php
+++ tests/Feature/Console/Commands/CreateCompanyByCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\app\Http\Controllers\api\Company\CreateCompanyController;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Models\Company;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -12,7 +13,7 @@ class CreateCompanyByCommandTest extends TestCase
 {
    use DatabaseTransactions;
 
-   public function testCompanyModelExists()
+   public function testCompanyModelExists() : void
    {
        $this->assertTrue(class_exists(Company::class), "The Company model does not exist.");
    }
@@ -23,5 +24,26 @@ class CreateCompanyByCommandTest extends TestCase
 
        $this->assertInstanceOf(CreateCompanyController::class, $companyController);
    }
+
+   public function testCompanyCanBeCreatedViaCommand(): void
+   {
+       $this->artisan('create:company')
+           ->expectsQuestion('Nombre de la compañia:', 'Test Company')
+           ->expectsQuestion('Email:', 'test@test.es')
+           ->expectsQuestion('CIF:', 'B12345678')
+           ->expectsQuestion('Localizacion:', 'Test Location')
+           ->expectsQuestion('Pagina web:', 'https://test.com')
+           ->assertExitCode(0);
+
+       $this->assertDatabaseHas('companies', [
+           'name' => 'Test Company',
+           'email' => 'test@localhost',
+           'CIF' => 'B12345678',
+           'location' => 'Test Location',
+           'website' => 'https://test.com/',
+       ]);
+   }
+
+
 
 }
