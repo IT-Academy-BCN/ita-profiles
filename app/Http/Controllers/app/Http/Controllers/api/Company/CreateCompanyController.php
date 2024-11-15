9c7dd9cf797b6edab65fafff3881660e2928e3e0
diegdar <diegdarch@hotmail.com>
2024-11-15 11:55:01 +0100

    feat: :sparkles: Create controller

---
+++ app/Http/Controllers/app/Http/Controllers/api/Company/CreateCompanyController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\app\Http\Controllers\api\Company;
+
+use App\Http\Controllers\Controller;
+use App\Models\Company;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CreateCompanyController extends Controller
+{
+    public function __invoke($request)
+    {
+
+    }
+}
