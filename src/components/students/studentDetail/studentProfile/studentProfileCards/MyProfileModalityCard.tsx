4b345d7f07608ffa466bc47597111bcfe498fe0a
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-11-18 12:49:26 +0100

    [FE] modify: MyProfileModalityCard, MyProfileLanguagesCard, interfaces

--- src/components/students/studentDetail/studentProfile/studentProfileCards/MyProfileModalityCard.tsx
+++ src/components/students/studentDetail/studentProfile/studentProfileCards/MyProfileModalityCard.tsx
@@ -1,7 +1,9 @@
 import { v4 as uuidv4 } from 'uuid'
+import { useDispatch } from 'react-redux'
 import remoto from '../../../../../assets/svg/remoto.svg'
 import { useAppSelector } from '../../../../../hooks/ReduxHooks'
 import LoadingSpiner from '../../../../atoms/LoadingSpiner'
+import { toggleEditAdditionalInformation } from '../../../../../store/slices/student/languagesSlice'
 
 const MyProfileModalityCard: React.FC = () => {
     const {
@@ -10,10 +12,19 @@ const MyProfileModalityCard: React.FC = () => {
         isErrorModality,
     } = useAppSelector((state) => state.ShowStudentReducer.studentAdditionalModality)
 
+    const dispatch = useDispatch();
+
     return (
         <div className="flex flex-col gap-3" data-testid="ModalityCard">
             <div className='flex'>
                 <h3 className="font-bold text-lg">Modalidad</h3>
+                <button
+                    type='button'
+                    className='ml-auto'
+                    onClick={() => dispatch(toggleEditAdditionalInformation())}
+                >
+                    <img src={Pencil} alt="edit modality information" />
+                </button>
             </div>
             {isLoadingModality && <LoadingSpiner />}
             {isErrorModality && (
