18d9aa37ce672fb59c82d498b51c71df0ea50413
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-11-19 11:53:40 +0100

    [FE] modify: Modify slice and the corresponding test

---
+++ src/store/thunks/updateProfileLanguagesThunk.ts
@@ -0,0 +1,27 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { resumes } from '../../api/student/getStudentDetailWithId';
+import { TLanguage } from '../../interfaces/interfaces';
+
+type DataFetch = {
+  name: string,
+  level: string
+}
+export const updateProfileLanguagesThunk = createAsyncThunk(
+  "updateProfileLanguagesThunk",
+  async (payload: TLanguage[]) => {
+
+    const data: DataFetch = {
+      "name": payload[0].name,
+      "level": payload[0].level
+    }
+    const axiosPost = await axios.post(`//localhost:8000/api/v1/student/${localStorage.getItem("studentID")}/resume/${resumes.languages}`, data, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        'Accept': 'application/json'
+      }
+    })
+
+    return axiosPost.data.message
+  }
+)
