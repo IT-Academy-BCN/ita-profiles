50d1336c1cdec0935bcc974292ef095ace29a362
Amanda457 <mandy96lomi@gmail.com>
2024-11-20 11:14:10 +0100

    command: config command --help

--- app/Console/Commands/CreateCompany.php
+++ app/Console/Commands/CreateCompany.php
@@ -8,22 +8,27 @@ use App\Http\Controllers\api\Company\CreateCompanyController;
 use App\Http\Requests\StoreCompanyRequest;
 use Illuminate\Console\Command;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\Console\Input\InputArgument;
 
 class CreateCompany extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'create:company';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Create a new company with name, email, CIF, location, and website';
+    protected function configure()
+    {
+        $this->setName('create:company')
+            ->addArgument('name', InputArgument::OPTIONAL, 'The name of the company')
+            ->addArgument('email', InputArgument::OPTIONAL, 'The email address of the company')
+            ->addArgument('CIF', InputArgument::OPTIONAL, 'The unique CIF of the company')
+            ->addArgument('location', InputArgument::OPTIONAL, 'The physical location of the company')
+            ->addArgument('website', InputArgument::OPTIONAL, 'The company\'s website URL -OPTIONAL-');
+    }
+    
+    protected $description =('Create a new company in the database giving the required arguments step by step in terminal.'.PHP_EOL.'  Example:
+        name: It Academy
+        email: itacademy@test.es
+        CIF: A1234567Z
+        location: Barcelona
+        website: https://itacademy.barcelonactiva.cat/');
+             
 
     /**
      * The CreateCompanyController instance.
@@ -90,8 +95,8 @@ class CreateCompany extends Command
         $request = new StoreCompanyRequest();
         $request->merge($data);
         $request->setContainer(app())
-                ->setRedirector(app('redirect'))
-                ->validateResolved();
+            ->setRedirector(app('redirect'))
+            ->validateResolved();
         return $request;
     }
 
@@ -120,5 +125,5 @@ class CreateCompany extends Command
                 $this->error("Error en el campo {$field}: {$message}");
             }
         }
-    }
+    }    
 }
