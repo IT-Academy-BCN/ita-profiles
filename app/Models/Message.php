e03a53abb882580753440b761d150aa3cdf587ed
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-11-20 18:36:20 +0100

    Messages HasUuids

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Message extends Model
 {
     use HasFactory;
+    use HasUuids;
 
     protected $fillable = [
         'receiver_id',
