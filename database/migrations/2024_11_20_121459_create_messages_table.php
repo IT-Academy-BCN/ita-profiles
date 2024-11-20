8987e61783f8021c9f99ae0cc8b66096cf849a48
Rossana Liendo <rossana.liendo@gmail.com>
2024-11-20 13:17:52 +0100

    Added messages table

---
+++ database/migrations/2024_11_20_121459_create_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->uuid();
+            $table->uuid('receiver');
+            $table->boolean('read')->default(false);
+            $table->string('subject');
+            $table->text('body');
+            $table->timestamps();
+
+            $table->foreign('receiver')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
