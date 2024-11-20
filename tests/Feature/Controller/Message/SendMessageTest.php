3670ed296256825f9beefd3194fa21416a3531b4
omgchyah <rossana.liendo@gmail.com>
2024-11-20 11:56:29 +0100

    added SetUp method to my tests

--- tests/Feature/Controller/Message/SendMessageTest.php
+++ tests/Feature/Controller/Message/SendMessageTest.php
@@ -13,64 +13,63 @@ class SendMessageTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $sender;
+    private $receiver;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Create users for tests
+        $this->sender = User::factory()->create();
+        $this->receiver = User::factory()->create();
+
+        // Authenticate the sender
+        Passport::actingAs($this->sender);
+    }
+
     public function test_user_can_send_message_successfully()
     {
-        $sender = User::factory()->create();
-        $receiver = User::factory()->create();
-
-        Passport::actingAs($sender);
-        
         $response = $this->postJson(route('message.send'), [
             'subject' => 'Hello!',
             'body' => 'This is a test message.',
-            'receiver' => $receiver->id,
+            'receiver' => $this->receiver->id,
         ]);
 
         $response->assertStatus(200);
 
         $this->assertDatabaseHas('messages', [
-            'receiver' => $receiver->id,
+            'receiver' => $this->receiver->id,
             'subject' => 'Hello!',
             'body' => 'This is a test message.',
             'read' => false,
         ]);
     }
+
     public function test_message_requires_subject_and_body_fields()
     {
-        $sender = User::factory()->create();
-        $receiver = User::factory()->create();
-
-        Passport::actingAs($sender);
-
         $response = $this->postJson(route('message.send'), [
-            'receiver' => $receiver->id,
+            'receiver' => $this->receiver->id,
         ]);
 
         $response->assertStatus(422);
         $response->assertJsonValidationErrors(['subject', 'body']);
     }
+
     public function test_subject_exceeds_255_characters()
     {
-        $sender = User::factory()->create();
-        $receiver = User::factory()->create();
-
-        Passport::actingAs($sender);
-
         $response = $this->postJson(route('message.send'), [
             'subject' => str_repeat('A', 256),
             'body' => 'This is a test message.',
-            'receiver' => $receiver->id,
+            'receiver' => $this->receiver->id,
         ]);
 
         $response->assertStatus(422);
         $response->assertJsonValidationErrors(['subject']);
     }
+
     public function test_receiver_is_invalid()
     {
-        $sender = User::factory()->create();
-
-        Passport::actingAs($sender);
-
         $response = $this->postJson(route('message.send'), [
             'subject' => 'Invalid Receiver Test',
             'body' => 'Testing receiver validation.',
@@ -80,37 +79,28 @@ class SendMessageTest extends TestCase
         $response->assertStatus(422);
         $response->assertJsonValidationErrors(['receiver']);
     }
+
     public function test_body_field_is_empty()
     {
-        $sender = User::factory()->create();
-        $receiver = User::factory()->create();
-
-        $this->actingAs($sender);
-
         $response = $this->postJson(route('message.send'), [
             'subject' => 'Empty Body Test',
             'body' => '',
-            'receiver' => $receiver->id,
+            'receiver' => $this->receiver->id,
         ]);
 
         $response->assertStatus(422);
         $response->assertJsonValidationErrors(['body']);
     }
+
     public function test_subject_field_is_empty()
     {
-        $sender = User::factory()->create();
-        $receiver = User::factory()->create();
-
-        Passport::actingAs($sender);
-
         $response = $this->postJson(route('message.send'), [
             'subject' => '',
             'body' => 'This is a message with an empty subject.',
-            'receiver' => $receiver->id,
+            'receiver' => $this->receiver->id,
         ]);
 
         $response->assertStatus(422);
         $response->assertJsonValidationErrors(['subject']);
     }
-      
 }
