5a4d95e2c8d85abc70d44782a03e5112488776e9
Tomi <tomas.casabona@gmail.com>
2024-11-21 11:46:42 +0100

    feature: add feedback message with react toastify

---
+++ src/utils/toastFeedbackMessages.ts
@@ -0,0 +1,39 @@
+
+import { toast } from 'react-toastify'
+
+export const toastError = (message: string) => {
+    toast.error(message, {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+    })
+}
+export const toastSuccess = (message: string) => {
+    toast.success(message, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+    })
+}
+export const toastLoading = (message: string) => {
+    toast.info(message, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+    })
+}
