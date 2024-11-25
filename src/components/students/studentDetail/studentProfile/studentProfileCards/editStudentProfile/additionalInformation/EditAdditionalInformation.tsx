42d2ef7813d507e766195ff7fc712f04b38df915
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-11-25 10:03:58 +0100

    fix: I clean up the code

--- src/components/students/studentDetail/studentProfile/studentProfileCards/editStudentProfile/additionalInformation/EditAdditionalInformation.tsx
+++ src/components/students/studentDetail/studentProfile/studentProfileCards/editStudentProfile/additionalInformation/EditAdditionalInformation.tsx
@@ -33,8 +33,8 @@ export const fetchChanges = async (langs: TLanguage[]): Promise<string> => {
     }
     const peticion = {
         url: `http://127.0.0.1:8000/api/v1/student/${localStorage.getItem(
-                'studentID',
-            )}/resume/languages`,
+            'studentID',
+        )}/resume/languages`,
         formData: data
     }
 
@@ -45,7 +45,7 @@ export const fetchChanges = async (langs: TLanguage[]): Promise<string> => {
 export const EditAdditionalInformation: FC = () => {
     const dispacth = useDispatch()
     const dispatchThunk = useAppDispatch()
-    const { languagesData, isOpenEditAdditionalInformation, notification, isErrorUpdateLanguages, isLoadingUpdateLanguages} = useAppSelector(
+    const { languagesData, isOpenEditAdditionalInformation, notification, isErrorUpdateLanguages, isLoadingUpdateLanguages } = useAppSelector(
         (state) => state.ShowStudentReducer.studentLanguages,
     )
 
@@ -57,26 +57,30 @@ export const EditAdditionalInformation: FC = () => {
         availableLanguages,
         deleteLanguage,
         editLanguage,
-        
+
     } = useUpdateLanguageHook(languagesData)
 
-  // TODDO: Refactor
-  const { modality } = useAppSelector(state => state.ShowStudentReducer.studentAdditionalModality)
+    const { modality } = useAppSelector(
+        (state) => state.ShowStudentReducer.studentAdditionalModality,
+    )
 
-  const refInput = useRef<HTMLInputElement>(null);
-  // const [selectOptionValue, setSelectOptionValue] = useState<string>();
-  const refDialog = useRef<HTMLDialogElement>(null);
+    const refInput = useRef<HTMLInputElement>(null)
+    const refDialog = useRef<HTMLDialogElement>(null)
 
-
-  const onInputLanguage = (event: ChangeEvent<HTMLInputElement>) => {
-    // TODDO: Implemetar 
-    globalThis.alert(JSON.stringify({ language: event.target.value, lenguaje: event.target.id }, null, 4));
-  }
-  const handlerSelect = () => {
-    if (refDialog.current) {
-      console.log(refDialog.current.open = !refDialog.current.open)
+    const onInputLanguage = (event: ChangeEvent<HTMLInputElement>) => {
+        globalThis.alert(
+            JSON.stringify(
+                { language: event.target.value, lenguaje: event.target.id },
+                null,
+                4,
+            ),
+        )
     }
-  }
+    const handlerSelect = () => {
+        if (refDialog.current) {
+            console.log((refDialog.current.open = !refDialog.current.open))
+        }
+    }
 
     const saveChanges = async () => {
 
@@ -85,27 +89,37 @@ export const EditAdditionalInformation: FC = () => {
 
         setTimeout(() => {
             dispacth(resetUpdateLanguages())
-         
-          }, 6000)
+
+        }, 6000)
     }
 
-  if (isOpenEditAdditionalInformation) {
-    return (
+    if (isOpenEditAdditionalInformation) {
+        return (
+            <div className="fixed w-full h-full flex items-center justify-center top-0 left-0 bg-[rgba(0,0,0,.7)]">
+                <button
+                    aria-label="ref-modal"
+                    type="button"
+                    ref={refBtnModal}
+                    onMouseEnter={handleFocusOnMouseEnter}
+                    onKeyUp={handleCloseModalKeyDown}
+                    onKeyDown={handleCloseModalKeyDown}
+                    onClick={handleCloseModal}
+                    className="fixed flex w-full h-[100vh] top-0 left-0 z-20 cursor-default"
+                />
 
-      <section className="fixed w-full h-full flex items-center justify-center top-0 left-0 bg-[rgba(0,0,0,.7)]">
+                <div className="flex flex-col  bg-white rounded-xl z-20">
+                    <div className="flex justify-between mx-4 mt-4 mb-2">
+                        <div />
+                        <button
+                            type="button"
+                            onClick={handleCloseModal}
+                        >
+                            <img src={Close} alt="close icon" />
+                        </button>
+                    </div>
 
-        <button aria-label="ref-modal"
-          type="button"
-          ref={refBtnModal}
-          onMouseEnter={handleFocusOnMouseEnter}
-          onKeyUp={handleCloseModalKeyDown}
-          onKeyDown={handleCloseModalKeyDown}
-          onClick={handleCloseModal}
-          className="fixed flex w-full h-[100vh] top-0 left-0 z-20 cursor-default" >
-          <strong className="text-red-500 absolute top-28 right-28 z-20">X</strong>
-        </button>
 
-        <article className="flex flex-col gap-4 justify-around p-4 max-w-[376px] bg-stone-200 select-none z-20">
+                    <div className='flex flex-col gap-8 pb-8'>
 
                         <div className="flex justify-between items-center mx-8 relative">
                             <h1 className="text-2xl my-0 font-bold text-[rgba(40,40,40,1)]">
@@ -113,9 +127,9 @@ export const EditAdditionalInformation: FC = () => {
                             </h1>
                             {isErrorUpdateLanguages && <h3 className="py-0 text-red-500 text-sm absolute top-full left-0 w-full h-auto animate-pulse text-[.7em]">{notification !== null && notification.message}</h3>}
                             {isLoadingUpdateLanguages && <h3 className="py-0 text-red-500 text-sm absolute top-full left-0 w-full h-auto animate-pulse text-[.7em]">{notification !== null && notification.message}</h3>}
-                            {notification.message !== null  && <h3 className="py-0 text-red-500 text-sm absolute top-full left-0 w-full h-auto animate-pulse text-[.7em]">{notification.message}</h3>}
+                            {notification.message !== null && <h3 className="py-0 text-red-500 text-sm absolute top-full left-0 w-full h-auto animate-pulse text-[.7em]">{notification.message}</h3>}
                         </div>
-                        
+
                         <div>
                             <div className="flex justify-between items-center mx-8 relative">
                                 <h2 className="font-bold">
@@ -129,11 +143,11 @@ export const EditAdditionalInformation: FC = () => {
                                 >
                                     +
                                 </button>
-                                
+
                                 <dialog
                                     ref={refDialog}
                                     className="top-8 left-40 z-20 h-80 w-2/3 bg-stone-200 rounded-xl overflow-hidden">
-                                        
+
                                     <form className="flex flex-col h-full overflow-y-auto z-11 p-2">
                                         {availableLanguages.map((language, index) => (
                                             <label
@@ -192,10 +206,10 @@ export const EditAdditionalInformation: FC = () => {
                                             />
                                         </label>
                                     </div>
-                                </dialog>                                
+                                </dialog>
                             </div>
                             <div className='my-4 border-b-2 border-gray-300 border-dashed'>
-                                        <span className="hidden">border-dashed</span>
+                                <span className="hidden">border-dashed</span>
                             </div>
                             <DragAndDropLanguages
                                 dropLanguages={updateLanguages}
@@ -204,6 +218,26 @@ export const EditAdditionalInformation: FC = () => {
                             />
                         </div>
 
+                        <div className='mx-8'>
+                            <EditModality additionalTraining={modality} />
+                        </div>
+
+                        <div className="flex gap-4 justify-between mx-8">
+                            <button
+                                type="button"
+                                className="flex-1 h-[63px] rounded-xl font-bold border border-[rgba(128,128,128,1)] text-[rgba(128,128,128,1)]"
+                                onClick={handleCloseModal}>
+                                Cancelar
+                            </button>
+                            <button
+                                type="button"
+                                onClick={saveChanges}
+                                className="flex-1 h-[63px] rounded-xl bg-primary font-bold text-white"
+                            >
+                                Aceptar
+                            </button>
+                        </div>
+                    </div>
                         <div className='mx-8'>
                             <EditModality additionalTraining={modality} />
                         </div>
