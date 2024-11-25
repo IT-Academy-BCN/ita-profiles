d6fd9dae809f2e7f145639919e88544a987e629e
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-11-25 10:03:58 +0100

    fix: I clean up the code

--- src/components/students/studentDetail/studentProfile/studentProfileCards/editStudentProfile/additionalInformation/DragAndDropLanguages.tsx
+++ src/components/students/studentDetail/studentProfile/studentProfileCards/editStudentProfile/additionalInformation/DragAndDropLanguages.tsx
@@ -1,6 +1,9 @@
-import { FC } from 'react';
-import { TLanguage, TLanguageLevel } from '../../../../../../../interfaces/interfaces';
-import { useDragAndDropLanguagesHook } from '../../../../../../../hooks/useDragAndDropLanguagesHook';
+import { FC } from 'react'
+import {
+    TLanguage,
+    TLanguageLevel,
+} from '../../../../../../../interfaces/interfaces'
+import { useDragAndDropLanguagesHook } from '../../../../../../../hooks/useDragAndDropLanguagesHook'
 
 export type TDragAndDropLanguagesProps = {
   dropLanguages: TLanguage[],
@@ -14,45 +17,58 @@ const DragAndDropLanguages: FC<TDragAndDropLanguagesProps> = ({ dropLanguages, d
 
   const { updateLanguagesDrop, handleDragStart, handleDragOver, handleDrop } = useDragAndDropLanguagesHook(dropLanguages)
 
-  return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-      <div>
-        <div>
+    return (
+        <div className='flex flex-col gap-4'>
+            {updateLanguagesDrop.map((language, index) => (
+                <div
+                    key={language.id}
+                    draggable
+                    onDragStart={() => handleDragStart(index)}
+                    onDragOver={handleDragOver}
+                    onDrop={() => handleDrop(index)}
+                    className="cursor-move pb-4 border-b-2 border-gray-300 border-dashed">
 
-          {updateLanguagesDrop.map((language, index) => (
-            <div
-              key={language.id}
-              draggable
-              onDragStart={() => handleDragStart(index)}
-              onDragOver={handleDragOver}
-              onDrop={() => handleDrop(index)}
-              className='p-2 m-[5px 0] cursor-move border-2 border-gray-900'
-            >
-              <div key={language.id} className="p-1 flex flex-col gap-4">
-                <div className="p-1 flex items-center">
-                  <div>
-                    <div>{language.name}</div>
-                  </div>
-                  <button className="py-1 px-2 text-sm" type="button" onClick={() => deleteLanguage(language.id)}>Eliminar idioma</button>
+                    <div
+                        key={language.id}
+                        className="flex flex-col gap-4 mx-8">
+
+                        <div className="flex items-center">
+                            <div className='font-semibold'>
+                                {language.name}
+                            </div>
+                            <button
+                                className="pl-2 text-primary font-medium"
+                                type="button"
+                                onClick={() => deleteLanguage(language.id)}
+                            >
+                                Eliminar idioma
+                            </button>
+                        </div>
+                        <div className="flex gap-4">
+                            {levels.map((level) => (
+                                <label key={`level-${level}`}>
+                                    <input
+                                        type="radio"
+                                        id={`level-${level}`}
+                                        name={`level-${level}`}
+                                        defaultValue={level}
+                                        checked={
+                                            language.level === level
+                                        }
+                                        onChange={() =>
+                                            editLanguage({
+                                                ...language,
+                                                level,
+                                            })
+                                        }
+                                    />
+                                    <span className='ml-2'>{level}</span>
+                                </label>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-                <div className="p-1">
-                  {levels.map((level) => (
-                    <label key={`level-${level}`}>
-                      <strong>{level}</strong>
-                      <input
-                        type="radio"
-                        id={`level-${level}`}
-                        name={`level-${level}`}
-                        defaultValue={level}
-                        checked={language.level === level}
-                        onChange={() => editLanguage({ ...language, level })}
-                      />
-                    </label>
-                  ))}
-                </div>
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
--- src/components/students/studentDetail/studentProfile/studentProfileCards/editStudentProfile/additionalInformation/EditAdditionalInformation.tsx
+++ src/components/students/studentDetail/studentProfile/studentProfileCards/editStudentProfile/additionalInformation/EditAdditionalInformation.tsx
@@ -28,8 +28,8 @@ export const fetchChanges = async (langs: TLanguage[]): Promise<string> => {
     }
     const peticion = {
         url: `http://127.0.0.1:8000/api/v1/student/${localStorage.getItem(
-                'studentID',
-            )}/resume/languages`,
+            'studentID',
+        )}/resume/languages`,
         formData: data
     }
 
@@ -40,7 +40,7 @@ export const fetchChanges = async (langs: TLanguage[]): Promise<string> => {
 export const EditAdditionalInformation: FC = () => {
     const dispacth = useDispatch()
     const dispatchThunk = useAppDispatch()
-    const { languagesData, isOpenEditAdditionalInformation, notification, isErrorUpdateLanguages, isLoadingUpdateLanguages} = useAppSelector(
+    const { languagesData, isOpenEditAdditionalInformation, notification, isErrorUpdateLanguages, isLoadingUpdateLanguages } = useAppSelector(
         (state) => state.ShowStudentReducer.studentLanguages,
     )
 
@@ -51,26 +51,30 @@ export const EditAdditionalInformation: FC = () => {
         availableLanguages,
         deleteLanguage,
         editLanguage,
-        
+
     } = useUpdateLanguageHook(languagesData)
 
-  // TODDO: Refactor
-  const { modality } = useAppSelector(state => state.ShowStudentReducer.studentAdditionalModality)
+    const { modality } = useAppSelector(
+        (state) => state.ShowStudentReducer.studentAdditionalModality,
+    )
 
-  const refInput = useRef<HTMLInputElement>(null);
-  // const [selectOptionValue, setSelectOptionValue] = useState<string>();
-  const refDialog = useRef<HTMLDialogElement>(null);
+    const refInput = useRef<HTMLInputElement>(null)
+    const refDialog = useRef<HTMLDialogElement>(null)
 
-
-  const onInputLanguage = (event: ChangeEvent<HTMLInputElement>) => {
-    // TODDO: Implemetar 
-    globalThis.alert(JSON.stringify({ language: event.target.value, lenguaje: event.target.id }, null, 4));
-  }
-  const handlerSelect = () => {
-    if (refDialog.current) {
-      console.log(refDialog.current.open = !refDialog.current.open)
+    const onInputLanguage = (event: ChangeEvent<HTMLInputElement>) => {
+        globalThis.alert(
+            JSON.stringify(
+                { language: event.target.value, lenguaje: event.target.id },
+                null,
+                4,
+            ),
+        )
     }
-  }
+    const handlerSelect = () => {
+        if (refDialog.current) {
+            console.log((refDialog.current.open = !refDialog.current.open))
+        }
+    }
 
     const saveChanges = async () => {
 
@@ -79,27 +83,37 @@ export const EditAdditionalInformation: FC = () => {
 
         setTimeout(() => {
             dispacth(resetUpdateLanguages())
-         
-          }, 6000)
+
+        }, 6000)
     }
 
-  if (isOpenEditAdditionalInformation) {
-    return (
+    if (isOpenEditAdditionalInformation) {
+        return (
+            <div className="fixed w-full h-full flex items-center justify-center top-0 left-0 bg-[rgba(0,0,0,.7)]">
+                <button
+                    aria-label="ref-modal"
+                    type="button"
+                    ref={refBtnModal}
+                    onMouseEnter={handleFocusOnMouseEnter}
+                    onKeyUp={handleCloseModalKeyDown}
+                    onKeyDown={handleCloseModalKeyDown}
+                    onClick={handleCloseModal}
+                    className="fixed flex w-full h-[100vh] top-0 left-0 z-20 cursor-default"
+                />
 
-      <section className="fixed w-full h-full flex items-center justify-center top-0 left-0 bg-[rgba(0,0,0,.7)]">
+                <div className="flex flex-col  bg-white rounded-xl z-20">
+                    <div className="flex justify-between mx-4 mt-4 mb-2">
+                        <div />
+                        <button
+                            type="button"
+                            onClick={handleCloseModal}
+                        >
+                            <img src={Close} alt="close icon" />
+                        </button>
+                    </div>
 
-        <button aria-label="ref-modal"
-          type="button"
-          ref={refBtnModal}
-          onMouseEnter={handleFocusOnMouseEnter}
-          onKeyUp={handleCloseModalKeyDown}
-          onKeyDown={handleCloseModalKeyDown}
-          onClick={handleCloseModal}
-          className="fixed flex w-full h-[100vh] top-0 left-0 z-20 cursor-default" >
-          <strong className="text-red-500 absolute top-28 right-28 z-20">X</strong>
-        </button>
 
-        <article className="flex flex-col gap-4 justify-around p-4 max-w-[376px] bg-stone-200 select-none z-20">
+                    <div className='flex flex-col gap-8 pb-8'>
 
                         <div className="flex justify-between items-center mx-8 relative">
                             <h1 className="text-2xl my-0 font-bold text-[rgba(40,40,40,1)]">
@@ -107,9 +121,9 @@ export const EditAdditionalInformation: FC = () => {
                             </h1>
                             {isErrorUpdateLanguages && <h3 className="py-0 text-red-500 text-sm absolute top-full left-0 w-full h-auto animate-pulse text-[.7em]">{notification !== null && notification.message}</h3>}
                             {isLoadingUpdateLanguages && <h3 className="py-0 text-red-500 text-sm absolute top-full left-0 w-full h-auto animate-pulse text-[.7em]">{notification !== null && notification.message}</h3>}
-                            {notification.message !== null  && <h3 className="py-0 text-red-500 text-sm absolute top-full left-0 w-full h-auto animate-pulse text-[.7em]">{notification.message}</h3>}
+                            {notification.message !== null && <h3 className="py-0 text-red-500 text-sm absolute top-full left-0 w-full h-auto animate-pulse text-[.7em]">{notification.message}</h3>}
                         </div>
-                        
+
                         <div>
                             <div className="flex justify-between items-center mx-8 relative">
                                 <h2 className="font-bold">
@@ -123,11 +137,11 @@ export const EditAdditionalInformation: FC = () => {
                                 >
                                     +
                                 </button>
-                                
+
                                 <dialog
                                     ref={refDialog}
                                     className="top-8 left-40 z-20 h-80 w-2/3 bg-stone-200 rounded-xl overflow-hidden">
-                                        
+
                                     <form className="flex flex-col h-full overflow-y-auto z-11 p-2">
                                         {availableLanguages.map((language, index) => (
                                             <label
@@ -186,10 +200,10 @@ export const EditAdditionalInformation: FC = () => {
                                             />
                                         </label>
                                     </div>
-                                </dialog>                                
+                                </dialog>
                             </div>
                             <div className='my-4 border-b-2 border-gray-300 border-dashed'>
-                                        <span className="hidden">border-dashed</span>
+                                <span className="hidden">border-dashed</span>
                             </div>
                             <DragAndDropLanguages
                                 dropLanguages={updateLanguages}
@@ -198,14 +212,26 @@ export const EditAdditionalInformation: FC = () => {
                             />
                         </div>
 
-          <EditModality additionalTraining={modality} />
+                        <div className='mx-8'>
+                            <EditModality additionalTraining={modality} />
+                        </div>
 
-          <footer className="p-1 flex justify-between">
-            <button type="button" className="bg-blue-400 py-2 px-4">Cancelar</button>
-            <button type="button" onClick={saveChanges} className="bg-red-400 py-2 px-4">Aceptar</button>
-          </footer>
-
-        </article>
+                        <div className="flex gap-4 justify-between mx-8">
+                            <button
+                                type="button"
+                                className="flex-1 h-[63px] rounded-xl font-bold border border-[rgba(128,128,128,1)] text-[rgba(128,128,128,1)]"
+                                onClick={handleCloseModal}>
+                                Cancelar
+                            </button>
+                            <button
+                                type="button"
+                                onClick={saveChanges}
+                                className="flex-1 h-[63px] rounded-xl bg-primary font-bold text-white"
+                            >
+                                Aceptar
+                            </button>
+                        </div>
+                    </div>
 
       </section>
     )
