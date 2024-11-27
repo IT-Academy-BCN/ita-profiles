f948e24a8877afb9531fa202f75a62b2eaf16aaf
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-11-27 14:45:20 +0100

    refactor: replace 'tokenable_id' and 'tokenable_type' by morphs('tokenable')

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -10,8 +10,7 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->string('tokenable_type');
-            $table->unsignedBigInteger('tokenable_id');
+            $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
