b796040c30dfd708ca6aca93d553b884d7a73fbe
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-12-03 10:43:14 +0100

    create: The modal test

---
+++ src/__tests__/components/atoms/Modal.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { vi } from "vitest";
+import Modal from "../../../components/atoms/modal/Modal";
+
+
+describe("Modal Component", () => {
+
+  // Se tiene que ver cuando esté abierto
+  test("You have to see it when it's open", () => {
+    render(
+      <Modal isOpen onClose={() => { }}>
+        <div>Test Modal Content</div>
+      </Modal>
+    );
+    expect(screen.getByText("Test Modal Content")).toBeInTheDocument();
+  });
+
+  // No se debería ver cuando esté cerrado
+  test("It should not be visible when closed.", () => {
+    const { queryByText } = render(
+      <Modal isOpen={false} onClose={() => { }}>
+        <div>Test Modal Content</div>
+      </Modal>
+    );
+    expect(queryByText("Test Modal Content")).toBeNull();
+  });
+
+  // Llama a onClose cuando se hace clic en la superposición
+  test("calls onClose when overlay is clicked", () => {
+    const mockOnClose = vi.fn();
+    render(
+      <Modal isOpen onClose={mockOnClose}>
+        <div>Test Modal Content</div>
+      </Modal>
+    );
+
+    fireEvent.click(screen.getByRole("dialog"));
+    expect(mockOnClose).toHaveBeenCalled();
+  });
+});
