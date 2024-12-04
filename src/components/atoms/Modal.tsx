5cf40d350d29c25bf5340b6d193634e0fc531a0f
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-12-04 13:40:51 +0100

    fix: Passing Tailwind styles to constants

--- src/components/atoms/Modal.tsx
+++ src/components/atoms/Modal.tsx
@@ -6,6 +6,10 @@ type TModal = {
   children: ReactNode
 }
 
+const overlayStyle = "fixed top-0 left-0 w-full h-full bg-[rgba(0, 0, 0, 0.8)] flex justify-center items-center";
+const contentStyle = "bg-white p-4 rounded-md relative";
+const buttonStyle = "absolute top-3 right-3 bg-none border-none text-xl cursor-pointer"
+
 const Modal: FC<TModal> = ({ children, isOpen, onClose }) => {
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
@@ -24,13 +28,13 @@ const Modal: FC<TModal> = ({ children, isOpen, onClose }) => {
       aria-label="modal-overlay"
       tabIndex={0}
       onKeyDown={handleKeyDown}
-      className="fixed top-0 left-0 w-full h-full bg-[rgba(0, 0, 0, 0.8)] flex justify-center items-center">
+      className={overlayStyle}>
 
       <div role="button" aria-label="modal-content" tabIndex={0} onKeyDown={(e) => {
         handleKeyDown(e);
         e.stopPropagation();
-      }} className="bg-white p-4 rounded-md relative" onClick={(e) => e.stopPropagation()}>
-        <button type="button" aria-label="modal-close" className="absolute top-3 right-3 bg-none border-none text-xl cursor-pointer" onClick={onClose}>&times;</button>
+      }} className={contentStyle} onClick={(e) => e.stopPropagation()}>
+        <button type="button" aria-label="modal-close" className={buttonStyle} onClick={onClose}>&times;</button>
         {children}
       </div>
 
