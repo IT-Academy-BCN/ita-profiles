696e922b25b41c31a9614f83111c5543f25b5702
diega <diegochiola@gmail.com>
2024-12-04 10:45:29 +0100

    ✅ Implement JobOfferController and pass JobOfferControllerTest 📝

---
+++ app/Http/Controllers/api/Job/JobOfferController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\api\Job;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Job\CreateJobOfferRequest;
+use App\Models\JobOffer;
+
+
+class JobOfferController extends Controller
+{
+
+    public function createJobOffer(CreateJobOfferRequest $request)
+    {
+        $jobOffer = JobOffer::create($request->validated());
+
+        return response()->json(['message' => 'Oferta de feina creada amb èxit', 'jobOffer' => $jobOffer], 201);
+    }
+}
