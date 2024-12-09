b7464f9b8bcf445582e92a41b080c4e160801c52
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-12-09 23:11:50 +0100

    feature: update StudentListSeeder

--- database/seeders/StudentListSeeder.php
+++ database/seeders/StudentListSeeder.php
@@ -10,8 +10,27 @@ class StudentListSeeder extends Seeder
 {
     public function run(): void
     {
-
         $resumes = Resume::factory()->count(30)->create();
+
+        $resumes->first()->update([
+            'github_url' => 'https://github.com/IT-Academy-BCN',
+        ]);
+
+        $firstUser = $resumes->first()->student->user;
+        if ($firstUser) {
+            $firstUser->update([
+                'dni' => '48332312C',
+                'password' => 'passOnePass',
+            ]);
+        }
+
+        $secondUser = $resumes->skip(1)->first()->student->user;
+        if ($secondUser) {
+            $secondUser->update([
+                'dni' => 'Y4527507V',
+                'password' => 'passOnePass',
+            ]);
+        }
 
         foreach($resumes as $resume){
             $collaborations = Collaboration::factory()->count(2)->create();
