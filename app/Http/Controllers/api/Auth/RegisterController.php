b466b1b93eba91a8517e0fbc1645a5f6ef057039
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-12-10 14:33:44 +0100

    refactor: remove service and integrate logic to controller

--- app/Http/Controllers/api/Auth/RegisterController.php
+++ app/Http/Controllers/api/Auth/RegisterController.php
@@ -7,11 +7,13 @@ namespace App\Http\Controllers\api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use App\Models\{
     Resume,
     Student,
     User,
 };
+use Illuminate\Support\Facades\Log;
 
 class RegisterController extends Controller
 {
@@ -20,11 +22,35 @@ class RegisterController extends Controller
         $userData = $request->validated();
 
         try {
-            $result = $this->userRegisterService->registerUser($userData);
+            $result = DB::transaction(function () use ($userData) {
+                // Crear usuario
+                $userData['password'] = bcrypt($userData['password']);
+                $user = User::create($userData);
+
+                // Crear estudiante relacionado
+                $student = Student::create(['user_id' => $user->id]);
+
+                // Crear currículum relacionado
+                Resume::create([
+                    'student_id' => $student->id,
+                    'specialization' => $userData['specialization'] ?? null,
+                ]);
+
+                // Generar token de acceso
+                return [
+                    'token' => $user->createToken('ITAcademy')->accessToken,
+                    'email' => $user->email,
+                ];
+            });
 
             return response()->json($result, 201);
         } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
+            Log::error('Error during user registration:', [
+                'exception' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+
+            return response()->json(['error' => 'An error occurred.'], 500);
         }
     }
 }
