4ebda28d155b16215a5112d8faab4f4481b4e923
[Sergi López] <socelsergi@gmail.com>
2024-12-12 19:06:51 +0100

    Simplified  specializationListController into collection to filter and present values. Also refactored tests

--- app/Http/Controllers/api/Student/SpecializationListController.php
+++ app/Http/Controllers/api/Student/SpecializationListController.php
@@ -5,26 +5,15 @@ declare(strict_types=1);
 namespace App\Http\Controllers\api\Student;
 
 use App\Http\Controllers\Controller;
-use App\Service\Student\SpecializationListService;
+use App\Http\Resources\SpecializationListCollection;
+use App\Models\Resume;
 use Illuminate\Http\JsonResponse;
-use Exception;
 
 class SpecializationListController extends Controller
 {
-    private SpecializationListService $specializationListService;
-
-    public function __construct(SpecializationListService $specializationListService) {
-
-        $this->specializationListService = $specializationListService;
-    }
-
-    public function __invoke(): jsonResponse
+    public function __invoke(): JsonResponse
     {
-        try {
-            $service = $this->specializationListService->execute();
-            return response()->json($service);
-        } catch (Exception $e) {
-            return response()->json(['message' => $e->getMessage()], $e->getCode() ?: 500);
-        }
+        $specializations = $student->resume?->specialization ?? collect();
+        return response()->json(new SpecializationListCollection($specializations));
     }
 }
