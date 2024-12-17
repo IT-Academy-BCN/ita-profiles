283407d3a195f0c2ccc05a9f29666a21c85bc566
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-12-17 13:05:29 +0100

    Modifying and cleaning up the comment code

--- src/api/FetchStudentsList.ts
+++ src/api/FetchStudentsList.ts
@@ -1,16 +1,20 @@
-import { TStudent } from "../../types";
-import api from "./api";
+import api from './api';
 
-export const FetchStudentsList = async (selectedRoles: Array<string> = []): Promise<TStudent[]> => {
+export const FetchStudentsList = async (selectedRoles: Array<string> = []) => {
+  try {
+    let queryParams = '';
 
-  let queryParams = '';
+    if (selectedRoles.length > 0) {
+      queryParams += `specialization=${selectedRoles.join(',')}`;
+    }
 
-  if (selectedRoles.length > 0) {
-    queryParams += `specialization=${selectedRoles.join(',')}`;
+    const url = `/student/resume/list${queryParams ? `?${queryParams}` : ''}`;
+
+    const response = await api.get(url);
+    return response;
+
+  } catch (e) {
+    const error = e as Error
+    throw new DOMException(error.message, 'ConnectionFailed');
   }
-
-  const url = `/student/resume/list${queryParams ? `?${queryParams}` : ''}`;
-
-  const response = await api.get(url);
-  return response;
 };
