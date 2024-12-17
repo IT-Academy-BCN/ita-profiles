165308093aac3662aa432dfeeaacaf39e0f9268f
aida-rodi <aida.rodi.1997@gmail.com>
2024-12-17 10:15:00 +0100

    refactor: add styles to Button component

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,18 +1,21 @@
 import { ButtonHTMLAttributes, FC } from 'react'
 import cls from 'classnames'
 
-const defaultButtonStyles =
-  'bg-primary cursor-pointer text-white font-bold py-2 px-4 rounded'
-const secondaryButtonStyles = 'bg-secondary'
-// Add here outline and other default styles ...
+const defaultButtonStyles = 'bg-primary text-white font-bold py-6 w-80 rounded-xl cursor-pointer hover:bg-primary-hover active:bg-primary-active disabled:bg-disabled disabled:cursor-not-allowed'
+const outlineButtonStyles = 'bg-white text-stone-500 border border-gray-500 hover:bg-pink-200 hover:border-primary-hover hover:text-primary-hover active:bg-pink-300 active:border-primary active:text-primary-hover disabled:bg-white disabled:text-disabled disabled:border-disabled'
+const navbarButtonStyles = 'bg-white text-gray-3 font-medium rounded-lg px-3 py-2 w-auto hover:scale-[1.02] transition duration-150 ease-in-out'
 
 type TButton = ButtonHTMLAttributes<HTMLButtonElement> & {
-  secondary?: boolean
+  defaultButton?: boolean
+  outline?: boolean
+  navbar?: boolean
 }
-
+ 
 export const Button: FC<TButton> = ({
   type = 'button',
-  secondary = false,
+  defaultButton = true,
+  outline = false,
+  navbar = false,
   className,
   ...rest
 }) => (
@@ -20,8 +23,9 @@ export const Button: FC<TButton> = ({
     // eslint-disable-next-line react/button-has-type
     type={type}
     className={cls(
-      defaultButtonStyles,
-      secondary && secondaryButtonStyles,
+      defaultButton && defaultButtonStyles,
+      outline && outlineButtonStyles,
+      navbar && navbarButtonStyles,
       className,
     )}
     {...rest}
