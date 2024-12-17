2105a27484c6277211de8d86d490069b2465a90d
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-12-17 20:02:14 +0100

    fix FetchStudentList

--- src/api/FetchStudentsList.ts
+++ src/api/FetchStudentsList.ts
@@ -1,16 +1,22 @@
-import { TStudent } from '../../types';
+import { AxiosError } from 'axios';
 import api from './api';
 
-export const FetchStudentsList = async (selectedRoles: string[] = []): Promise<TStudent[]> => {
-  let queryParams = '';
+export const FetchStudentsList = async (selectedRoles: Array<string> = []) => {
+  try {
+    let queryParams = '';
 
-  if (selectedRoles.length > 0) {
-    queryParams += `specialization=${selectedRoles.join(',')}`;
+    if (selectedRoles.length > 0) {
+      queryParams += `specialization=${selectedRoles.join(',')}`;
+    }
+
+    const url = `//localhost:8000/api/v1/student/resume/list${queryParams ? `?${queryParams}` : ''}`;
+
+    const response = await api.get(url);
+
+    return response;
+
+  } catch (e) {
+    const error = e as AxiosError
+    throw new DOMException(error.message, 'ConnectionFailed');
   }
-
-  const url = `//localhost:8000/api/v1/student/resume/list${queryParams ? `?${queryParams}` : ''}`;
-
-  const response = await api.get(url);
-
-  return response;
 };
