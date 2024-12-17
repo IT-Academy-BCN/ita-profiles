695a02828ecea39530c2fa43ef9ffec38a59fd2e
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-12-17 13:06:26 +0100

    removing the string "//localhost:8000/api/v1"

--- src/components/students/studentDetail/filters/StudentFiltersContent.tsx
+++ src/components/students/studentDetail/filters/StudentFiltersContent.tsx
@@ -23,8 +23,8 @@ const StudentFiltersProvider: React.FC = () => {
         [selectedRoles, addRole, removeRole],
     )
 
-    const urlRoles = '//localhost:8000/api/v1/specialization/list'
-    const urlDevelopment = '//localhost:8000/api/v1/development/list'
+    const urlRoles = '/specialization/list'
+    const urlDevelopment = '/development/list'
 
     const fetchData = async (
         url: string,
