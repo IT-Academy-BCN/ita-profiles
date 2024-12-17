40732b30e35e5c5e25f2b686dadbc3112e5006ba
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-12-17 19:47:59 +0100

    Modification of the FetchSrtudentList file

--- src/api/FetchStudentsList.ts
+++ src/api/FetchStudentsList.ts
@@ -1,20 +1,16 @@
+import { TStudent } from '../../types';
 import api from './api';
 
-export const FetchStudentsList = async (selectedRoles: Array<string> = []) => {
-  try {
-    let queryParams = '';
+export const FetchStudentsList = async (selectedRoles: string[] = []): Promise<TStudent[]> => {
+  let queryParams = '';
 
-    if (selectedRoles.length > 0) {
-      queryParams += `specialization=${selectedRoles.join(',')}`;
-    }
-
-    const url = `/student/resume/list${queryParams ? `?${queryParams}` : ''}`;
-
-    const response = await api.get(url);
-    return response;
-
-  } catch (e) {
-    const error = e as Error
-    throw new DOMException(error.message, 'ConnectionFailed');
+  if (selectedRoles.length > 0) {
+    queryParams += `specialization=${selectedRoles.join(',')}`;
   }
+
+  const url = `//localhost:8000/api/v1/student/resume/list${queryParams ? `?${queryParams}` : ''}`;
+
+  const response = await api.get(url);
+
+  return response;
 };
