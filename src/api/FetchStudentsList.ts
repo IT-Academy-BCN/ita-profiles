171ceed53eee930c63472a84378c0549de100222
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-12-17 11:09:24 +0100

    Modifying the FetchStudentList file, to use the api.ts file

--- src/api/FetchStudentsList.ts
+++ src/api/FetchStudentsList.ts
@@ -1,26 +1,16 @@
-// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
-import axios, { AxiosError } from 'axios';
-import { TStudentList } from '../../types';
+import { TStudent } from "../../types";
+import api from "./api";
 
-// eslint-disable-next-line consistent-return
-export const FetchStudentsList = async (selectedRoles: Array<string> = []) => {
+export const FetchStudentsList = async (selectedRoles: Array<string> = []): Promise<TStudent[]> => {
 
-  try {
-    let queryParams = '';
+  let queryParams = '';
 
-    // Construir la cadena de consulta para roles seleccionados
-    if (selectedRoles.length > 0) {
-      queryParams += `specialization=${selectedRoles.join(',')}`;
-    }
-
-    // Construir la URL completa con la cadena de consulta
-    const url = `//localhost:8000/api/v1/student/resume/list${queryParams ? `?${queryParams}` : ''}`;
-
-    const response = await axios.get<TStudentList[]>(url);
-    return response.data;
-    // @ts-expect-error throws AxiosError exception
-  } catch (e: AxiosError) {
-    // eslint-disable-next-line no-console
-    throw new DOMException(e.message, 'ConnectionFailed');
+  if (selectedRoles.length > 0) {
+    queryParams += `specialization=${selectedRoles.join(',')}`;
   }
+
+  const url = `/student/resume/list${queryParams ? `?${queryParams}` : ''}`;
+
+  const response = await api.get(url);
+  return response;
 };
