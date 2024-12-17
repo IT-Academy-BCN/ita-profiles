312cde6367f2f0848f96753ed78daf575277c070
diega <diegochiola@gmail.com>
2024-12-17 15:49:30 +0100

    refactor: edit invalid messages

--- app/Http/Requests/Job/CreateJobOfferRequest.php
+++ app/Http/Requests/Job/CreateJobOfferRequest.php
@@ -44,24 +44,15 @@ class CreateJobOfferRequest extends FormRequest
         return [
             'recruiter_id.required' => 'The recruiter_id field is required.',
             'recruiter_id.exists' => 'The recruiter_id must correspond to an existing recruiter.',
-
             'company_id.required' => 'The company_id field is required.',
             'company_id.exists' => 'The company_id must correspond to an existing company.',
-
             'title.required' => 'The title field is required.',
             'title.max' => 'The title cannot exceed 255 characters.',
-
             'description.required' => 'The description field is required.',
             'description.max' => 'The description cannot exceed 255 characters.',
-
             'location.required' => 'The location field is required.',
             'location.max' => 'The location cannot exceed 255 characters.',
-
             'salary.max' => 'The salary cannot exceed 255 characters.',
-
-            'skills.max' => 'Skills cannot exceed 255 characters.',
-
-
         ];
     }
 }
