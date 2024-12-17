b5214ccbd5ac1acbb699d582b9e130e1151549cc
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-12-17 13:23:41 +0100

    query optimization & clockwork optimization

--- app/Models/Resume.php
+++ app/Models/Resume.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Models\Project;
+use App\Models\Student;
+use App\Models\Bootcamp;
+use App\Models\Language;
+use App\Models\Collaboration;
+use Illuminate\Support\Carbon;
 use App\Observers\ResumeObserver;
-use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use App\Models\AdditionalTraining;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 
 #[ObservedBy(ResumeObserver::class)]
 class Resume extends Model
@@ -68,5 +75,23 @@ class Resume extends Model
                 app()->instance('originalGitHubUrl', $resume->getOriginal('github_url'));
             }
         });
+    }
+
+    public static function findAllResumesWithStudentAndTags(): Collection
+    {
+        return self::query()
+            ->with([
+                'student' => function ($query) {
+                    $query->addSelect(['id', 'name', 'surname', 'photo', 'status']);
+                    $query->with([
+                        'tags' => function ($query) {
+                            $query->addSelect(['id', 'name']);
+                        }
+                    ]);
+                }
+            ])
+            ->addSelect(['id', 'specialization', 'modality', 'github_url', 'created_at'])
+            ->distinct()
+            ->get();
     }
 }
