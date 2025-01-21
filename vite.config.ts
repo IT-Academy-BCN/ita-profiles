8fb4548cd480b2f1ab443b1f3bf3576a0101bd81
Jordi <jmdesarrollo82@gmail.com>
2025-01-21 17:08:58 +0100

    fix: change the api target url when building the project

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,28 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    port: 80,
-    open: false,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/__tests__/setup.ts',
-    coverage: {
-      provider: 'v8',
-      reporter: ['lcov', 'text'],
+    plugins: [react()],
+    build: {
+        outDir: 'build'
     },
-  },
+    server: {
+        port: 80,
+        open: false,
+        proxy: {
+            '/api': {
+                target: 'https://ita-profiles.onrender.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/__tests__/setup.ts',
+        coverage: {
+            provider: 'v8',
+            reporter: ['lcov', 'text']
+        }
+    }
 })
