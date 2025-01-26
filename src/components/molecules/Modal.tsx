9f17f0877fc5a92bbf2247b4e0ccc6ab0a7e9281
raquelEve <calculadoraverde@gmail.com>
2025-01-26 20:28:39 +0100

    change modal rounded-md to rounded-xl

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -5,7 +5,7 @@ import svgClose from "../../assets/svg/close.svg"
 import { TModal } from "../../../types";
 
 const overlayStyle = "fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,.5)] flex justify-center items-center";
-const contentStyle = "bg-white p-4 rounded-md relative";
+const contentStyle = "bg-white p-4 rounded-xl relative";
 const buttonStyle = "absolute top-3 right-3 bg-none border-none text-xl cursor-pointer"
 
 const Modal: FC<TModal> = ({ children, isOpen, onClose }) => {
